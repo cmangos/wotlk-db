3515a2b0461f8098e4dd5f3c0b840139af19ef92
BuuGG <BuuGG@6c1fcba4-eb2c-0410-9096-d29676c3c6e1>
2009-02-08 17:08:28 +0000

    Update Pack 368 has been released!
    Requires: Core 7252+
    
    Biggest changes in 368:
    - Massive updates and additions for creature_model_info and creature_equip_template
    - New npc_texts/gameobjects/items/page_texts and wdb-updates for old ones
    - BonusTalents, RewMoneyMaxLevel and PlayersSlain data for quest_template
    - Hundreds of emotes and text updates for quests
    - Some immunities for tbc instances' trash mobs
    - Some data for spell_loot_template
    - Spawned ~4000 new herbs/veins
    - Updated skill_discovery_template 
    - Updates from ACID rev25 and SD2 rev 888
    - Many fixes from forums, thanks to all contributors!
    
    
    In order to get from 0.11.0 (367) to 0.11.0 (368), apply these files:
    * 368_corepatch_characters_7110_to_7252.sql <- apply to characters database. UPDATE DATABASE NAMES IN QUERIES
    * 368_corepatch_mangos_7110_to_7252.sql <- apply to mangos database
    * 368_updatepack_mangos.sql <- apply to mangos database

--- Updates/0.10.5_additions/366_corepatch_characters_6929_to_7069.sql
+++ Updates/0.10.5_additions/366_corepatch_characters_6929_to_7069.sql
@@ -99,8 +99,8 @@ ALTER TABLE character_db_version CHANGE COLUMN required_2008_12_22_18_characters
 UPDATE item_instance SET data = REPLACE(data,'  ',' ');
 UPDATE item_instance SET data = CONCAT(TRIM(data),' ');
 UPDATE item_instance SET data= CONCAT(
-  SUBSTRING_INDEX(SUBSTRING_INDEX(data,' ',30),' ',-30),' 0 0 0 ',
-  SUBSTRING_INDEX(SUBSTRING_INDEX(data,' ',60),' ',-60+30),' 0 ')
+  SUBSTRING_INDEX(SUBSTRING_INDEX(data,' ',54),' ',-54),' 0 0 0 ',
+  SUBSTRING_INDEX(SUBSTRING_INDEX(data,' ',60),' ',-60+54),' 0 ')
 WHERE SUBSTRING_INDEX(data,' ',60) = data AND SUBSTRING_INDEX(data,' ',60-1) <> data;
 UPDATE item_instance SET data= CONCAT(
   SUBSTRING_INDEX(SUBSTRING_INDEX(data,' ',30),' ',-30),' 0 0 0 ',
