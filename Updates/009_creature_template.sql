c807317fa7e1866cdd35d587ece70065fe442733
X-Savior <xsavior@acid.com>
2014-09-21 18:59:49 +0200

    Update creature_template data for creature 25782
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

---
+++ Updates/009_creature_template.sql
@@ -0,0 +1,3 @@
+
+-- Creature template update
+UPDATE creature_template SET MinLevel=70, MaxLevel=70, FactionAlliance=114, FactionHorde=114, Expansion=1, MinLevelHealth=6602, MaxLevelHealth=6602, MinMeleeDmg=252, MaxMeleeDmg=357, MinRangedDmg=215, MaxRangedDmg=320, UnitFlags=UnitFlags|33554688, Armor=6792, MeleeAttackPower=304, RangedAttackPower=44 WHERE entry=25782;
