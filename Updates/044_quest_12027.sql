2ecd02f6c9f612a636143c3d6ad0136a9d181f5b
Xfurry <xfurry@scriptdev2.com>
2014-10-23 21:01:40 +0200

    Fix [044]
    Thanks to @Neatelves

--- Updates/044_quest_12027.sql
+++ Updates/044_quest_12027.sql
@@ -5,4 +5,4 @@ insert into spell_script_target (entry,type,targetEntry,inverseEffectMask) value
 DELETE FROM creature_linking_template WHERE entry IN (26589);
 INSERT INTO creature_linking_template (entry, map, master_entry, flag, search_range) VALUES
 (26589, 571, 26588, 512, 100);
-UPDATE creature_template SET FactionAlliance=14, FactionHorde=14, UnitFlags=UnitFlags768 WHERE entry IN (26586,26590);
+UPDATE creature_template SET FactionAlliance=14, FactionHorde=14, UnitFlags=UnitFlags|768 WHERE entry IN (26586,26590);
