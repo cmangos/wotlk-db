32aa4e1094fef586a6a5e20475c0291050801f1f
Grz3s <grz3sp@wp.pl>
2014-11-01 19:50:38 +0000

    [061] Signboards in "The Library" Irongorge
    signboards in "The Library" Irongorge -- rotation update

---
+++ Updates/061_gameobject.sql
@@ -0,0 +1,13 @@
+-- signboards in "The Library" Irongorge -- rotation update 
+-- Roc Talon 
+UPDATE gameobject SET rotation0 = 0.607692, rotation1 = -0.361538, rotation2 = 0.607693, rotation3 = 0.361539 WHERE guid = 5398;
+-- The Skull of Tyrannistrasz
+UPDATE gameobject SET orientation = 1.5708, rotation0 = 0.147016, rotation1 = -0.691655, rotation2 = 0.147016, rotation3 = 0.691655 WHERE guid = 5425;
+-- Fossilized Egg
+UPDATE gameobject SET orientation = 2.21004, rotation0 = 0.607692, rotation1 = -0.361538, rotation2 = 0.607693, rotation3 = 0.361539 WHERE guid = 5405;
+-- Toothgnasher's Skeleton
+UPDATE gameobject SET orientation = 4.71239, rotation0 = 0.61994, rotation1 = -0.340111, rotation2 = 0.619939, rotation3 = 0.34011 WHERE guid = 5382;
+-- Horde Catapult
+UPDATE gameobject SET orientation = -0.521631, rotation0 = 0.309975, rotation1 = -0.635543, rotation2 = 0.309974, rotation3 = 0.635543 WHERE guid = 5205;
+-- Uldaman Reliefs
+UPDATE gameobject SET orientation = 2.30151, rotation0 = 0.681389, rotation1 = -0.188966, rotation2 = 0.68139, rotation3 = 0.188967 WHERE guid = 5141;
