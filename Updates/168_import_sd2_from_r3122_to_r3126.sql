065105d05d6962395ac2601cba81a30d234c5237
Grz3s <grz3sp@wp.pl>
2015-04-21 17:56:45 +0100

    [168] Applied SD2 updates
    pls update your SD2

---
+++ Updates/168_import_sd2_from_r3122_to_r3126.sql
@@ -0,0 +1,8 @@
+-- r3122
+UPDATE creature_template SET ScriptName='npc_light_orb_collector' WHERE entry IN (21926,22333);
+
+-- r3125
+UPDATE creature_template SET ScriptName='' WHERE entry=28912;
+
+-- r3126
+UPDATE creature_template SET ScriptName='npc_scarlet_courier' WHERE entry=29076;
