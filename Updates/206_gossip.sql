656c85d703b652863d5e218cfc8beefdcdc3edfb
Grz3s <grz3sp@wp.pl>
2015-05-30 00:09:39 +0100

    hotFix for [206]
    -- Hothix
    -- ids taken from trinity may not be right
    -- fixed one comming from cata (the same not changed texts) and whats
    most important here: they're comming from 100% source

--- Updates/206_gossip.sql
+++ Updates/206_gossip.sql
@@ -1,9 +1,15 @@
--- Marshal Dughan
-UPDATE creature_template SET npcFlags = 3, GossipMenuId = 6159 WHERE Entry = 240;
-DELETE FROM gossip_menu WHERE entry = 6159; 
-INSERT INTO gossip_menu (entry, text_id, script_id, condition_id) VALUES
-(6159, 6157, 0, 0);
--- missing text added
+-- Hothix
+-- ids taken from trinity may not be right 
+-- fixed one comming from cata (the same not changed texts) and whats most important here: they're comming from 100% source
+
+-- Marshal Dughan - gossip corrected
+DELETE FROM gossip_menu WHERE entry = 6159;
 DELETE FROM npc_text WHERE ID = 6157;
+UPDATE creature_template SET npcFlags = 3, GossipMenuId = 11611 WHERE Entry = 240;
+DELETE FROM gossip_menu WHERE entry = 11611; 
+INSERT INTO gossip_menu (entry, text_id, script_id, condition_id) VALUES
+(11611, 16211, 0, 0);
+-- missing text added
+DELETE FROM npc_text WHERE ID = 16211;
 INSERT INTO npc_text (ID, text0_0, text0_1, lang0, prob0, em0_0, em0_1) VALUES 
-(6157, 'Ach, it\'s hard enough keeping order around here without all these new troubles popping up!  I hope you have good news, $n...', 'Ach, it\'s hard enough keeping order around here without all these new troubles popping up!  I hope you have good news, $n...', 0, 1, 5, 5);
+(16211, 'Ach, it\'s hard enough keeping order around here without all these new troubles popping up!  I hope you have good news, $n...', 'Ach, it\'s hard enough keeping order around here without all these new troubles popping up!  I hope you have good news, $n...', 0, 1, 5, 5);
