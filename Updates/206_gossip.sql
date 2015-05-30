c62c88725444c07b1612ac7754eb5bb45161bdb4
Grz3s <grz3sp@wp.pl>
2015-05-30 14:35:58 +0100

    hotFix for [206] #2
    We'll leave text for Remy also.. so he can have correct gossip
    (still not sure if that correct value from trinity - but have no better
    option)

--- Updates/206_gossip.sql
+++ Updates/206_gossip.sql
@@ -4,12 +4,12 @@
 
 -- Marshal Dughan - gossip corrected
 DELETE FROM gossip_menu WHERE entry = 6159;
-DELETE FROM npc_text WHERE ID = 6157;
 UPDATE creature_template SET npcFlags = 3, GossipMenuId = 11611 WHERE Entry = 240;
 DELETE FROM gossip_menu WHERE entry = 11611; 
 INSERT INTO gossip_menu (entry, text_id, script_id, condition_id) VALUES
 (11611, 16211, 0, 0);
 -- missing text added
-DELETE FROM npc_text WHERE ID = 16211;
+DELETE FROM npc_text WHERE ID IN (6157,16211);
 INSERT INTO npc_text (ID, text0_0, text0_1, lang0, prob0, em0_0, em0_1) VALUES 
-(16211, 'Ach, it\'s hard enough keeping order around here without all these new troubles popping up!  I hope you have good news, $n...', 'Ach, it\'s hard enough keeping order around here without all these new troubles popping up!  I hope you have good news, $n...', 0, 1, 5, 5);
+(16211, 'Ach, it\'s hard enough keeping order around here without all these new troubles popping up!  I hope you have good news, $n...', 'Ach, it\'s hard enough keeping order around here without all these new troubles popping up!  I hope you have good news, $n...', 0, 1, 5, 5),
+(6157, 'Hey there, friend. My name\'s Remy. I\'m from Redridge to the east, and came here looking for business, looking for business. You got any...got any??', 'Hey there, friend. My name\'s Remy. I\'m from Redridge to the east, and came here looking for business, looking for business. You got any...got any??', 0, 1, 0, 0);
