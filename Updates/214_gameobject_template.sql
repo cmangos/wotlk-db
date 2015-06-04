0b122215e7ef95acd53a438bb86899d8d4a567ca
Grz3s <grz3sp@wp.pl>
2015-06-04 01:00:57 +0100

    [214] q.10629 'Shizz Work'
    After digging in "POO' we should recive debuff.. that comes from missing
    object:
    - missing object added: TEMP Felhound Poo Trap (source: YTDB)
    
    Also: Core need small update for this spell:
    must be debuff instead of buff
    more can be found here:
    http://cmangos.net/thread-6991.html

---
+++ Updates/214_gameobject_template.sql
@@ -0,0 +1,7 @@
+-- q.10629 'Shizz Work'
+
+-- source: YTDB
+-- missing object added: TEMP Felhound Poo Trap
+DELETE FROM gameobject_template WHERE entry = 184981;
+INSERT INTO gameobject_template (entry, type, displayId, name, IconName, castBarCaption, unk1, faction, flags, size, questItem1, questItem2, questItem3, questItem4, questItem5, questItem6, data0, data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14, data15, data16, data17, data18, data19, data20, data21, data22, data23, mingold, maxgold, ScriptName) VALUES
+(184981,6,1287,'TEMP Felhound Poo Trap','','','',0,0,1,0,0,0,0,0,0,0,0,0,37695,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'');
