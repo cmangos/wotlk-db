7536df61a3250cdd18f64d0fed39c9cb6adf82ff
Tamian <spkansas@msn.com>
2015-09-03 23:37:25 +0200

    Added support for remote DB, and adjusted naming to support multiple DB/Schemas.

--- InstallFullDB.sh
+++ InstallFullDB.sh
@@ -13,9 +13,10 @@ CONFIG_FILE="InstallFullDB.config"
 ADDITIONAL_PATH=""
 
 #variables assigned and read from $CONFIG_FILE
-DATABASE=""
-USERNAME=""
-PASSWORD=""
+MANGOS_DBHOST=""
+MANGOS_DBNAME=""
+MANGOS_DBUSER=""
+MANGOS_DBPASS=""
 MYSQL=""
 CORE_PATH=""
 SD2_UPDATES="1"
@@ -27,24 +28,28 @@ cat >  $CONFIG_FILE << EOF
 # This is the config file for the '$SCRIPT_FILE' script
 #
 # You need to insert
-#   DATABASE:     Your database
-#   USERNAME:     Your username
-#   PASSWORD:     Your password
-#   CORE_PATH:    Your path to core's directory (OPTIONAL: Use if you want to apply remaining core updates automatically)
-#   ACID_PATH:    Your path to a git-clone of ACID (OPTIONAL: Use it if you want to install recent downloaded acid)
-#   SD2_UPDATES:  If you want to disable adding ScriptDev2 updates (Has only meaning if CORE_PATH above is set
-#   MYSQL:        Your mysql command (usually mysql)
+#   MANGOS_DBHOST:	Your MANGOS database host
+#   MANGOS_DBNAME:	Your MANGOS database schema
+#   MANGOS_DBUSER:	Your MANGOS username
+#   MANGOS_DBPASS:	Your MANGOS password
+#   CORE_PATH:    	Your path to core's directory (OPTIONAL: Use if you want to apply remaining core updates automatically)
+#   ACID_PATH:    	Your path to a git-clone of ACID (OPTIONAL: Use it if you want to install recent downloaded acid)
+#   SD2_UPDATES:  	If you want to disable adding ScriptDev2 updates (Has only meaning if CORE_PATH above is set
+#   MYSQL:        	Your mysql command (usually mysql)
 #
 ####################################################################################################
 
+## Define the host on which the mangos database resides (typically localhost)
+MANGOS_DBHOST="localhost"
+
 ## Define the database in which you want to add clean UDB
-DATABASE="mangos"
+MANGOS_DBNAME="mangos"
 
 ## Define your username
-USERNAME="mangos"
+MANGOS_DBUSER="mangos"
 
 ## Define your password (It is suggested to restrict read access to this file!)
-PASSWORD="mangos"
+MANGOS_DBPASS="mangos"
 
 ## Define the path to your core's folder (This is optional)
 ##   If set the core updates located under sql/updates from this mangos-directory will be added automatically
@@ -86,7 +91,7 @@ fi
 
 . $CONFIG_FILE
 
-MYSQL_COMMAND="$MYSQL -u$USERNAME -p$PASSWORD $DATABASE"
+MYSQL_MANGOSDB_CMD="$MYSQL -h$MANGOS_DBHOST -u$MANGOS_DBUSER -p$MANGOS_DBPASS $MANGOS_DBNAME"
 
 ## Print header
 echo
@@ -105,42 +110,42 @@ echo .
 
 ## Full
 echo "Process UDB 0.12.2"
-$MYSQL_COMMAND < ${ADDITIONAL_PATH}Current_Release/Full_DB/UDB_0.12.2_mangos_11792_SD2_2279.sql
+$MYSQL_MANGOSDB_CMD < ${ADDITIONAL_PATH}Current_Release/Full_DB/UDB_0.12.2_mangos_11792_SD2_2279.sql
 [[ $? != 0 ]] && exit 1
 
 ## 403
 echo "Process Updatepack 403"
-$MYSQL_COMMAND < ${ADDITIONAL_PATH}Current_Release/Updates/403_corepatch_mangos_11793_to_11840.sql
+$MYSQL_MANGOSDB_CMD < ${ADDITIONAL_PATH}Current_Release/Updates/403_corepatch_mangos_11793_to_11840.sql
 [[ $? != 0 ]] && exit 1
-$MYSQL_COMMAND < ${ADDITIONAL_PATH}Current_Release/Updates/403_updatepack_mangos.sql
+$MYSQL_MANGOSDB_CMD < ${ADDITIONAL_PATH}Current_Release/Updates/403_updatepack_mangos.sql
 [[ $? != 0 ]] && exit 1
 
 ## 404
 echo "Process Updatepack 404"
-$MYSQL_COMMAND < ${ADDITIONAL_PATH}Current_Release/Updates/404_corepatch_mangos_11841_to_11928.sql
+$MYSQL_MANGOSDB_CMD < ${ADDITIONAL_PATH}Current_Release/Updates/404_corepatch_mangos_11841_to_11928.sql
 [[ $? != 0 ]] && exit 1
-$MYSQL_COMMAND < ${ADDITIONAL_PATH}Current_Release/Updates/404_updatepack_mangos.sql
+$MYSQL_MANGOSDB_CMD < ${ADDITIONAL_PATH}Current_Release/Updates/404_updatepack_mangos.sql
 [[ $? != 0 ]] && exit 1
 
 ## 405
 echo "Process Updatepack 405"
-$MYSQL_COMMAND < ${ADDITIONAL_PATH}Current_Release/Updates/405_corepatch_mangos_11929_to_12111.sql
+$MYSQL_MANGOSDB_CMD < ${ADDITIONAL_PATH}Current_Release/Updates/405_corepatch_mangos_11929_to_12111.sql
 [[ $? != 0 ]] && exit 1
-$MYSQL_COMMAND < ${ADDITIONAL_PATH}Current_Release/Updates/405_updatepack_mangos.sql
+$MYSQL_MANGOSDB_CMD < ${ADDITIONAL_PATH}Current_Release/Updates/405_updatepack_mangos.sql
 [[ $? != 0 ]] && exit 1
 
 ## 406
 echo "Process Updatepack 406"
-$MYSQL_COMMAND < ${ADDITIONAL_PATH}Current_Release/Updates/406_corepatch_mangos_12112_to_12444.sql
+$MYSQL_MANGOSDB_CMD < ${ADDITIONAL_PATH}Current_Release/Updates/406_corepatch_mangos_12112_to_12444.sql
 [[ $? != 0 ]] && exit 1
-$MYSQL_COMMAND < ${ADDITIONAL_PATH}Current_Release/Updates/406_updatepack_mangos.sql
+$MYSQL_MANGOSDB_CMD < ${ADDITIONAL_PATH}Current_Release/Updates/406_updatepack_mangos.sql
 [[ $? != 0 ]] && exit 1
 
 ## 407
 echo "Process Updatepack 407"
-$MYSQL_COMMAND < ${ADDITIONAL_PATH}Current_Release/Updates/407_corepatch_mangos_12445_to_12670.sql
+$MYSQL_MANGOSDB_CMD < ${ADDITIONAL_PATH}Current_Release/Updates/407_corepatch_mangos_12445_to_12670.sql
 [[ $? != 0 ]] && exit 1
-$MYSQL_COMMAND < ${ADDITIONAL_PATH}Current_Release/Updates/407_updatepack_mangos.sql
+$MYSQL_MANGOSDB_CMD < ${ADDITIONAL_PATH}Current_Release/Updates/407_updatepack_mangos.sql
 [[ $? != 0 ]] && exit 1
 
 
@@ -160,7 +165,7 @@ for f in Updates/*.sql
 do
   CUR_REV=`basename "$f" | sed 's/^\([0-9]*\)_.*/\1/' `
   echo "Applying Update $CUR_REV"
-  $MYSQL_COMMAND < "$f"
+  $MYSQL_MANGOSDB_CMD < "$f"
   [[ $? != 0 ]] && exit 1
 
   # Was this a core or SD2-update?
@@ -210,7 +215,7 @@ then
         then
           # found a newer core update file
           echo "Append core update `basename "$f"` to database $DATABASE"
-          $MYSQL_COMMAND < "$f"
+          $MYSQL_MANGOSDB_CMD < "$f"
           [[ $? != 0 ]] && exit 1
         fi
       done
@@ -225,7 +230,7 @@ then
     then
       # found a newer core update file
       echo "Append core update `basename "$f"` to database $DATABASE"
-      $MYSQL_COMMAND < "$f"
+      $MYSQL_MANGOSDB_CMD < "$f"
       [[ $? != 0 ]] && exit 1
     fi
   done
@@ -251,7 +256,7 @@ then
         then
           # found a newer core update file
           echo "Append SD2 update`basename "$f"` to database $DATABASE"
-          $MYSQL_COMMAND < "$f"
+          $MYSQL_MANGOSDB_CMD < "$f"
           [[ $? != 0 ]] && exit 1
         fi
       done
@@ -266,7 +271,7 @@ then
     then
       # found a newer core update file
       echo "Append SD2 update`basename "$f"` to database $DATABASE"
-      $MYSQL_COMMAND < "$f"
+      $MYSQL_MANGOSDB_CMD < "$f"
       [[ $? != 0 ]] && exit 1
     fi
   done
@@ -290,7 +295,7 @@ then
 
   # Apply acid_wotlk.sql
   echo "Applying $ACID_PATH/acid_wotlk.sql ..."
-  $MYSQL_COMMAND < "${ACID_PATH}"/acid_wotlk.sql
+  $MYSQL_MANGOSDB_CMD < "${ACID_PATH}"/acid_wotlk.sql
   [[ $? != 0 ]] && exit 1
   echo "Recent state of ACID applied"
 fi
