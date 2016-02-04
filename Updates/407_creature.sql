e725216c25c47eb804ddf1a53adef169dfa61ed5
Xfurry <xfurry@scriptdev2.com>
2016-02-04 23:17:03 +0000

    [407] Magwin
    Update req. for scriptdev2 script.
    Source:
    https://github.com/cmangos/mangos-wotlk/commit/a16bfccf3605e46df736e04195526ad382e3a85e

---
+++ Updates/407_creature.sql
@@ -0,0 +1,4 @@
+-- Magwin
+DELETE FROM creature_template_addon WHERE entry = 17312;
+INSERT INTO creature_template_addon (entry,mount,bytes1,b2_0_sheath,b2_1_pvp_state,emote,moveflags,auras) VALUES
+(17312, 0, 0, 1, 16, 0, 0, '');
