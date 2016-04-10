622d2d99bc3534dd23b2fdefa24968f426a0038e
Grz3s <grz3sp@wp.pl>
2016-04-10 13:57:04 +0100

    fix for prev commit

--- Updates/469_quest_template.sql
+++ Updates/469_quest_template.sql
@@ -1,6 +1,3 @@
 -- Quests relations.
 -- q.9759 'Ending their world' -- should become available after 'What We Know...'
 UPDATE quest_template SET PrevQuestId = 9753 WHERE entry = 9759;
-
--- q.9760 'Vindicator's Rest'
-UPDATE quest_template SET NextQuestInChain = 0 WHERE entry = 9760;
