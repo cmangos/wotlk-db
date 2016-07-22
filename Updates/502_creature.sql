a75147070239eba04753f5bc06f8f527fcf3e1d3
Grz3s <grz3sp@wp.pl>
2016-07-22 20:19:37 +0100

    [502] Warlock's Imp and Succubus - Class corrected
    thx to @Darth-Postal
    
    Closes: https://github.com/unified-db/Database/issues/231

---
+++ Updates/502_creature.sql
@@ -0,0 +1,5 @@
+-- Warlock's Imp and Succubus - Class corrected
+-- Imp
+UPDATE creature_template SET UnitClass = 8 WHERE entry = 416;
+-- Succubus
+UPDATE creature_template SET UnitClass = 2 WHERE entry = 1863;
