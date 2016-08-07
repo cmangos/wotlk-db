d532c91789e344ffdbb58006e8b43bf5e66fde79
MantisLord <MantisLord@users.noreply.github.com>
2016-08-07 11:09:57 +0100

    [519] Fix Grim Batol sign position in Wetlands
    Source: https://github.com/TBC-DB/Database/pull/49

---
+++ Updates/519_gameobject.sql
@@ -0,0 +1,2 @@
+-- Fix Grim Batol sign position in Wetlands
+UPDATE gameobject SET position_x = -3597.3, position_y = -2714.84, position_z = 17.02, orientation = 2.47837, rotation0 = 0, rotation1 = 0, rotation2 = 0.945519, rotation3 = 0.325567, spawntimesecs = 7200, animprogress = 100, state = 1 WHERE guid = 13764;
