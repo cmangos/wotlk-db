413b308d460d4f8f6097b53e0c6d3efea71864af
killerwife <killerwife@users.noreply.github.com>
2016-08-16 19:53:22 +0100

    [526] q.6568 'Mistress of Deception'
    RewardText corrected.
    
    Closes: https://github.com/unified-db/Database/issues/240

---
+++ Updates/526_quest_6568.sql
@@ -0,0 +1,3 @@
+-- Mistress of Deception(q.6568)
+-- OfferRewardText corrected
+UPDATE quest_template SET OfferRewardText ='Hmph. That one.$B$BHow is Rokaro? As suspicious as ever, sending me a note like this.$B$BYou must be on an important mission if Rokaro has sent you to me.$B$BWell, I owe the Warchief a favor or two.' WHERE entry = 6568; 
