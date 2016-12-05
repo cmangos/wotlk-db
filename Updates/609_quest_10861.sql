74daafbcf53796d5fa2315b279881f4894963af6
Grz3s <grz3sp@wp.pl>
2016-12-05 23:00:38 +0000

    [610] Silvermoon Dragonhawk
    Missing added
    - 2 in Eversong Woods
    - 2 in Ghostlands
    Also:
    Flags corrected for Sathein
    Small fix for [609]
    
    Part of:
    https://github.com/cmangos/wotlk-db/issues/4

--- Updates/609_quest_10861.sql
+++ Updates/609_quest_10861.sql
@@ -12,7 +12,7 @@ DELETE FROM dbscripts_on_go_template_use WHERE id IN (185208,185212);
 UPDATE creature_template SET Inhabittype = 4, MovementType = 2 WHERE Entry = 22339;
 DELETE FROM creature_movement_template WHERE entry = 22339;
 INSERT INTO creature_movement_template (entry, point, position_x, position_y, position_z, waittime, script_id, orientation, model1, model2) VALUES
-(22339, 1, -3636.85,5767.76,191.548, 5000, 0, 1.89967, 0, 0);
+(22339, 1, -3636.85,5767.76,191.548, 5000, 2233901, 1.89967, 0, 0);
 DELETE FROM dbscripts_on_creature_movement WHERE id = 2233901; 
 INSERT INTO dbscripts_on_creature_movement (id, delay, command, datalong, datalong2, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
 (2233901,0,18,0,0,0,0,0x04,0,0,0,0,0,0,0,0,'desp self');
---
+++ Updates/610_creature.sql
@@ -0,0 +1,19 @@
+-- Silvermoon Dragonhawk - missing added - UDB free guids reused
+DELETE FROM creature WHERE guid IN (129168,129169,129170,129171);
+DELETE FROM creature_addon WHERE guid IN (129168,129169,129170,129171);
+DELETE FROM creature_movement WHERE id IN (129168,129169,129170,129171);
+DELETE FROM game_event_creature WHERE guid IN (129168,129169,129170,129171);
+DELETE FROM game_event_creature_data WHERE guid IN (129168,129169,129170,129171);
+DELETE FROM creature_battleground WHERE guid IN (129168,129169,129170,129171);
+DELETE FROM creature_linking WHERE guid IN (129168,129169,129170,129171) OR master_guid IN (129168,129169,129170,129171);
+INSERT INTO creature (guid, id, map, spawnMask, phaseMask, modelid, equipment_id, position_x, position_y, position_z, orientation, spawntimesecs, spawndist, currentwaypoint, curhealth, curmana, DeathState, MovementType) VALUES 
+-- Eversong Woods
+(129168,27946,530,1,1,0,0,9377.618,-7169.038,9.17141,3.106686,300,0,0,17742,0,0,0),
+(129169,27946,530,1,1,0,0,9378.314,-7162.116,8.988449,3.141593,300,0,0,17742,0,0,0),
+-- Ghostlands
+(129170,27946,530,1,1,0,0,7591.52,-6777.482,86.86617,4.310963,300,0,0,17742,0,0,0),
+(129171,27946,530,1,1,0,0,7600.588,-6780.264,86.06586,4.258604,300,0,0,17742,0,0,0);
+
+-- Sathein
+-- Flags corrected (now will display correct icon)
+UPDATE creature_template SET NpcFlags = 83, UnitFlags = 512 WHERE entry = 16278;
