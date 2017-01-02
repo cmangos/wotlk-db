d7f9a618d452301089a5a827e88e3279d3d2603b
X-Savior <xsavior@acid.com>
2017-01-02 16:31:16 +0000

    [673] Remove item from loot templates - vendor only

---
+++ Updates/673_item.sql
@@ -0,0 +1,4 @@
+-- ================================================================
+-- DELETE Miniscule Diamond Ring FROM CHEST LOOT (VENDOR ITEM ONLY)
+-- ================================================================
+DELETE FROM gameobject_loot_template WHERE item IN (7339);
