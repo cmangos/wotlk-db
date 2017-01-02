1a69b16b1c993ad192b861018a2a6ca51ac35514
X-Savior <xsavior@acid.com>
2017-01-02 17:45:07 +0000

    [679] Fix Holy Spring loot

---
+++ Updates/679_item.sql
@@ -0,0 +1,4 @@
+-- =====================
+-- Not Only a Quest Item - Also Available If Not On The Quests
+-- =====================
+UPDATE gameobject_loot_template SET ChanceOrQuestChance = 100 WHERE entry = 938 AND item = 737;
