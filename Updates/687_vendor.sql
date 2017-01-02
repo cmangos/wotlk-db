77d3f1a4fc3362b9b709e9b396a8e88545fbabaa
X-Savior <xsavior@acid.com>
2017-01-02 18:28:22 +0000

    [687] Add Item names to Vendor tables

---
+++ Updates/687_vendor.sql
@@ -0,0 +1,5 @@
+-- ==============================================
+-- Add Additional Comment Fields For Vendor Items
+-- ==============================================
+ALTER TABLE npc_vendor_template ADD COLUMN comments TEXT;
+ALTER TABLE npc_vendor ADD COLUMN comments TEXT;
