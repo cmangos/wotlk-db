7105678e5d415ec8e860deda98227c29b7663b92
X-Savior <xsavior@acid.com>
2017-01-02 14:19:35 +0000

    [659] Fix Monstrous Crawler Stats

---
+++ Updates/659_creature.sql
@@ -0,0 +1,2 @@
+-- Monstrous Crawler
+UPDATE creature_template SET Expansion = 0, MinLevel = 43, MaxLevel = 44, HealthMultiplier = 1, MinLevelHealth = 1716, MaxLevelHealth = 1782, ArmorMultiplier = 1.4892, Armor = 3808, MinMeleeDmg = 67.0, MaxMeleeDmg = 96.0, MinRangedDmg = 46, MaxRangedDmg = 73, DamageMultiplier = 1.0, DamageVariance = 1.0, MeleeAttackPower = 174, RangedAttackPower = 16 WHERE entry = 1088;
