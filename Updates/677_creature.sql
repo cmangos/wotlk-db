7eee744aae9caf24b123a53705b12ed042403f0a
X-Savior <xsavior@acid.com>
2017-01-02 17:37:51 +0000

    [677] Fix Chief Engineer Lothander Factions

---
+++ Updates/677_creature.sql
@@ -0,0 +1,4 @@
+-- =========================================
+-- Correct Chief Engineer Lorthander Faction - (He Was 35 Friendly For Some Reason)
+-- =========================================
+UPDATE creature_template SET FactionAlliance = 1793, FactionHorde = 1793 WHERE entry = 18697;
