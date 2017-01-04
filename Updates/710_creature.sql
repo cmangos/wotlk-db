90450a97170a31f2b263e47fa2367587de741ea2
X-Savior <xsavior@acid.com>
2017-01-04 21:09:06 +0000

    [710] Fix q.9737 respawn time

---
+++ Updates/710_creature.sql
@@ -0,0 +1,4 @@
+-- ================================================================================
+-- Fix Respawn Time for Quest 9737 "True Masters of the Light" (Paladin epic mount)
+-- ================================================================================
+UPDATE creature SET spawntimesecs = 15 WHERE id IN (17915);
