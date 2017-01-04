7a31ad8634b81a1c647af1a27d1d3de9161a2dc5
X-Savior <xsavior@acid.com>
2017-01-04 21:14:08 +0000

    [712] Fix q.10855 special flags

---
+++ Updates/712_quest_10855.sql
@@ -0,0 +1,4 @@
+-- ==========================================
+-- Fix Quest: 10855 "Fel Reavers, No Thanks!"
+-- ==========================================
+UPDATE quest_template SET SpecialFlags = 0 WHERE entry = 10855;
