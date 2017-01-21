c575d2bb3f1f273f833134cf1308778108c55598
Grz3s <grz3sp@wp.pl>
2017-01-21 14:02:26 +0000

    [732] Stats
    Teron Gorefiend
    Karsius the Ancient Watcher

---
+++ Updates/732_Stats.sql
@@ -0,0 +1,5 @@
+-- Stats
+-- Teron Gorefiend
+UPDATE creature_template SET Expansion = 1, MinLevelHealth = 17709, MaxLevelHealth = 17709, MinLevelMana = 19854, MaxLevelMana = 19854, Armor = 6719, MeleeBaseAttackTime = 2000, RangedBaseAttackTime = 2000, MeleeAttackPower = 296, MinMeleeDmg = 242, MaxMeleeDmg = 342 WHERE entry = 21867;
+-- Karsius the Ancient Watcher
+UPDATE creature_template SET Expansion = 1, MinLevelHealth = 17709, MaxLevelHealth = 17709, MinLevelMana = 19854, MaxLevelMana = 19854, Armor = 6719, MeleeBaseAttackTime = 2000, RangedBaseAttackTime = 2000, MeleeAttackPower = 296, MinMeleeDmg = 242, MaxMeleeDmg = 342 WHERE entry = 21877;
