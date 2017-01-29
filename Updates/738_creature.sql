9a18609d65f64d56e52cb0a56d1166ff9f42e1c8
Grz3s <grz3sp@wp.pl>
2017-01-29 21:02:56 +0000

    [738] Earthbinder Rayge
    - position and stats corrected
    
    Closes:
    https://github.com/cmangos/wotlk-db/issues/61

---
+++ Updates/738_creature.sql
@@ -0,0 +1,3 @@
+-- Earthbinder Rayge
+UPDATE creature SET position_x = 296.6974, position_y = -362.373, position_z = 50.15062, Orientation = 5.5676, spawndist = 0, MovementType = 0 WHERE guid = 86372;
+UPDATE creature_template SET FactionAlliance = 1660, FactionHorde = 1660, UnitFlags = 33024, MinLevelHealth = 4572, MaxLevelHealth = 4572, MinLevelMana = 2705, MaxLevelMana = 2705, Armor = 4932, MeleeBaseAttackTime = 2000, RangedBaseAttackTime = 2000, MeleeAttackPower = 256, MinMeleeDmg = 177, MaxMeleeDmg = 247 WHERE Entry = 17885;
