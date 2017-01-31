27e28915a7469ae707c1da6b7d6c6359562f159f
Grz3s <grz3sp@wp.pl>
2017-01-31 00:52:03 +0000

    [741] Ethereal Transporter Control Panel
    Condition corrected to activate q.10218 'Someone Else's Hard Work Pays
    Off'
    
    Closes:
    https://github.com/cmangos/wotlk-db/issues/65

---
+++ Updates/741_gameobject.sql
@@ -0,0 +1,3 @@
+-- Ethereal Transporter Control Panel
+-- condition corrected to activate q.10218 'Someone Else's Hard Work Pays Off'
+UPDATE gossip_menu_option SET condition_id = 1015 WHERE menu_id = 8023;
