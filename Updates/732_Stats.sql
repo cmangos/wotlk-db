3e003f600dc0e3f7ab11f115dd89b28f9ac27d45
Grz3s <grz3sp@wp.pl>
2017-02-12 11:06:46 +0000

    Update for [732]
    Hp corrected for Karsius the Ancient Watcher
    - change has no impact in game (Inly for DB itself)
    
    Thx to @killerwife

--- Updates/732_Stats.sql
+++ Updates/732_Stats.sql
@@ -2,4 +2,4 @@
 -- Teron Gorefiend
 UPDATE creature_template SET Expansion = 1, MinLevelHealth = 17709, MaxLevelHealth = 17709, MinLevelMana = 19854, MaxLevelMana = 19854, Armor = 6719, MeleeBaseAttackTime = 2000, RangedBaseAttackTime = 2000, MeleeAttackPower = 296, MinMeleeDmg = 242, MaxMeleeDmg = 342 WHERE entry = 21867;
 -- Karsius the Ancient Watcher
-UPDATE creature_template SET Expansion = 1, MinLevelHealth = 17709, MaxLevelHealth = 17709, MinLevelMana = 19854, MaxLevelMana = 19854, Armor = 6719, MeleeBaseAttackTime = 2000, RangedBaseAttackTime = 2000, MeleeAttackPower = 296, MinMeleeDmg = 242, MaxMeleeDmg = 342 WHERE entry = 21877;
+UPDATE creature_template SET Expansion = 1, MinLevelHealth = 47224, MaxLevelHealth = 47224, MinLevelMana = 19854, MaxLevelMana = 19854, Armor = 6719, MeleeBaseAttackTime = 2000, RangedBaseAttackTime = 2000, MeleeAttackPower = 296, MinMeleeDmg = 242, MaxMeleeDmg = 342 WHERE entry = 21877;
