40e79f454fc7a96cbea53156d859c924526c7806
Grz3s <grz3sp@wp.pl>
2017-03-16 23:26:40 +0000

    [775] q.3125 'Faerie Dragon Muisek'
    Prevent Players to get all credits from one death body.
    Must be 1body = 1credit

---
+++ Updates/775_quest_3125.sql
@@ -0,0 +1,6 @@
+-- q.3125 'Faerie Dragon Muisek'
+-- Prevent Players to get all credits from one death body.  
+-- ID - 11888 Capture Faerie Dragon
+DELETE FROM dbscripts_on_spell WHERE id = 11888;
+INSERT INTO dbscripts_on_spell (id, delay, command, datalong, datalong2, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
+(11888, 1, 18, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'despawn self');
