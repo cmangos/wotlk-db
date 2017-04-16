cfaa6b76121d6f587797af2a836d50079b00838e
NeatElves <neatelves@gmail.com>
2017-04-16 12:27:45 +0100

    [805] Target added for spell 45222
    Closes:
    https://github.com/cmangos/wotlk-db/issues/55

---
+++ Updates/805_creature.sql
@@ -0,0 +1,4 @@
+-- Let's help Harrison Jones with Strange Gong
+DELETE FROM spell_script_target WHERE entry = 45222;
+INSERT INTO spell_script_target (entry, type, targetEntry, inverseEffectMask) VALUES
+(45222, 0, 187359, 0);
