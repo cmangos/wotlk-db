01e8f79edb2e1ee2e9c730fc3112005da0e21efe
Tobschinski <tobschinski@gmail.com>
2017-05-20 14:04:26 +0100

    [848] Molten Core...
    + Son of Flame: update walk- & runspeed
    + Ragnaros: change elemental to physical melee damage (again?)
    Source: https://github.com/cmangos/classic-db/commit/260b059a1f8ece4fe10510f07c7eb999da0068d4

---
+++ Updates/848_creature.sql
@@ -0,0 +1,4 @@
+-- Son of Flame: update walk- & runspeed
+-- Ragnaros: change elemental to physical melee damage (again?)
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (12 / 7) WHERE Entry = 12143;
+UPDATE creature_template SET DamageSchool = 0 WHERE Entry = 11502;
