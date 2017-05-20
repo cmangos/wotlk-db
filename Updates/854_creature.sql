73441cdb516aadb6f26fe04cd442959653cc5dfd
Tobschinski <tobschinski@gmail.com>
2017-05-20 14:14:42 +0100

    [854][ACID] Vekniss Soldier ... cast 'Frenzy Aura' when spawned, not when dead
    Source: https://github.com/cmangos/classic-db/commit/148af38f8320f8bd988a2bc9d81c3d22dfcebfbf

---
+++ Updates/854_creature.sql
@@ -0,0 +1,4 @@
+-- Vekniss Soldier 
+DELETE FROM creature_template_addon WHERE entry = 15229;
+INSERT INTO creature_template_addon (entry, mount, bytes1, b2_0_sheath, b2_1_pvp_state, emote, moveflags, auras) VALUES 
+(15229, 0, 0, 1, 16, 0, 0, 25801);
