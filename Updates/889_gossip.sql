7707086df88816530a30809880ad3fe9a217d599
Grz3s <grz3sp@wp.pl>
2017-05-29 21:21:45 +0100

    [892] Stats ...
    Weakened Magnataur Huntress
    Weakened Gammothra
    Part of:
    https://github.com/unified-db/Database/issues/130
    
    Also typo fixed in [889]

--- Updates/889_gossip.sql
+++ Updates/889_gossip.sql
@@ -5,7 +5,7 @@ UPDATE creature_template SET NpcFlags = 3, GossipMenuId = 50103 WHERE entry = 22
 UPDATE creature SET position_x = -296.493, position_y = -459.043, position_z = 6.14677, orientation = 0.767945 WHERE guid = 12403;
 DELETE FROM gossip_menu WHERE entry = 50103;
 INSERT INTO gossip_menu (entry, text_id, script_id, condition_id) VALUES 
-(50000, 10676, 0, 0);
+(50103, 10676, 0, 0);
 
 -- Skar'this Prison
 -- must be visible also in Heroic mode
