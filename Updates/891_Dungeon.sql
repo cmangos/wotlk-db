8eece40ab60d16408b25238769a5c7f3b1b5e182
Grz3s <grz3sp@wp.pl>
2017-05-30 20:29:33 +0100

    fix for [891]
    https://www.youtube.com/watch?v=-y6wXYeZk8A

--- Updates/891_Dungeon.sql
+++ Updates/891_Dungeon.sql
@@ -2,12 +2,12 @@
 -- Some creatures schould be linked and walk together
 
 -- Coilfang Technician
-DELETE FROM creature_linking WHERE guid IN (84601,84602);
+DELETE FROM creature_linking WHERE guid IN (79386,79411);
 INSERT INTO creature_linking VALUES
 (79386, 79367, 1+512), -- Coilfang Collaborator
 (79411, 79367, 1+512); -- Coilfang Collaborator
-UPDATE creature SET Spawndist = 0, MovementType = 0 WHERE guid IN (84601,84602);
-DELETE FROM creature_movement WHERE id IN (84601,84602);
+UPDATE creature SET Spawndist = 0, MovementType = 0 WHERE guid IN (79386,79411);
+DELETE FROM creature_movement WHERE id IN (79386,79411);
 
 -- Coilfang Enchantress #1
 DELETE FROM creature_linking WHERE guid IN (79509,79518);
