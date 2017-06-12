3b1ac95fceb6efef3216a4a6ea08fb1ec684833e
Grz3s <grz3sp@wp.pl>
2017-06-12 22:28:29 +0100

    Update for [889]
    Thx to @killerwife We now have correct gossip entry.

--- Updates/889_gossip.sql
+++ Updates/889_gossip.sql
@@ -1,11 +1,10 @@
 -- Skar'this the Heretic
 -- position corrected & Gossip
--- *Note* gossip_menu id is temp. until we'll find correct one.
-UPDATE creature_template SET NpcFlags = 3, GossipMenuId = 50103 WHERE entry = 22421;
+UPDATE creature_template SET NpcFlags = 3, GossipMenuId = 8534 WHERE entry = 22421;
 UPDATE creature SET position_x = -296.493, position_y = -459.043, position_z = 6.14677, orientation = 0.767945 WHERE guid = 12403;
-DELETE FROM gossip_menu WHERE entry = 50103;
+DELETE FROM gossip_menu WHERE entry = 8534;
 INSERT INTO gossip_menu (entry, text_id, script_id, condition_id) VALUES 
-(50103, 10676, 0, 0);
+(8534, 10676, 0, 0);
 
 -- Skar'this Prison
 -- must be visible also in Heroic mode
