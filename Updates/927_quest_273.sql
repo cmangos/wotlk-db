962497b8cd90fbe9f0fa5219219bbe345bb1eada
Grz3s <grz3sp@wp.pl>
2017-07-02 17:48:31 +0100

    Fix for [927]
    Sor i used that guid again...

--- Updates/927_quest_273.sql
+++ Updates/927_quest_273.sql
@@ -6,16 +6,16 @@
 
 -- Saean
 -- missing added - WoTLKDB free guids reused
-DELETE FROM creature WHERE guid = 75870;
-DELETE FROM creature_addon WHERE guid = 75870;
-DELETE FROM creature_movement WHERE id = 75870;
-DELETE FROM game_event_creature WHERE guid = 75870;
-DELETE FROM game_event_creature_data WHERE guid = 75870;
-DELETE FROM creature_battleground WHERE guid = 75870;
-DELETE FROM creature_linking WHERE guid = 75870 
-OR master_guid = 75870;
+DELETE FROM creature WHERE guid = 75887;
+DELETE FROM creature_addon WHERE guid = 75887;
+DELETE FROM creature_movement WHERE id = 75887;
+DELETE FROM game_event_creature WHERE guid = 75887;
+DELETE FROM game_event_creature_data WHERE guid = 75887;
+DELETE FROM creature_battleground WHERE guid = 75887;
+DELETE FROM creature_linking WHERE guid = 75887 
+OR master_guid = 75887;
 INSERT INTO creature (guid, id, map, spawnmask, phasemask, modelid, equipment_id, position_x, position_y, position_z, orientation, spawntimesecsmin, spawntimesecsmax, spawndist, currentwaypoint, curhealth, curmana, DeathState, MovementType) values
-(75870,1380,0,1,1,0,0,-5760.73,-3437.71,305.54,2.41,300,300,0,0,247,0,0,0);
+(75887,1380,0,1,1,0,0,-5760.73,-3437.71,305.54,2.41,300,300,0,0,247,0,0,0);
 -- Stats & Equip updated
 UPDATE creature_template SET MinLevel = 12, MaxLevel = 12, MinLevelHealth = 247, MaxLevelHealth = 247, Armor = 562, MeleeBaseAttackTime = 2000, RangedBaseAttackTime = 2000, MeleeAttackPower = 42, MinMeleeDmg = 17, MaxMeleeDmg = 22.3, equipmentTemplateid = 375 WHERE entry = 1380;
 
