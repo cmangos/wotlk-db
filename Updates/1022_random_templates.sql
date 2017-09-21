aadc92c0de0ed63ec57d7871306cd86757cef272
Saltgurka <alexander.bertilsson2@gmail.com>
2017-09-21 22:21:50 +0100

    [1022][ACID][TBCDB] Add OOC emotes and texts for 17136, 17137, 20812
    Source: https://github.com/cmangos/tbc-db/commit/b03fc73451a19cc5ba89940ca98ca98130cbbd86

---
+++ Updates/1022_random_templates.sql
@@ -0,0 +1,8 @@
+-- Corki
+-- Random Say OOC
+DELETE FROM dbscript_random_templates WHERE id = 20;
+INSERT INTO dbscript_random_templates (id, type, target_id, chance) VALUES
+(20,0,-1707,0),
+(20,0,-1708,0),
+(20,0,-1709,0),
+(20,0,-1710,0);
