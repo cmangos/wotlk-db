a1ff7e317f77b3f18888bb7c37f728aa97244cf2
Saltgurka <alexander.bertilsson2@gmail.com>
2017-09-21 22:13:08 +0100

    [1019][ACID][TBCDB] Various OOC scripts for creatures in Shattrath Lower City
    Source: https://github.com/cmangos/tbc-db/commit/a232a904162f319738181c79e6c072f923a53fde

---
+++ Updates/1019_random_templates.sql
@@ -0,0 +1,10 @@
+DELETE FROM dbscript_random_templates WHERE id IN (18,19);
+INSERT INTO dbscript_random_templates (id, type, target_id, chance) VALUES
+-- The Rokk
+(18,0,-1701,0),
+(18,0,-1702,0),
+(18,0,-1703,0),
+-- Grisy Spicecrackle
+(19,0,-1704,0),
+(19,0,-1705,0),
+(19,0,-1706,0);
