0500cee8eb354aad59cfd143acf4a4063b816c20
MantisLord <sabinprosper@gmail.com>
2017-09-21 23:24:35 +0100

    [1040][ACID][TBCDB] Marshal Isildor (19308): Add one missing text and missing emotes
    Source: https://github.com/cmangos/tbc-db/commit/7a1e8fd9a7d2308f063e4b78961c20eeca7f8a57
    Source: https://github.com/cmangos/tbc-db/commit/c3f76cb7fee97eb3484af640b4f0b2714faedd20

---
+++ Updates/1040_random_templates.sql
@@ -0,0 +1,7 @@
+-- Marshal Isildor - Random texts
+DELETE FROM dbscript_random_templates WHERE id = 24;
+INSERT INTO dbscript_random_templates (id, type, target_id, chance, comments) VALUES
+(24,0,-334,0,'19308 - SAY OOC'),
+(24,0,-335,0,'19308 - SAY OOC'),
+(24,0,-336,0,'19308 - SAY OOC'),
+(24,0,-1731,0,'19308 - SAY OOC');
