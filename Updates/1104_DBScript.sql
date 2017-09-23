677e15611796215a1b2f6c6faffa1108dd250f26
Grz3s <grz3sp@wp.pl>
2017-09-23 18:07:48 +0100

    Update for [1104]
    thx to @MantisLord

--- Updates/1104_DBScript.sql
+++ Updates/1104_DBScript.sql
@@ -3,5 +3,4 @@
 DELETE FROM dbscripts_on_creature_movement WHERE id IN (1894401,1894402);
 
 -- Peon Overseer
--- will be added after confirm from @MantisLord
--- should fix error: Table `dbscripts_on_relay` uses nonexistent dbscript_random_template ID 0 in SCRIPT_COMMAND_START_RELAY_SCRIPT for script id 10100.
+UPDATE dbscripts_on_relay SET datalong2 = 10103, dataint = 0, dataint2 = 0 WHERE id = 10100 AND delay = 3;
