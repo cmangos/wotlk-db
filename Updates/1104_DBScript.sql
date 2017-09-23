05133b85fc5831ea229e61eb6249196293160c6c
Grz3s <grz3sp@wp.pl>
2017-09-23 17:14:27 +0100

    [1004] TBCDB Backport fixes

---
+++ Updates/1104_DBScript.sql
@@ -0,0 +1,7 @@
+-- Fel Soldier
+-- not req. anymore
+DELETE FROM dbscripts_on_creature_movement WHERE id IN (1894401,1894402);
+
+-- Peon Overseer
+-- will be added after confirm from @MantisLord
+-- should fix error: Table `dbscripts_on_relay` uses nonexistent dbscript_random_template ID 0 in SCRIPT_COMMAND_START_RELAY_SCRIPT for script id 10100.
