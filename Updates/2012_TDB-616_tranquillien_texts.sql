3602d25801a1dcb2247f8c650e080af2e010c6f2
Saltgurka <saltgurkasalt@gmail.com>
2017-09-25 20:34:49 +0200

    [2012][TDB][ACID] Add OOC texts to c.16289, c.16231, c.16252, c.16251
    * Based on this commit in TrinityCore: https://github.com/TrinityCore/TrinityCore/commit/7559d837b93a2d316a80c7d05f04e1b17676f975 by @Xyventh
    - Grz3s: text ids changed to 15k+ (TBCDB range neutral for both DBs)
    
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/b558a439fc30a2eeee95837fb3f96304cbc5c223

---
+++ Updates/2012_TDB-616_tranquillien_texts.sql
@@ -0,0 +1,130 @@
+DELETE FROM `dbscripts_on_relay` WHERE `id` BETWEEN 10035 AND 10040;
+INSERT INTO `dbscripts_on_relay` (`id`,`delay`,`command`,`datalong`,`datalong2`,`datalong3`,`dataint`,`dataint2`,`dataint3`,`dataint4`,`buddy_entry`,`search_radius`,`data_flags`,`comments`,`x`,`y`,`z`,`o`) VALUES
+(10035,0,0,0,0,0,2000015054,0,0,0,16289,7,3,'Advisor Valwyn - %s whispers something in the Dame''s ear.',0,0,0,0),
+(10035,2,0,0,0,0,2000015033,2000015034,2000015035,0,0,0,0,'Dame Auriferous - Random Text Emote',0,0,0,0),
+
+(10036,0,36,0,0,0,0,0,0,0,16252,5,1,'Dame Auriferous - Face High Executor Mavren',0,0,0,0),
+(10036,0,0,0,0,0,2000015036,0,0,0,0,0,0,'Dame Auriferous - Text 1',0,0,0,0),
+(10036,1,36,0,0,0,0,0,0,0,16252,5,3,'High Executor Mavren - Face Dame Auriferous',0,0,0,0),
+(10036,3,0,10007,0,0,0,0,0,0,16251,7,3,'Deathstalker Maltendis - Random Text 0',0,0,0,0),
+(10036,4,36,1,0,0,0,0,0,0,0,0,0,'Dame Auriferous - Reset Facing',0,0,0,0),
+(10036,12,0,0,0,0,2000015042,2000015043,2000015044,2000015045,16252,5,3,'High Executor Mavren - Random Text 0',0,0,0,0),
+(10036,16,0,10008,0,0,0,0,0,0,16289,7,3,'Advisor Valwyn - Random Text 1',0,0,0,0),
+(10036,20,36,1,0,0,0,0,0,0,16252,5,7,'High Executor Mavren - Reset Facing',0,0,0,0),
+(10036,27,0,0,0,0,2000015037,0,0,0,0,0,0,'Dame Auriferous - Text 2',0,0,0,0),
+
+(10037,0,36,0,0,0,0,0,0,0,16252,5,1,'Dame Auriferous - Face High Executor Mavren',0,0,0,0),
+(10037,0,0,0,0,0,2000015038,0,0,0,0,0,0,'Dame Auriferous - Text 3',0,0,0,0),
+(10037,1,36,0,0,0,0,0,0,0,16252,5,3,'High Executor Mavren - Face Dame Auriferous',0,0,0,0),
+(10037,3,0,10007,0,0,0,0,0,0,16251,7,3,'Deathstalker Maltendis - Random Text 0',0,0,0,0),
+(10037,4,36,1,0,0,0,0,0,0,0,0,0,'Dame Auriferous - Reset Facing',0,0,0,0),
+(10037,12,0,0,0,0,2000015046,2000015047,2000015048,2000015049,16252,5,3,'High Executor Mavren - Random Text 1',0,0,0,0),
+(10037,16,0,10008,0,0,0,0,0,0,16289,7,3,'Advisor Valwyn - Random Text 1',0,0,0,0),
+(10037,20,36,1,0,0,0,0,0,0,16252,5,7,'High Executor Mavren - Reset Facing',0,0,0,0),
+(10037,27,0,0,0,0,2000015039,0,0,0,0,0,0,'Dame Auriferous - Text 4',0,0,0,0),
+
+(10038,0,36,0,0,0,0,0,0,0,16252,5,1,'Dame Auriferous - Face High Executor Mavren',0,0,0,0),
+(10038,0,0,0,0,0,2000015040,0,0,0,0,0,0,'Dame Auriferous - Text 5',0,0,0,0),
+(10038,1,36,0,0,0,0,0,0,0,16252,5,3,'High Executor Mavren - Face Dame Auriferous',0,0,0,0),
+(10038,3,0,10007,0,0,0,0,0,0,16251,7,3,'Deathstalker Maltendis - Random Text 0',0,0,0,0),
+(10038,4,36,1,0,0,0,0,0,0,0,0,0,'Dame Auriferous - Reset Facing',0,0,0,0),
+(10038,12,0,0,0,0,2000015050,2000015051,2000015052,2000015053,16252,5,3,'High Executor Mavren - Random Text 2',0,0,0,0),
+(10038,16,0,10008,0,0,0,0,0,0,16289,7,3,'Advisor Valwyn - Random Text 1',0,0,0,0),
+(10038,20,36,1,0,0,0,0,0,0,16252,5,7,'High Executor Mavren - Reset Facing',0,0,0,0),
+(10038,27,0,0,0,0,2000015041,0,0,0,0,0,0,'Dame Auriferous - Text 6',0,0,0,0),
+
+(10039,0,36,0,0,0,0,0,0,0,16251,7,3,'Deathstalker Maltendis - Face Dame Auriferous',0,0,0,0),
+(10039,0,0,0,0,0,2000015072,0,0,0,16251,7,7,'Deathstalker Maltendis - %s turns to Advisor Valwyn and winks lasciviously in her direction.',0,0,0,0),
+(10039,7,45,10040,0,0,0,0,0,0,16289,13,7,'Advisor Valwyn - Start Relay Script',0,0,0,0),
+(10039,7,0,0,0,0,2000015063,0,0,0,16289,7,7,'Advisor Valwyn - %s looks disgusted at the deathstalker''s flirtations.',0,0,0,0),
+(10039,7,1,153,0,0,0,0,0,0,16251,7,7,'Deathstalker Maltendis - Emote Laugh',0,0,0,0),
+(10039,15,36,1,0,0,0,0,0,0,16289,7,7,'Advisor Valwyn - Reset Facing',0,0,0,0),
+(10039,17,36,1,0,0,0,0,0,0,16251,7,7,'Deathstalker Maltendis - Reset Facing',0,0,0,0),
+
+(10040,0,36,0,0,0,0,0,0,0,16251,13,1,'Advisor Valwyn - Face Deathstalker Maltendis',0,0,0,0);
+
+
+DELETE FROM `dbscript_string` WHERE `entry` BETWEEN 2000015033 AND 2000015072;
+INSERT INTO `dbscript_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`) VALUES 
+-- Dame Auriferous
+-- 0
+(2000015033, '%s nods her head yes at what her advisor has to say.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 273, 'Dame Auriferous (Entry: 16231)'),
+(2000015034, '%s disagrees with whatever it is that Valwyn has whispered to her.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 274, 'Dame Auriferous (Entry: 16231)'),
+(2000015035, '%s seems nonplussed by whatever it is that her advisor has whispered in her ear.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 6, 'Dame Auriferous (Entry: 16231)'),
+-- 1
+(2000015036, 'Executor, your direct assault approach will cost us more lives than I am comfortable with. A more nuanced strategy is called for; one that involves us reclaiming the surrounding territory, starting with the villages so as to cut off all points of reinforcement to Deatholme.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, 0, 'Dame Auriferous (Entry: 16231)'),
+-- 2
+(2000015037, 'I hear what you are saying, Mavren. Nevertheless, the final decision is mine. I appreciate your assistance, but the majority of the lives on the line are blood elf. I will not have those lives carelessly thrown away!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, 0, 'Dame Auriferous (Entry: 16231)'),
+-- 3
+(2000015038, 'How long until Forsaken reinforcements arrive? Our position here is tenuous. Your Lady promised us more soldiers. We must be able to concentrate solely on Deatholme and the Scourge!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, 0, 'Dame Auriferous (Entry: 16231)'),
+-- 4
+(2000015039, 'Mavren, I don''t want excuses, I want results! The Farstriders aren''t available and we''ve received all that we''re going to get from Silvermoon for now. I cannot ignore the Shadowpine trolls and Zul''Aman... they''re arrayed across our eastern border!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, 0, 'Dame Auriferous (Entry: 16231)'),
+-- 5
+(2000015040, 'No, I was right to begin with. Deatholme must come last. We must secure all of the Ghostlands first. I will not commit the forces here to a battle against Dar''khan with enemies to our flank and rear!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, 0, 'Dame Auriferous (Entry: 16231)'),
+-- 6
+(2000015041, 'Disagree with me all you like, High Executor. I will weigh your counsel, and then we will take the steps to free all of Quel''Thalas as I deem necessary. We shall continue this discussion anon.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, 0, 'Dame Auriferous (Entry: 16231)'),
+
+-- High Executor Mavren
+-- 0
+(2000015042, 'With all due respect, milady, you have not fought the Scourge as I have. The Lady appointed me to assist you in defeating Dar''khan because of that, and you would do well to consider what I have to say.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, 0, 'High Executor Mavren (Entry: 16252)'),
+(2000015043, 'I strongly disagree. Dar''khan is not going to get reinforcements because he''ll be bottlenecked. Deatholme has only one way in and out. Your traitor was a fool to corner himself there, and I intend to exploit that weakness!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, 0, 'High Executor Mavren (Entry: 16252)'),
+(2000015044, 'This is war, Dame Auriferous, and in any war lives will be lost! The only areas that we need to secure are the two ziggurats. We will turtle in and take the Tower of the Damned and its master by brute force. Then you will have your Quel''Thalas.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, 0, 'High Executor Mavren (Entry: 16252)'),
+(2000015045, 'And if your Farstriders were here to reinforce us, I might agree. But, they are busying themselves instead with the Shadowpine trolls on your eastern border. The longer we wait for them, the stronger the Scourge will become.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, 0, 'High Executor Mavren (Entry: 16252)'),
+-- 1
+(2000015046, 'I believe that you''re making my argument for me, but yes it would be nice if we had more reinforcements. I would counter that it would be faster to pull the Farstriders from their senseless skirmishing with the trolls.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, 0, 'High Executor Mavren (Entry: 16252)'),
+(2000015047, 'Dame Auriferous, we are the reinforcements. It may come to pass that The Lady will send more men in time. Better that you convince Silvermoon City to muster all of its forces and put them at our disposal here.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, 0, 'High Executor Mavren (Entry: 16252)'),
+(2000015048, 'You are correct; we must concentrate on the Scourge. Ignore the trolls and the villages, and focus our attentions on Deatholme! With Dar''khan defeated you will find that the rest of the Scourge ''body'' will fall quickly, lacking their ''head''.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, 0, 'High Executor Mavren (Entry: 16252)'),
+(2000015049, 'Forsaken reinforcements? Undercity is stretched thin with the Scourge on all sides. No, this will be a matter largely dealt with by the blood elves. We Forsaken are here as backup and advisors.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, 0, 'High Executor Mavren (Entry: 16252)'),
+-- 2
+(2000015050, 'Again I strongly disagree with your view of the strategic situation. Your plan will spread our forces too thin. It will leave us with only a small force to assault Deatholme. I cannot in good conscience execute such a strategy.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, 0, 'High Executor Mavren (Entry: 16252)'),
+(2000015051, 'Spread throughout the Ghostlands, our forces will be of little use in an assault on Deatholme. No, milady, I am here to kill Dar''khan and that is what I intend to do!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, 0, 'High Executor Mavren (Entry: 16252)'),
+(2000015052, 'We''ve been over this before. Only a focused assault upon Deatholme will meet with success. These other targets are distractions that we can ill afford. I suggest that you inform your Captain Helios that he is to leave off his campaign against the Shadowpine trolls and focus solely on the Scourge.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, 0, 'High Executor Mavren (Entry: 16252)'),
+(2000015053, 'Perhaps we should let the matter rest for the time being until your chef has prepared your meal? You look a bit piqued if you don''t mind my saying. We can resume our discussion when you are feeling more yourself.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, 0, 'High Executor Mavren (Entry: 16252)'),
+
+-- Advisor Valwyn
+-- 0
+(2000015054, '%s whispers something in the Dame''s ear.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, 'Advisor Valwyn (Entry: 16289)'),
+-- 1
+(2000015055, '%s looks appalled at the Executor''s tone!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, 'Advisor Valwyn (Entry: 16289)'),
+(2000015056, '%s tries to busy herself with other matters.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, 'Advisor Valwyn (Entry: 16289)'),
+(2000015057, '%s glares daggers in the direction of High Executor Mavren and Deathstalker Maltendis.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, 'Advisor Valwyn (Entry: 16289)'),
+(2000015058, '%s lifts her nose and sniffs in response to the High Executor''s reply.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, 'Advisor Valwyn (Entry: 16289)'),
+(2000015059, '%s shakes her head in disbelief at what she is hearing out of the Forsaken''s mouth.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, 'Advisor Valwyn (Entry: 16289)'),
+(2000015060, '%s blanches as much of the blood drains from her face.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, 'Advisor Valwyn (Entry: 16289)'),
+(2000015061, '%s reddens furiously at what she is hearing in response to her lady''s question.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, 'Advisor Valwyn (Entry: 16289)'),
+(2000015062, '%s concentrates on the wall opposite her, clearly ignoring the looks of the Executor and his assistant.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, 'Advisor Valwyn (Entry: 16289)'),
+-- 2
+(2000015063, '%s looks disgusted at the deathstalker''s flirtations.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 274, 'Advisor Valwyn (Entry: 16289)'),
+
+-- Deathstalker Maltendis
+-- 0
+(2000015064, '%s nods in agreement.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, 'Deathstalker Maltendis (Entry: 16251)'),
+(2000015065, '%s opens his mouth as if to add something, and then apparently thinks the better of it.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, 'Deathstalker Maltendis (Entry: 16251)'),
+(2000015066, '%s looks over at Advisor Valwyn with a smirk on his face.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, 'Deathstalker Maltendis (Entry: 16251)'),
+(2000015067, '%s grins at the High Executor''s words.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, 'Deathstalker Maltendis (Entry: 16251)'),
+(2000015068, '%s appears bored with the whole discussion.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, 'Deathstalker Maltendis (Entry: 16251)'),
+(2000015069, '%s winks slyly at Advisor Valwyn.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, 'Deathstalker Maltendis (Entry: 16251)'),
+(2000015070, '%s fidgets with his armor, clearly uncomfortable at the tone of the discussion.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, 'Deathstalker Maltendis (Entry: 16251)'),
+(2000015071, '%s looks like he''d rather be anywhere else but here.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, 'Deathstalker Maltendis (Entry: 16251)'),
+-- 1
+(2000015072, '%s turns to Advisor Valwyn and winks lasciviously in her direction.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 2, 'Deathstalker Maltendis (Entry: 16251)');
+
+DELETE FROM dbscript_random_templates WHERE id BETWEEN 10007 AND 10008;
+INSERT INTO dbscript_random_templates (id,type,target_id,chance,comments) VALUES
+(10007,0,2000015064,0,'Deathstalker Maltendis - Random Texts Group 0'),
+(10007,0,2000015065,0,'Deathstalker Maltendis - Random Texts Group 0'),
+(10007,0,2000015066,0,'Deathstalker Maltendis - Random Texts Group 0'),
+(10007,0,2000015067,0,'Deathstalker Maltendis - Random Texts Group 0'),
+(10007,0,2000015068,0,'Deathstalker Maltendis - Random Texts Group 0'),
+(10007,0,2000015069,0,'Deathstalker Maltendis - Random Texts Group 0'),
+(10007,0,2000015070,0,'Deathstalker Maltendis - Random Texts Group 0'),
+(10007,0,2000015071,0,'Deathstalker Maltendis - Random Texts Group 0'),
+
+(10008,0,2000015055,0,'Advisor Valwyn - Random Texts Group 1'),
+(10008,0,2000015056,0,'Advisor Valwyn - Random Texts Group 1'),
+(10008,0,2000015057,0,'Advisor Valwyn - Random Texts Group 1'),
+(10008,0,2000015058,0,'Advisor Valwyn - Random Texts Group 1'),
+(10008,0,2000015059,0,'Advisor Valwyn - Random Texts Group 1'),
+(10008,0,2000015060,0,'Advisor Valwyn - Random Texts Group 1'),
+(10008,0,2000015061,0,'Advisor Valwyn - Random Texts Group 1'),
+(10008,0,2000015062,0,'Advisor Valwyn - Random Texts Group 1');
