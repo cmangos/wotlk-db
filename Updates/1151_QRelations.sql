94054220be4e48ea7038a055568436b3097b844f
killerwife <killerwife@gmail.com>
2017-10-01 22:14:48 +0100

    [1151][TBCDB] q.8275 8276 8280 chaining fixes
    Source: https://github.com/cmangos/tbc-db/commit/d9b957f82918d6023e8cfa7c758d0d44270c3425

---
+++ Updates/1151_QRelations.sql
@@ -0,0 +1,4 @@
+-- q.8275 8276 8280 chaining fixes
+UPDATE `quest_template` SET `NextQuestId`='8280', `ExclusiveGroup`='8275' WHERE (`entry`='8275');
+UPDATE `quest_template` SET `NextQuestId`='8280', `ExclusiveGroup`='8275' WHERE (`entry`='8276');
+UPDATE `quest_template` SET `PrevQuestId`='0', `ExclusiveGroup`='8275' WHERE (`entry`='8280');
