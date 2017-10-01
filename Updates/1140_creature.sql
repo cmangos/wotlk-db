ddf283b913e61f0be4b16f72b1ce6ad45f612fc9
killerwife <killerwife@gmail.com>
2017-10-01 22:00:41 +0100

    [1140][TBCDB] Magtheridons lair tweaks
    Source: https://github.com/cmangos/tbc-db/commit/50762212a483f831a26f9504a669e0528d88d8cd

---
+++ Updates/1140_creature.sql
@@ -0,0 +1,4 @@
+-- make infernal immune to fire damage
+UPDATE creature_template SET SchoolImmuneMask= SchoolImmuneMask|4 WHERE entry = 17454;
+-- make magtheridon immune to taunt
+UPDATE creature_template SET ExtraFlags = ExtraFlags | 256 WHERE entry=17257;
