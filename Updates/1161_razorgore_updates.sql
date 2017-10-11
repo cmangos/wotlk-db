ba2189126ebb2342068340be0d0122c80f90a06e
cala <cala@users.noreply.github.com>
2017-10-11 16:50:20 +0200

    [1161] Few functional fixes for Razorgore encounter in BWL
    * Fixed triggers not respawning when Razorgore dies in Phase 1
    * Added missing target for spell 23024 (Fireball)
    * Prevented Grethok and its adds to respawn while Razorgore is dead

---
+++ Updates/1161_razorgore_updates.sql
@@ -0,0 +1,12 @@
+-- Few functional fixes for Razorgore encounter in BWL
+
+-- Fixed triggers not respawning when Razorgore dies in Phase 1
+UPDATE creature SET spawntimesecsmin = 10, spawntimesecsmax = 10 WHERE id IN (14449, 14459, 16604);
+
+-- Added missing target for spell 23024 (Fireball)
+DELETE FROM spell_script_target WHERE entry=23024;
+INSERT INTO spell_script_target VALUES
+(23024, 1, 14449, 0);
+
+-- Prevented Grethok and its adds to respawn while Razorgore is dead
+UPDATE creature_linking_template SET flag=flag+1024 WHERE master_entry IN (12557, 12435);
