8856918b22144aed5a0a444176cef9c46796719a
killerwife <killerwife@gmail.com>
2017-10-30 23:45:10 +0000

    [1186][TBCDB] Fix flags for last arcatraz bossfight NPCs
    Source: https://github.com/cmangos/tbc-db/commit/08a665b1c2d3de4948d48932583b7b7e1b6e2a57

---
+++ Updates/1186_creature.sql
@@ -0,0 +1,4 @@
+-- Fix flags for last arcatraz bossfight NPCs 
+UPDATE creature_template SET UnitFlags=33344 WHERE entry IN(20904,21622); -- warden melichar correct flags
+UPDATE creature_template SET UnitFlags=832 WHERE entry IN(20977,21602); -- millhouse manastor correct flags
+UPDATE creature_template SET UnitFlags=33600 WHERE entry IN(20912,21599); -- harbringer skyriss correct flags
