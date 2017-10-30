3d1043f90d7d731310fd245512012b438c17e938
Cala <calaftp@free.fr>
2017-10-30 23:19:51 +0000

    [1172][TBCDB] q.8487 script
    Source: https://github.com/cmangos/tbc-db/commit/5acdcd322188599da2a9ddaca3ee63f0fa1f9f56

---
+++ Updates/1172_quest_8487.sql
@@ -0,0 +1,13 @@
+-- q.8487 script
+UPDATE quest_template SET RewSpellCast=0,CompleteScript=8487 WHERE entry=8487;
+
+DELETE FROM dbscript_string WHERE entry=2000001433;
+INSERT INTO dbscript_string(entry,content_default) VALUES
+(2000001433,'Such... dark... energy! How can this be possible?');
+
+DELETE FROM dbscripts_on_quest_end WHERE id=8487;
+INSERT INTO dbscripts_on_quest_end VALUES
+(8487,0,29,2,0,0,0,0,0,0,0,0,0,0,0,0,'Corrupted Soil - Apprentice remove quest flag'),
+(8487,0,15,29535,0,0,0,0,0,0,0,0,0,0,0,0,'Corrupted Soil - Apprentice cast spell Test Soil'),
+(8487,2,0,0,0,0,0,0,2000001433,0,0,0,0,0,0,0,'Corrupted Soil - Apprentice say text'),
+(8487,10,29,2,0,0,0,0,0,0,0,0,0,0,0,0,'Corrupted Soil - Apprentice add quest flag');
