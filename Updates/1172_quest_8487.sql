d663e097de1c7db6265fc067c1978f919986ce97
Grz3s <grz3sp@wp.pl>
2017-10-31 00:03:49 +0000

    Backport Fixes

--- Updates/1172_quest_8487.sql
+++ Updates/1172_quest_8487.sql
@@ -6,7 +6,7 @@ INSERT INTO dbscript_string(entry,content_default) VALUES
 (2000001433,'Such... dark... energy! How can this be possible?');
 
 DELETE FROM dbscripts_on_quest_end WHERE id=8487;
-INSERT INTO dbscripts_on_quest_end VALUES
+INSERT INTO dbscripts_on_quest_end (id, delay, command, datalong, datalong2, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
 (8487,0,29,2,0,0,0,0,0,0,0,0,0,0,0,0,'Corrupted Soil - Apprentice remove quest flag'),
 (8487,0,15,29535,0,0,0,0,0,0,0,0,0,0,0,0,'Corrupted Soil - Apprentice cast spell Test Soil'),
 (8487,2,0,0,0,0,0,0,2000001433,0,0,0,0,0,0,0,'Corrupted Soil - Apprentice say text'),
