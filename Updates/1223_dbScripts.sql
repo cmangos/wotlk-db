49b893aaeeeeb70ca6ddbfb375a5591cb88d3b4b
Grz3s <grz3sp@wp.pl>
2017-12-04 19:19:24 +0000

    Second Update for [1223]
    thx to @NeatElves

--- Updates/1223_dbScripts.sql
+++ Updates/1223_dbScripts.sql
@@ -1,12 +1,12 @@
 -- TBC fields corrected for WOTLK
 -- UNIT_FIELD_FLAGS = (59-WoTLK) (46-TBC) 
-UPDATE dbscripts_on_creature_death SET datalong = 59 WHERE command IN (4,5) AND datalong = 46; 
-UPDATE dbscripts_on_creature_movement SET datalong = 59 WHERE command IN (4,5) AND datalong = 46;
-UPDATE dbscripts_on_event SET datalong = 59 WHERE command IN (4,5) AND datalong = 46;
-UPDATE dbscripts_on_go_use SET datalong = 59 WHERE command IN (4,5) AND datalong = 46;
-UPDATE dbscripts_on_go_template_use SET datalong = 59 WHERE command IN (4,5) AND datalong = 46;
-UPDATE dbscripts_on_gossip SET datalong = 59 WHERE command IN (4,5) AND datalong = 46;
-UPDATE dbscripts_on_quest_end SET datalong = 59 WHERE command IN (4,5) AND datalong = 46;
-UPDATE dbscripts_on_quest_start SET datalong = 59 WHERE command IN (4,5) AND datalong = 46;
-UPDATE dbscripts_on_spell SET datalong = 59 WHERE command IN (4,5) AND datalong = 46;
-UPDATE dbscripts_on_relay SET datalong = 59 WHERE command IN (4,5) AND datalong = 46;
+UPDATE dbscripts_on_creature_death SET datalong = 59 WHERE command IN (2,4,5) AND datalong IN (46,58); 
+UPDATE dbscripts_on_creature_movement SET datalong = 59 WHERE command IN (2,4,5) AND datalong IN (46,58); 
+UPDATE dbscripts_on_event SET datalong = 59 WHERE command IN (2,4,5) AND datalong IN (46,58); 
+UPDATE dbscripts_on_go_use SET datalong = 59 WHERE command IN (2,4,5) AND datalong IN (46,58); 
+UPDATE dbscripts_on_go_template_use SET datalong = 59 WHERE command IN (2,4,5) AND datalong IN (46,58); 
+UPDATE dbscripts_on_gossip SET datalong = 59 WHERE command IN (2,4,5) AND datalong IN (46,58); 
+UPDATE dbscripts_on_quest_end SET datalong = 59 WHERE command IN (2,4,5) AND datalong IN (46,58); 
+UPDATE dbscripts_on_quest_start SET datalong = 59 WHERE command IN (2,4,5) AND datalong IN (46,58); 
+UPDATE dbscripts_on_spell SET datalong = 59 WHERE command IN (2,4,5) AND datalong IN (46,58); 
+UPDATE dbscripts_on_relay SET datalong = 59 WHERE command IN (2,4,5) AND datalong IN (46,58); 
