6242099e91231c125fcddf16e1c0023c6701c0de
Grz3s <grz3sp@wp.pl>
2017-12-05 23:35:58 +0000

    Update for [1225]
    missing texts added
    hound will flee before desp.
    And ofc ... this is q.11253 not 11256 :)

--- Updates/1225_quest_11253.sql
+++ Updates/1225_quest_11253.sql
@@ -1,4 +1,4 @@
--- q.11256 'Sniff Out the Enemy'
+-- q.11253 'Sniff Out the Enemy'
 
 -- Missing added - WoTLKDB FREE guids used
 DELETE FROM gameobject WHERE guid = 15125;
@@ -12,7 +12,7 @@ UPDATE creature_template SET ExtraFlags = ExtraFlags|4096, MovementType = 2 WHER
 DELETE FROM `creature_movement_template` WHERE `entry` = 24156;
 INSERT INTO `creature_movement_template` (`entry`, `point`, `position_x`, `position_y`, `position_z`, `waittime`, `script_id`) VALUES
 (24156, 1, 1859.74, -6178.15, 24.3033, 0, 5),
-(24156, 2, 1866.06, -6172.81, 23.9216, 0, 0),
+(24156, 2, 1866.06, -6172.81, 23.9216, 2000, 2415601),
 (24156, 3, 1876.43, -6167.42, 23.7332, 0, 0),
 (24156, 4, 1903.37, -6167.9, 23.312, 0, 0),
 (24156, 5, 1934.43, -6168.46, 23.7148, 0, 0),
@@ -36,4 +36,15 @@ INSERT INTO `creature_movement_template` (`entry`, `point`, `position_x`, `posit
 (24156, 23, 2285.54, -6046.96, 1.6305, 0, 0),
 (24156, 24, 2298.05, -6041.56, 2.01465, 0, 0),
 (24156, 25, 2307.53, -6031.23, 2.93796, 0, 0),
-(24156, 26, 2309.46, -6024.45, 3.65369, 5000, 1);
+(24156, 26, 2309.46, -6024.45, 3.65369, 9000, 2415602),
+(24156, 27, 2288.56, -6045.34, 1.45586, 2000, 1);
+
+-- Script
+DELETE FROM dbscripts_on_creature_movement WHERE id IN (2415601,2415602);
+INSERT INTO dbscripts_on_creature_movement (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
+(2415601,0,0,0,0,0,0,0,0,2000001455,0,0,0,0,0,0,0,''),
+(2415602,2,0,0,0,0,0,0,0,2000001456,0,0,0,0,0,0,0,'');
+DELETE FROM dbscript_string WHERE entry IN (2000001455,2000001456);
+INSERT INTO dbscript_string (entry, content_default, sound, type, language, emote, comment) VALUES
+(2000001455,'The hound appears to pick up a scent from the north and prepares to sprint to its source.',0,2,0,0,''),
+(2000001456,'Upon reaching its destination the hound becomes startled by noise up ahead and appears ready to flee.',0,2,0,0,'');
