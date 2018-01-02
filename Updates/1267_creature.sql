d28ed9e2f5e70a2781a200f3af7460510139d084
Grz3s <grz3sp@wp.pl>
2018-01-02 21:25:58 +0000

    Update for [1270][1267] & [ACID]
    Regarding to q.11592
    - Force-Commander Steeljaw death text moved to AciD
    - entry link removed (we'll use guid links)
    - Respawntime reduced for Longrunner Proudhoof & Warsong Caravan Guards
    - so other players will wait only 60secs not 5 mins :)
    
    PLS ReRuN .InstallFullDB.sh  - to avoid conflicts.

--- Updates/1267_creature.sql
+++ Updates/1267_creature.sql
@@ -1,10 +1,2 @@
 -- Force-Commander Steeljaw
 UPDATE creature_template SET FactionAlliance = 21, FactionHorde = 21, MinLevel = 71, MaxLevel = 71, MinLevelHealth = 30196, MaxLevelHealth = 30196, Armor = 7018, MeleeBaseAttackTime = 2000, RangedBaseAttackTime = 2000, MeleeAttackPower = 308, MinMeleeDmg = 2241, MaxMeleeDmg = 3196 WHERE entry = 25359;
-
-DELETE FROM dbscripts_on_creature_death WHERE id = 25359;
-INSERT INTO dbscripts_on_creature_death (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
-(25359,0,0,0,0,0,0,0,0,2000001517,0,0,0,0,0,0,0,''),
-(25359,0,18,20000,0,0,0,0,0x04,0,0,0,0,0,0,0,0,'');
-DELETE FROM dbscript_string WHERE entry = 2000001517;
-INSERT INTO dbscript_string (entry, content_default, sound, type, language, emote, comment) VALUES 
-(2000001517,'I\'m freed! Thank you.',0,0,0,1,NULL);
--- Updates/1270_quest_11592.sql
+++ Updates/1270_quest_11592.sql
@@ -1,6 +1,6 @@
-
-
 -- Quest 11592
-DELETE FROM creature_linking_template WHERE entry IN (25338);
-INSERT INTO creature_linking_template (entry, map, master_entry, flag, search_range) VALUES
-(25338, 571, 25335, 1+2+128+512+8192, 0); -- FLAG_AGGRO_ON_AGGRO, FLAG_TO_AGGRO_ON_AGGRO, FLAG_RESPAWN_ON_RESPAWN, FLAG_FOLLOW, FLAG_DESPAWN_ON_DESPAWN
+-- Longrunner Proudhoof
+UPDATE creature SET spawntimesecsmin = 60, spawntimesecsmax = 60 WHERE id = 25335;
+
+-- Warsong Caravan Guard
+UPDATE creature SET spawntimesecsmin = 60, spawntimesecsmax = 60 WHERE id = 25338;
