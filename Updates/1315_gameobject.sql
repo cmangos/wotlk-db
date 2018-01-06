5a7c3f67d17ed75c3c82aa9869263e97d12d8943
killerwife <killerwife@gmail.com>
2018-01-06 15:33:05 +0000

    [1315][TBCDB] Fix Andorhal sign quaternion
    Source:
    
    https://github.com/cmangos/tbc-db/commit/7ede63a137b8bc14ce9d268e0fb1aa722684056b

---
+++ Updates/1315_gameobject.sql
@@ -0,0 +1,2 @@
+-- fix andorhal sign quaternion  
+UPDATE gameobject SET position_x = 452.427, position_y = -629.573, position_z = 166.348, orientation = 2.90598, rotation0 = 0.576802, rotation1 = -0.173584, rotation2 = 0.779713, rotation3 = 0.170927 WHERE guid = 30079;
