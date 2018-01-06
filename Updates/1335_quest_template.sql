33442241ea24a0b77fff2401330e7fff9aaa6fcd
Sneakynick <crhansen12@apu.edu>
2018-01-06 16:03:01 +0000

    [1335][TBCDB] Fix typos in quest text
    Source:
    
    https://github.com/cmangos/tbc-db/commit/03757c51934d0f977818204ce895a6e41cb1711a

---
+++ Updates/1335_quest_template.sql
@@ -0,0 +1,3 @@
+-- Fix typos in texts
+UPDATE `quest_template` SET `Objectives`='Deliver the Sealed Letter to Ag\'tor Bloodfist in Azshara.' WHERE `entry`='3504';
+UPDATE `quest_template` SET `OfferRewardText`='You say that you found it on one of the Scourge at Windrunner Spire and that there\'s an inscription on it? Let me see! Here, take this coin for a job well done.' WHERE `entry`='9175';
