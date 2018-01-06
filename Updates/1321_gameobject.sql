a9e5b77b704f96acee9a83762910e00c63ed0791
killerwife <killerwife@gmail.com>
2018-01-06 15:45:23 +0000

    [1321][TBCDB] Add proper quaternion for deep elem mine
    Source:
    
    https://github.com/cmangos/tbc-db/commit/9676917c5e8b95aaa7a533ae189f2650192e4796

---
+++ Updates/1321_gameobject.sql
@@ -0,0 +1,2 @@
+-- Deep Elem Mine proper quaternion  
+UPDATE gameobject SET position_x = 340.22, position_y = 1249.68, position_z = 80.8974, orientation = 2.76636, rotation0 = 0.68659, rotation1 = 0.001571, rotation2 = 0.72691, rotation3 = 0.013906 WHERE guid = 33326;
