f5cfeaabecf523112910257374d7117bb26352c0
MantisLord <sabinprosper@gmail.com>
2018-01-06 13:45:38 +0000

    [1279][TBCDB] Fix faction of creatures in Black Morass
    Source:
    
    https://github.com/cmangos/tbc-db/commit/29cf5dd8970c6ccbd97df3200c49b23b1439cc34

---
+++ Updates/1279_creature.sql
@@ -0,0 +1,4 @@
+-- Time Keeper (1)
+UPDATE creature_template SET FactionHorde=1718, FactionAlliance=1718 WHERE entry=20746;
+-- Infinite Dragonflight
+UPDATE creature_template SET FactionHorde=1720, FactionAlliance=1720 WHERE entry IN (17835,17838,17839,17879,17880,17881,17892,21818,21104,21697,21698,20740,20741,20744,22169,22170);
