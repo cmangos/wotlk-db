0b5a5a5b312ff1b9342cae0a0737cbc452201d01
MantisLord <sabinprosper@gmail.com>
2018-01-06 14:02:35 +0000

    [1290][TBCDB] Increase drop chance of Thunderlord Clan Artifact
    Source:
    
    https://github.com/cmangos/tbc-db/commit/84f2a6552a8f82efb2ad4947284a8f6096565918

---
+++ Updates/1290_drop.sql
@@ -0,0 +1,3 @@
+-- Thunderlord Clan Artifact should drop more often
+-- http://www.wowhead.com/item=30431/thunderlord-clan-artifact#comments:id=368675
+UPDATE creature_loot_template SET ChanceOrQuestChance=10 WHERE item=30431;
