74d784e2fab8601bfadc9fb91926e4aecc9106d1
AnonXS <AnonXS@users.noreply.github.com>
2018-02-16 20:51:56 +0000

    [1379][ACID][TBCDB] Corrupted Fire Elemental 21706
    Source:
    https://github.com/cmangos/tbc-db/commit/38965e2f5e73dff3af0ed37e8e6fb927fd860808

---
+++ Updates/1379_creature.sql
@@ -0,0 +1,4 @@
+-- Corrupted Fire Elemental - Shadowform
+DELETE FROM creature_template_addon WHERE entry = 21706;
+INSERT INTO creature_template_addon (entry, mount, bytes1, b2_0_sheath, b2_1_flags, emote, moveflags, auras) VALUES
+('21706', '0', '0', '1', '16', '0', '0', '35194');
