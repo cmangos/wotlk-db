5c498b5051fe33118abc0ea2b505d3580c97d973
Grz3s <grz3sp@wp.pl>
2018-02-20 20:48:47 +0000

    typo corrected in [1424]

--- Updates/1424_Borean_quests.sql
+++ Updates/1424_Borean_quests.sql
@@ -1,6 +1,6 @@
 -- q.11938 'Buying Some Time'
 -- q. item must be removed after complete
-UPDATE quest_template SET ReqSourceCount2 = 1 WHERE entry = 11909;
+UPDATE quest_template SET ReqSourceCount2 = 1 WHERE entry = 11938;
 
 -- q.11675 'A Proper Death'
 UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 1000, DetailsEmoteDelay3 = 1000, IncompleteEmote = 6, CompleteEmote = 1, OfferRewardEmote1 = 1, OfferRewardEmote2 = 1, OfferRewardEmoteDelay2 = 1000 WHERE entry = 11675;
