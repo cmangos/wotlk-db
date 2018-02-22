637b59c4a3ec3e919302a3b5abf53c717734a357
Grz3s <grz3sp@wp.pl>
2018-02-22 22:09:45 +0000

    [1429] Jenny
    stats
    
    PArt of:
    https://github.com/cmangos/wotlk-db/issues/213
    https://github.com/unified-db/Database/issues/89

---
+++ Updates/1429_creature.sql
@@ -0,0 +1,3 @@
+-- Jenny 
+-- Stats
+UPDATE creature_template SET Expansion = 2, UnitClass = 2, MinLevel = 70, MaxLevel = 70, MinLevelHealth = 6536, MaxLevelHealth = 6536, MinLevelMana = 3155, MaxLevelMana = 3155, Armor = 6719, MeleeBaseAttackTime = 2000, RangedBaseAttackTime = 2000, MeleeAttackPower = 286, MinMeleeDmg = 288, MaxMeleeDmg = 412 WHERE entry = 25969;
