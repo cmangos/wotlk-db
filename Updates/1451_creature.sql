8aec8671366096f7421c33cd8e4101c99ce6ede3
Grz3s <grz3sp@wp.pl>
2018-04-02 14:51:04 +0100

    [1451][ACID] Valiance Keep Rifleman
    corrected
    
    [ACID]
    Valiance Keep Rifleman

---
+++ Updates/1451_creature.sql
@@ -0,0 +1,4 @@
+-- Valiance Keep Rifleman
+DELETE FROM creature_template_addon WHERE entry = 25311;
+INSERT INTO creature_template_addon (entry,mount,bytes1,b2_0_sheath,b2_1_pvp_state,emote,moveflags,auras) VALUES
+(25311,0,0,2,1,214,0,NULL);
