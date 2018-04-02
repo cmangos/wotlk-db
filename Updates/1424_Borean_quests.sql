409ff0d44e5ea61bb6d869eb3ea1d6937ccb09f3
Grz3s <grz3sp@wp.pl>
2018-04-02 18:47:52 +0100

    [1453] Addons added for few Npcs
    - Medic Hawthorn
    - Wendy Darren
    - Gerald Green
    - Jeremiah Hawning
    
    Also more quest added in [1424]

--- Updates/1424_Borean_quests.sql
+++ Updates/1424_Borean_quests.sql
@@ -13,12 +13,20 @@
 -- q.12728 'Monitoring the Rift: Winterfin Cavern'
 UPDATE quest_template SET ReqSourceCount1 = 1 WHERE entry IN (11576,11582,11587,11590,11593,11648,11671,11677,11680,11919,11940,12728);
 
+-- q.11889 'Death From Above'
 -- q.11938 'Buying Some Time'
-UPDATE quest_template SET ReqSourceCount2 = 1 WHERE entry = 11938;
+UPDATE quest_template SET ReqSourceCount2 = 1 WHERE entry IN (11889,11938);
+
+-- q.11897 'Plug the Sinkholes'
+UPDATE quest_template SET ReqSourceCount3 = 1 WHERE entry = 11897;
 
 -- q.11610 'Leading the Ancestors Home'
+-- q.11794 'The Hunt is On'
 -- q.11892 'The Assassination of Harold Lane'
-UPDATE quest_template SET ReqSourceCount4 = 1 WHERE entry IN (11610,11892);
+UPDATE quest_template SET ReqSourceCount4 = 1 WHERE entry IN (11610,11794,11892);
+
+
+-- Emotes
 
 -- q.11630 'The Bad Earth'
 UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 1000, DetailsEmoteDelay3 = 1000, IncompleteEmote = 6, CompleteEmote = 1, OfferRewardEmote1 = 1, OfferRewardEmote2 = 1, OfferRewardEmoteDelay2 = 1000 WHERE entry = 11630;
@@ -26,11 +34,17 @@ UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 
 -- q.11640 'Words of Power'
 UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmoteDelay2 = 1000, IncompleteEmote = 6, CompleteEmote = 1, OfferRewardEmote1 = 1, OfferRewardEmote2 = 1, OfferRewardEmoteDelay2 = 1000 WHERE entry = 11640;
 
+-- q.11672 'Enlistment Day'
+UPDATE quest_template SET CompleteEmote = 66 WHERE entry = 11672;
+
 -- q.11675 'A Proper Death'
 UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 1000, DetailsEmoteDelay3 = 1000, IncompleteEmote = 6, CompleteEmote = 1, OfferRewardEmote1 = 1, OfferRewardEmote2 = 1, OfferRewardEmoteDelay2 = 1000 WHERE entry = 11675;
 
 -- q.11677 'Stop the Plague'
 UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 1000, DetailsEmoteDelay3 = 1000, IncompleteEmote = 6, CompleteEmote = 1, OfferRewardEmote1 = 1, OfferRewardEmote2 = 1, OfferRewardEmoteDelay2 = 1000 WHERE entry = 11677;
+
+-- q.11727 'A Time for Heroes'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmoteDelay2 = 1000 WHERE entry = 11727;
 
 -- q.11898 'Breaking Through'
 UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 1000, DetailsEmoteDelay3 = 1000, IncompleteEmote = 6, CompleteEmote = 1, OfferRewardEmote1 = 1, OfferRewardEmote2 = 1, OfferRewardEmoteDelay2 = 1000 WHERE entry = 11898;
