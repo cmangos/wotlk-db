0c780f8bab25a18d1fb78cad23088d101525c4e4
Grz3s <grz3sp@wp.pl>
2018-04-17 23:50:04 +0100

    [1460][ACID] Fizzcrank Airstrip - Borean Tundra
    Zone Redone
    
    Know issue:
    2 spawns and each should have diff gossip menu id ...
    CMANGOS is missing this feature (we going to set only top one to allow
    player enjoy this small event)
    Also:
    q.11723 'Deploy the Shake-n-Quake!' - added into [1424]
    
    [ACID]
    ELM General Purpose Bunny - update
    Defendo-tank 66D - update
    Fizzcrank Airstrip Survivor - added

--- Updates/1424_Borean_quests.sql
+++ Updates/1424_Borean_quests.sql
@@ -22,9 +22,10 @@ UPDATE quest_template SET ReqSourceCount1 = 1 WHERE entry IN (11576,11582,11587,
 -- q.12035 'Repurposed Technology'
 UPDATE quest_template SET ReqSourceCount2 = 1 WHERE entry IN (11889,11913,11938,12035);
 
+-- q.11723 'Deploy the Shake-n-Quake!'
 -- q.11728 'Lupus Pupus'
 -- q.11897 'Plug the Sinkholes'
-UPDATE quest_template SET ReqSourceCount3 = 1 WHERE entry IN (11728,11897);
+UPDATE quest_template SET ReqSourceCount3 = 1 WHERE entry IN (11723,11728,11897);
 
 -- q.11610 'Leading the Ancestors Home'
 -- q.11794 'The Hunt is On'
