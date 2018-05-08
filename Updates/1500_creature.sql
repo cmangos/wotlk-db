712dbd5dd8d52eed314c0ef468e6fbbd1fa97d5e
AnonXS <fetzi-d@web.de>
2018-05-08 10:35:57 +0100

    [1500][TBCDB] Adjust Damageschool for Ember of Azzinoth 23192
    Source:
    https://github.com/cmangos/tbc-db/commit/d8995691f104ad345ded76251ec95de9670b2e81

---
+++ Updates/1500_creature.sql
@@ -0,0 +1,3 @@
+-- Ember of Azzinoth - DamageSchool Fire http://www.wowhead.com/npc=23192/ember-of-azzinoth#comments:id=327761
+-- http://www.wowhead.com/npc=23192/ember-of-azzinoth#comments:id=741416 <- interesting comment about a supposed blizzlike mechanic
+UPDATE `creature_template` SET `DamageSchool` = 2 WHERE `entry` = 23192;
