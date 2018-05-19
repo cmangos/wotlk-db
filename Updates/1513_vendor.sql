80db45b3ca0cdd3ef91ad179b39f97fd5df891df
AnonXS <fetzi-d@web.de>
2018-05-19 23:02:13 +0100

    [1513][TBCDB] Remove Clam Meat 5503 & Hearthstone 6948 from npc_vendor(_template)
    Source:
    https://github.com/cmangos/tbc-db/commit/78c23c6d9666f1c10c05981764434beb7eaa028d

---
+++ Updates/1513_vendor.sql
@@ -0,0 +1,5 @@
+-- Remove wrong vendor data
+DELETE FROM `npc_vendor` WHERE `item` = 5503; -- Clam Meat
+
+DELETE FROM `npc_vendor` WHERE `item` = 6948; -- Hearthstone
+DELETE FROM `npc_vendor_template` WHERE `item` = 6948; -- Hearthstone
