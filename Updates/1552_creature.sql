e6b6ba43e4be72b404f021c8ce4f645546c6cd5b
X-Savior <xsavior@acid.com>
2018-06-15 23:17:51 +0100

    [1552][TBCDB] Warbringer Omrogg Updates
    Source:
    https://github.com/cmangos/tbc-db/commit/232fd3472de345c0743921b1db8f0740a230e86c

---
+++ Updates/1552_creature.sql
@@ -0,0 +1,5 @@
+-- =========================
+-- Warbringer O'mrogg Update
+-- ========================= 
+UPDATE `creature_template` SET `ExtraFlags`='256' WHERE `Entry`='16809';
+UPDATE `creature_template` SET `ExtraFlags`='256' WHERE `Entry`='20596';
