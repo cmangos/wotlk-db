ed1666e2408e8f0c7415c1ed1ca59657cbeeb9dc
AnonXS <fetzi-d@web.de>
2018-07-07 18:17:11 +0100

    [1592][ACID][TBCDB] Port Auras from ACID to creature_(template)addon & Cleanup
    Source:
    https://github.com/cmangos/tbc-db/commit/d7a777aa66a58993a1ff5adccb5e1419c44f5012

---
+++ Updates/1592_creature.sql
@@ -0,0 +1,5 @@
+-- Corrupted Healing Stream Totem 18235 & Corrupted Air Totem 18236
+UPDATE `creature_template` SET `MovementType` = 0, `AIName` = 'TotemAI' WHERE `entry` IN (18235,18236);
+INSERT INTO `creature_template_spells` (`entry`, `spell1`) VALUES
+(18235,32136),  
+(18236,32138); 
