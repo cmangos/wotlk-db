c208891d432de2f3c806544615aade656c4972dc
X-Savior <xsavior@acid.com>
2018-07-07 15:35:30 +0100

    [1587][TBCDB] Add Ignore MMAP Extraflag to Quagmirran c.17942
    Source:
    https://github.com/cmangos/tbc-db/commit/bc293c10974eed2baab7fc99cfa0d3379ade3264

---
+++ Updates/1587_creature.sql
@@ -0,0 +1,4 @@
+-- =======================================================================
+-- Add Ignore MMAP Extraflag to Quagmirran
+-- =======================================================================
+UPDATE creature_template SET ExtraFlags=ExtraFlags|0x00004000 WHERE entry IN (17942);
