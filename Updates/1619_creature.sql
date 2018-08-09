29231545c1904ca043e09d0c242f32d5019645cc
Grz3s <grz3sp@wp.pl>
2018-08-09 23:18:15 +0100

    [1619] Turgore - update
    movement and addon corrected

---
+++ Updates/1619_creature.sql
@@ -0,0 +1,3 @@
+-- Turgore
+UPDATE creature SET spawndist = 0, MovementType = 0 WHERE guid = 97576;
+UPDATE creature_template_addon SET bytes1 = 0, b2_1_pvp_state = 0, emote = 0, auras = 32951 WHERE entry = 23565;
