6c5c63def97180f6abc4593f62377e3ea84e98e9
AnonXS <fetzi-d@web.de>
2018-09-08 18:09:57 +0100

    [1665][TBCDB] Adjust Respawntimer for Scarshield Quartermaster 9046
    Source:
    https://github.com/cmangos/tbc-db/commit/271c0886fa758c4902eba531cb30b5637c506c0c

---
+++ Updates/1665_creature.sql
@@ -0,0 +1,2 @@
+-- Scarshield Quartermaster 9046 - http://www.wowhead.com/npc=9046/scarshield-quartermaster#comments:id=599352
+UPDATE `creature` SET `spawntimesecsmin` = 600, `spawntimesecsmax` = 900 WHERE `guid` = 248 AND `id` = 9046;
