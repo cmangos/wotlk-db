f62903e8d207e050f645e4fe7494c4ceb08875df
MantisLord <sabinprosper@gmail.com>
2018-09-09 01:35:56 +0100

    [1680][TBCDB] Add some data for Dragonmaw races
    Source:
    https://github.com/cmangos/tbc-db/commit/b3c946496edb776529ddccdc1885653c9a9b67ed

---
+++ Updates/1680_creature.sql
@@ -0,0 +1,6 @@
+-- Murg "Oldie" Muckjaw
+UPDATE creature_template SET UnitFlags=33024 WHERE entry=23340;
+-- Trope the Filth-Belcher
+UPDATE creature_template SET UnitFlags=33024 WHERE entry=23342;
+-- Dragonmaw Race: Trope's Target
+UPDATE creature_template SET UnitFlags=33554432, InhabitType=7 WHERE entry=23357;
