f8c08d854166a425ec195909ad7b7e32d3ed3f0d
killerwife <killerwife@gmail.com>
2018-09-09 03:35:03 +0100

    [1713][TBCDB] Add new updated threat string
    Sources:
    https://github.com/cmangos/tbc-db/commit/ad5a4ea280991817fdf1ed3abed0916a8d6c45f2
    https://github.com/cmangos/tbc-db/commit/64ee9bb1ffa128c809654b1bb3b2b90c315b4a98

---
+++ Updates/1713_mangos_string.sql
@@ -0,0 +1,5 @@
+-- Add new updated threat string
+UPDATE mangos_string SET content_default='Player |cffff0000%s|r [GUID: %u] has |cffff0000%f|r threat and taunt state %u' WHERE entry IN (1702);
+
+UPDATE mangos_string SET content_default='   Targeted to player %s (lowguid %u) distance %f angle %f' WHERE entry IN(532);
+UPDATE mangos_string SET content_default='   Targeted to creature %s (lowguid %u) distance %f angle %f' WHERE entry IN(533);
