85594892433112e31bc4e0e356003d6b63cded65
Xfurry <xfurry.cmangos@outlook.com>
2018-09-16 01:47:43 +0200

    [1721] Updates for Strand of the Ancients
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>

---
+++ Updates/1721_zone_strand_of_the_ancients.sql
@@ -0,0 +1,17 @@
+
+-- Sota updates
+-- Fix phase masks
+UPDATE gameobject SET phaseMask=128 WHERE id=190753;
+UPDATE gameobject SET phaseMask=64 WHERE id=194086;
+
+-- insert missing horde portals
+DELETE FROM gameobject WHERE guid IN (20592,20593,20594,20595,20596);
+INSERT INTO gameobject (guid,id,map,spawnMask,phaseMask,position_x,position_y,position_z,orientation,rotation0,rotation1,rotation2,rotation3,spawntimesecsmin,spawntimesecsmax,animprogress,state) VALUES
+(20592, 190763, 607, 3, 128, 1065.02, -89.9522, 81.0758, 1.58771, 0, 0, 0.713061, 0.701102, 800, 800, 0, 1),
+(20593, 190763, 607, 3, 128, 1255.73, -233.153, 56.4357, 5.01833, 0, 0, 0.591105, -0.806595, 800, 800, 0, 1),
+(20594, 190763, 607, 3, 128, 1216.12, 47.7665, 54.2785, 4.05465, 0, 0, 0.897588, -0.440835, 800, 800, 0, 1),
+(20595, 190763, 607, 3, 128, 1394.07, 72.3632, 31.0541, 0.818809, 0, 0, 0.398063, 0.917358, 800, 800, 0, 1),
+(20596, 190763, 607, 3, 128, 1468.12, -225.7, 30.8969, 5.68042, 0, 0, 0.296841, -0.954927, 800, 800, 0, 1);
+
+-- not battleground related
+UPDATE creature_template_addon SET auras = '55228 55230 55167 61179' WHERE entry=32520;
