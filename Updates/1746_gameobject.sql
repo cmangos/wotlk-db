ef3e5d7811c8032435609a637abf0c80d8fb7378
Grz3s <grz3sp@wp.pl>
2018-10-13 14:08:10 +0100

    [1746] Saga of the Twins
    duplicated object removed
    
    Part of:
    https://github.com/cmangos/wotlk-db/issues/230

---
+++ Updates/1746_gameobject.sql
@@ -0,0 +1,5 @@
+-- Saga of the Twins
+-- duplicate removed
+DELETE FROM gameobject WHERE guid = '15392';
+DELETE FROM game_event_gameobject WHERE guid = '15392';
+DELETE FROM gameobject_battleground WHERE guid = '15392';
