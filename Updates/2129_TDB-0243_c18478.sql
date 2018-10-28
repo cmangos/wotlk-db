539e56f683ed6ac1c6ab7262da8f36ee68b85977
anonxs <anonxs@web.de>
2018-10-28 16:50:40 +0100

    [2129][TDB] Fix Faction and UnitFlags for Avatar of the Martyred 18478,20303
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/e51bf3678c125c9fac5d9a7458925734349e6ee2

---
+++ Updates/2129_TDB-0243_c18478.sql
@@ -0,0 +1,4 @@
+-- Avatar of the Martyred 18478,20303
+UPDATE `creature_template` SET `Faction` = 16, `UnitFlags` = 32832 WHERE `entry` = 18478;
+UPDATE `creature_template` SET `Faction` = 16, `UnitFlags` = 32832 WHERE `entry` = 20303;
+
