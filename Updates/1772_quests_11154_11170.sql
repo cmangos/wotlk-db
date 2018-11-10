ac0fba6121b1c63182f3cf8611d31ac874c2476b
Grz3s <grz3sp@wp.pl>
2018-11-10 19:10:44 +0000

    [1772] q.11154 & q.11170
    q item must be removed after q. completed:
    - q.11154 'Scare the Guano Out of Them!'
    - q.11170 'Test at Sea'

---
+++ Updates/1772_quests_11154_11170.sql
@@ -0,0 +1,7 @@
+-- q.11154 'Scare the Guano Out of Them!'
+-- q item must be removed after q. completed
+UPDATE quest_template SET ReqSourceCount2 = 1 WHERE entry = 11154;
+
+-- q.11170 'Test at Sea'
+-- q item must be removed after q. completed
+UPDATE quest_template SET ReqSourceCount2 = 1 WHERE entry = 11170;
