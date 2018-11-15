b9f295accd4ca261d0ee11091d7da77472a6a649
Grz3s <grz3sp@wp.pl>
2018-11-15 22:36:41 +0000

    update for [1776]
    another object added

--- Updates/1776_gameobject_template.sql
+++ Updates/1776_gameobject_template.sql
@@ -1,3 +1,4 @@
-DELETE FROM `gameobject_template` WHERE entry = 195641;
+DELETE FROM `gameobject_template` WHERE entry IN (195641,176750);
 INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `faction`, `size`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`) VALUES
-(195641, 6, 0, 'Brazier', '', '', 0, 1, 0, 1, 1, 7897, 0, 0);
+(195641, 6, 0, 'Brazier', '', '', 0, 1, 0, 1, 1, 7897, 0, 0),
+(176750, 6, 327, 'Kodo Bones', '', '', 0, 1, 0, 0, 0, 17960, 0, 0);
