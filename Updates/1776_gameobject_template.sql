a0577d49a978dcd8d793ae7b087a9aeddd4a3241
Grz3s <grz3sp@wp.pl>
2018-11-15 21:47:08 +0000

    [1776] Brazier - 195641
    missing object added
    Thx to @Saltgurka
    
    Source:
    TrinityCore

---
+++ Updates/1776_gameobject_template.sql
@@ -0,0 +1,3 @@
+DELETE FROM `gameobject_template` WHERE entry = 195641;
+INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `faction`, `size`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`) VALUES
+(195641, 6, 0, 'Brazier', '', '', 0, 1, 0, 1, 1, 7897, 0, 0);
