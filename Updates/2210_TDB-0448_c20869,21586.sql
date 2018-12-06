150d511a4b7a021ac25658aa309363f7292119c8
anonxs <anonxs@web.de>
2018-12-06 23:54:21 +0100

    [2210][TDB] Correct Faction & UnitFlags for Arcatraz Sentinel 20869,21586
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/504a1b8e27f45de9b71465db305761e1e52b4f67

---
+++ Updates/2210_TDB-0448_c20869,21586.sql
@@ -0,0 +1,3 @@
+-- Arcatraz Sentinel 20869,21586
+UPDATE `creature_template` SET `Faction` = 16, `UnitFlags` = 64 WHERE `entry` = 20869;
+UPDATE `creature_template` SET `Faction` = 16, `UnitFlags` = 64 WHERE `entry` = 21586;
