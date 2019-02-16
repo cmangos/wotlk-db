4e6e0bbb253a79cbf8d2f2a87403efdc614fb991
Grz3s <grz3sp@wp.pl>
2019-02-16 01:41:56 +0000

    [1850] Enthralled Stone Giant 24346
    Stats

---
+++ Updates/1850_creature.sql
@@ -0,0 +1,2 @@
+-- Enthralled Stone Giant 24346
+UPDATE creature_template SET UnitClass = 2, Expansion = 2, MinLevel = 71, MaxLevel = 71, MinLevelHealth = 9291, MaxLevelHealth = 9291, MinLevelMana = 3231, MaxLevelMana = 3231, Armor = 7007, MeleeBaseAttackTime = 2000, RangedBaseAttackTime = 2000, MeleeAttackPower = 290, MinMeleeDmg = 296, MaxMeleeDmg = 424 WHERE entry = 24346;
