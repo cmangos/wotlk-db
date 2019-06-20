e30523ccde9404b1f930fce7186a0beb31119b7b
MantisLord <sabinprosper@gmail.com>
2019-06-20 10:40:03 -0400

    [TDB][SSC] Use creature_spawn_entry for Morogrim Tidewalker trash in SSC
    (cherry picked https://github.com/cmangos/tbc-db/commit/92cc4e5ad64237ef56f4135cc52c2ef1962a90cc

--- Updates/Instances/548_serpentshrine_cavern.sql
+++ Updates/Instances/548_serpentshrine_cavern.sql
@@ -699,11 +699,11 @@ REPLACE INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `b2_0_sheath
 
 INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES
 -- link "leaders" to bosses
-(@CGUID+63, @CGUID+38, 1024), -- Tidewalker Depth-Seer -> Morogrim Tidewalker
-(@CGUID+66, @CGUID+38, 1024), -- Tidewalker Depth-Seer -> Morogrim Tidewalker
-(@CGUID+69, @CGUID+38, 1024), -- Tidewalker Warrior -> Morogrim Tidewalker
-(@CGUID+70, @CGUID+38, 1024), -- Tidewalker Warrior -> Morogrim Tidewalker
-(@CGUID+72, @CGUID+38, 1024), -- Tidewalker Warrior -> Morogrim Tidewalker
+(@CGUID+63, @CGUID+38, 1024), -- Murloc Group 1 -> Morogrim Tidewalker
+(@CGUID+66, @CGUID+38, 1024), -- Murloc Group 2 -> Morogrim Tidewalker
+(@CGUID+69, @CGUID+38, 1024), -- Murloc Group 3 -> Morogrim Tidewalker
+(@CGUID+70, @CGUID+38, 1024), -- Murloc Group 4 -> Morogrim Tidewalker
+(@CGUID+72, @CGUID+38, 1024), -- Murloc Group 5 -> Morogrim Tidewalker
 (@CGUID+189, @CGUID+38, 1024), -- Coilfang Serpentguard -> Morogrim Tidewalker
 
 (@CGUID+127, @CGUID+39, 1024), -- Greyheart Shield-Bearer -> Fathom-Lord Karathress
@@ -817,32 +817,32 @@ INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES
 (@CGUID+221, @CGUID+62, 1167), -- Coilfang Hate-Screamer -> Coilfang Beast-Tamer
 (@CGUID+222, @CGUID+62, 1167), -- Coilfang Hate-Screamer -> Coilfang Beast-Tamer
 
-(@CGUID+67, @CGUID+63, 1679), -- Tidewalker Warrior -> Tidewalker Depth-Seer
-(@CGUID+68, @CGUID+63, 1679), -- Tidewalker Warrior -> Tidewalker Depth-Seer
-(@CGUID+76, @CGUID+63, 1679), -- Tidewalker Shaman -> Tidewalker Depth-Seer
-(@CGUID+83, @CGUID+63, 1679), -- Tidewalker Harpooner -> Tidewalker Depth-Seer
-(@CGUID+85, @CGUID+63, 1679), -- Tidewalker Hydromancer -> Tidewalker Depth-Seer
+(@CGUID+67, @CGUID+63, 1679), -- Murloc Group 1
+(@CGUID+68, @CGUID+63, 1679), -- Murloc Group 1
+(@CGUID+76, @CGUID+63, 1679), -- Murloc Group 1
+(@CGUID+83, @CGUID+63, 1679), -- Murloc Group 1
+(@CGUID+85, @CGUID+63, 1679), -- Murloc Group 1
 
-(@CGUID+74, @CGUID+66, 1679), -- Tidewalker Warrior -> Tidewalker Depth-Seer
-(@CGUID+75, @CGUID+66, 1679), -- Tidewalker Warrior -> Tidewalker Depth-Seer
-(@CGUID+81, @CGUID+66, 1679), -- Tidewalker Shaman -> Tidewalker Depth-Seer
+(@CGUID+74, @CGUID+66, 1679), -- Murloc Group 2
+(@CGUID+75, @CGUID+66, 1679), -- Murloc Group 2
+(@CGUID+81, @CGUID+66, 1679), -- Murloc Group 2
 
-(@CGUID+77, @CGUID+69, 1679), -- Tidewalker Shaman -> Tidewalker Warrior
-(@CGUID+78, @CGUID+69, 1679), -- Tidewalker Shaman -> Tidewalker Warrior
-(@CGUID+79, @CGUID+69, 1679), -- Tidewalker Shaman -> Tidewalker Warrior
-(@CGUID+86, @CGUID+69, 1679), -- Tidewalker Hydromancer -> Tidewalker Warrior
-(@CGUID+87, @CGUID+69, 1679), -- Tidewalker Hydromancer -> Tidewalker Warrior
+(@CGUID+77, @CGUID+69, 1679), -- Murloc Group 3
+(@CGUID+78, @CGUID+69, 1679), -- Murloc Group 3
+(@CGUID+79, @CGUID+69, 1679), -- Murloc Group 3
+(@CGUID+86, @CGUID+69, 1679), -- Murloc Group 3
+(@CGUID+87, @CGUID+69, 1679), -- Murloc Group 3
 
-(@CGUID+64, @CGUID+70, 1679), -- Tidewalker Depth-Seer -> Tidewalker Warrior
-(@CGUID+65, @CGUID+70, 1679), -- Tidewalker Depth-Seer -> Tidewalker Warrior
-(@CGUID+71, @CGUID+70, 1679), -- Tidewalker Warrior -> Tidewalker Warrior
-(@CGUID+84, @CGUID+70, 1679), -- Tidewalker Harpooner -> Tidewalker Warrior
-(@CGUID+88, @CGUID+70, 1679), -- Tidewalker Hydromancer -> Tidewalker Warrior
+(@CGUID+64, @CGUID+70, 1679), -- Murloc Group 4
+(@CGUID+65, @CGUID+70, 1679), -- Murloc Group 4
+(@CGUID+71, @CGUID+70, 1679), -- Murloc Group 4
+(@CGUID+84, @CGUID+70, 1679), -- Murloc Group 4
+(@CGUID+88, @CGUID+70, 1679), -- Murloc Group 4
 
-(@CGUID+73, @CGUID+72, 1679), -- Tidewalker Warrior -> Tidewalker Warrior
-(@CGUID+80, @CGUID+72, 1679), -- Tidewalker Shaman -> Tidewalker Warrior
-(@CGUID+82, @CGUID+72, 1679), -- Tidewalker Shaman -> Tidewalker Warrior
-(@CGUID+89, @CGUID+72, 1679), -- Tidewalker Hydromancer -> Tidewalker Warrior
+(@CGUID+73, @CGUID+72, 1679), -- Murloc Group 5
+(@CGUID+80, @CGUID+72, 1679), -- Murloc Group 5
+(@CGUID+82, @CGUID+72, 1679), -- Murloc Group 5
+(@CGUID+89, @CGUID+72, 1679), -- Murloc Group 5
 
 (@CGUID+115, @CGUID+94, 1167), -- Greyheart Nether-Mage -> Greyheart Tidecaller
 (@CGUID+133, @CGUID+94, 1167), -- Greyheart Skulker -> Greyheart Tidecaller
@@ -941,6 +941,36 @@ REPLACE INTO `creature_linking_template` (`entry`, `map`, `master_entry`, `flag`
 (22055, 548, 21212, 4096, 0), -- Coilfang Elite -> Lady Vashj
 (22056, 548, 21212, 4096, 0), -- Coilfang Strider -> Lady Vashj
 (22140, 548, 21212, 4096, 0); -- Toxic Spore Bat -> Lady Vashj
+
+INSERT INTO creature_spawn_entry (guid, entry) VALUES
+-- Random between Tidewalker Depth-Seer (21224), Tidewalker Warrior (21225), Tidewalker Shaman (21226), Tidewalker Harpooner (21227), and Tidewalker Hydromancer (21228)
+(@CGUID+63, 21224), (@CGUID+63, 21225), (@CGUID+63, 21226), (@CGUID+63, 21227), (@CGUID+63, 21228),
+(@CGUID+64, 21224), (@CGUID+64, 21225), (@CGUID+64, 21226), (@CGUID+64, 21227), (@CGUID+64, 21228),
+(@CGUID+65, 21224), (@CGUID+65, 21225), (@CGUID+65, 21226), (@CGUID+65, 21227), (@CGUID+65, 21228),
+(@CGUID+66, 21224), (@CGUID+66, 21225), (@CGUID+66, 21226), (@CGUID+66, 21227), (@CGUID+66, 21228),
+(@CGUID+67, 21224), (@CGUID+67, 21225), (@CGUID+67, 21226), (@CGUID+67, 21227), (@CGUID+67, 21228),
+(@CGUID+68, 21224), (@CGUID+68, 21225), (@CGUID+68, 21226), (@CGUID+68, 21227), (@CGUID+68, 21228),
+(@CGUID+69, 21224), (@CGUID+69, 21225), (@CGUID+69, 21226), (@CGUID+69, 21227), (@CGUID+69, 21228),
+(@CGUID+70, 21224), (@CGUID+70, 21225), (@CGUID+70, 21226), (@CGUID+70, 21227), (@CGUID+70, 21228),
+(@CGUID+71, 21224), (@CGUID+71, 21225), (@CGUID+71, 21226), (@CGUID+71, 21227), (@CGUID+71, 21228),
+(@CGUID+72, 21224), (@CGUID+72, 21225), (@CGUID+72, 21226), (@CGUID+72, 21227), (@CGUID+72, 21228),
+(@CGUID+73, 21224), (@CGUID+73, 21225), (@CGUID+73, 21226), (@CGUID+73, 21227), (@CGUID+73, 21228),
+(@CGUID+74, 21224), (@CGUID+74, 21225), (@CGUID+74, 21226), (@CGUID+74, 21227), (@CGUID+74, 21228),
+(@CGUID+75, 21224), (@CGUID+75, 21225), (@CGUID+75, 21226), (@CGUID+75, 21227), (@CGUID+75, 21228),
+(@CGUID+76, 21224), (@CGUID+76, 21225), (@CGUID+76, 21226), (@CGUID+76, 21227), (@CGUID+76, 21228),
+(@CGUID+77, 21224), (@CGUID+77, 21225), (@CGUID+77, 21226), (@CGUID+77, 21227), (@CGUID+77, 21228),
+(@CGUID+78, 21224), (@CGUID+78, 21225), (@CGUID+78, 21226), (@CGUID+78, 21227), (@CGUID+78, 21228),
+(@CGUID+79, 21224), (@CGUID+79, 21225), (@CGUID+79, 21226), (@CGUID+79, 21227), (@CGUID+79, 21228),
+(@CGUID+80, 21224), (@CGUID+80, 21225), (@CGUID+80, 21226), (@CGUID+80, 21227), (@CGUID+80, 21228),
+(@CGUID+81, 21224), (@CGUID+81, 21225), (@CGUID+81, 21226), (@CGUID+81, 21227), (@CGUID+81, 21228),
+(@CGUID+82, 21224), (@CGUID+82, 21225), (@CGUID+82, 21226), (@CGUID+82, 21227), (@CGUID+82, 21228),
+(@CGUID+83, 21224), (@CGUID+83, 21225), (@CGUID+83, 21226), (@CGUID+83, 21227), (@CGUID+83, 21228),
+(@CGUID+84, 21224), (@CGUID+84, 21225), (@CGUID+84, 21226), (@CGUID+84, 21227), (@CGUID+84, 21228),
+(@CGUID+85, 21224), (@CGUID+85, 21225), (@CGUID+85, 21226), (@CGUID+85, 21227), (@CGUID+85, 21228),
+(@CGUID+86, 21224), (@CGUID+86, 21225), (@CGUID+86, 21226), (@CGUID+86, 21227), (@CGUID+86, 21228),
+(@CGUID+87, 21224), (@CGUID+87, 21225), (@CGUID+87, 21226), (@CGUID+87, 21227), (@CGUID+87, 21228),
+(@CGUID+88, 21224), (@CGUID+88, 21225), (@CGUID+88, 21226), (@CGUID+88, 21227), (@CGUID+88, 21228),
+(@CGUID+89, 21224), (@CGUID+89, 21225), (@CGUID+89, 21226), (@CGUID+89, 21227), (@CGUID+89, 21228);
 
 INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `spawndist`, `currentwaypoint`, `DeathState`, `MovementType`) VALUES
 (@CGUID+1, 15384, 548, 1, 386.299, -433.73, 29.6121, 4.08407, 7200, 7200, 0, 0, 0, 0), -- OLDWorld Trigger (DO NOT DELETE)
