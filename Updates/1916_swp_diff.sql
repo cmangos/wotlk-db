a43bc1e021ecbb6089951497d61c6b147bbbd73f
anonxs <anonxs@web.de>
2019-08-24 22:32:57 +0200

    [1916] Update - Remove Unused Generic Script 1375

--- Updates/1916_swp_diff.sql
+++ Updates/1916_swp_diff.sql
@@ -9,6 +9,6 @@ DELETE FROM `dbscript_string` WHERE `entry` IN (
 DELETE FROM `dbscripts_on_creature_movement` WHERE `id` IN (1273,1375,1396);
 INSERT INTO `dbscripts_on_creature_movement` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES
 (1273, 0, 1, 273, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'GENERIC EMOTE - EMOTE_ONESHOT_YES'),
-(1375, 0, 1, 375, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'GENERIC EMOTE - EMOTE_STATE_READY2H'),
+-- (1375, 0, 1, 375, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'GENERIC EMOTE - EMOTE_STATE_READY2H'),
 (1396, 0, 1, 396, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'GENERIC EMOTE - EMOTE_ONESHOT_TALK_NO_SHEATHE');
 
