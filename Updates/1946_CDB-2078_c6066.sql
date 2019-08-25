6a73b86831d085e18c2476b99f6e6ea66486ea8b
anonxs <anonxs@web.de>
2019-08-25 19:07:37 +0200

    [1946] Update - Add correct spell for TotemAI NPC 6066

--- Updates/1946_CDB-2078_c6066.sql
+++ Updates/1946_CDB-2078_c6066.sql
@@ -1,4 +1,4 @@
 -- Earthgrab Totem 6066 - TotemAI
 UPDATE `creature_template` SET `AIName` = 'TotemAI' WHERE `entry` = 6066;
-REPLACE INTO `creature_template_spells` (`entry`, `spell1`) VALUES (6066, 8376);
+REPLACE INTO `creature_template_spells` (`entry`, `spell1`) VALUES (6066, 8378);
 
