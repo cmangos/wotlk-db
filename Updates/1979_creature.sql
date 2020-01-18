48ab22acf166560d731cb5d19646ba38b0382c2e
Grz3s <Grz3s@users.noreply.github.com>
2020-01-18 00:15:24 +0000

    [1979][ACID] Wyrmcaller Vile 24029 - Fixed
    - channeling stuck on aggro - fixed
    - stuck in air - corrected

---
+++ Updates/1979_creature.sql
@@ -0,0 +1,5 @@
+-- Wyrmcaller Vile 24029
+UPDATE creature SET position_x = 2826.445313, position_y = -3601.521240, position_z = 245.940674, orientation = 3.330130 WHERE guid = 88741;
+
+-- moved to wotlk 571x range
+UPDATE creature SET guid = 5712160 WHERE guid = 88741;
