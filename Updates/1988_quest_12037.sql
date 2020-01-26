f8b1b65c51e96d0579b8e915364bf66db014fc32
Grz3s <Grz3s@users.noreply.github.com>
2020-01-26 01:02:16 +0000

    [1988][ACID] q.12037 'Search and Rescue'
    fixed
    
    Closes:
    https://github.com/cmangos/wotlk-db/issues/257

---
+++ Updates/1988_quest_12037.sql
@@ -0,0 +1,4 @@
+-- q.12037 'Search and Rescue'
+DELETE FROM dbscripts_on_spell WHERE id IN (49960);
+INSERT INTO dbscripts_on_spell (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
+(49960,0,15,49958,0,0,0,0,0,49959,49952,0,0,0,0,0,0,'');
