80117920aa7fce3f48c5bf7a458faa357aeb23b0
Grz3s <Grz3s@users.noreply.github.com>
2020-01-29 21:05:44 +0000

    [1993] Added weapon for Kirtonos The Herald
    Based on CDB update: [1980] Added equipment for NPC 10506
    by: @Swater96
    - * Added weapon for Kirtonos The Herald Ref. https://www.youtube.com/watch?v=FkxkmDNnv3Y

---
+++ Updates/1993_equipment.sql
@@ -0,0 +1,4 @@
+-- Based on CDB update: [1980] Added equipment for NPC 10506
+-- by: @Swater96
+-- * Added weapon for Kirtonos The Herald Ref. https://www.youtube.com/watch?v=FkxkmDNnv3Y
+UPDATE creature_template SET equipmentTemplateid = 310 WHERE Entry = 10506;
