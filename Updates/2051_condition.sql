3fa96af15be6ad6b18086d153303de6897499165
Grz3s <Grz3s@users.noreply.github.com>
2020-03-01 22:48:36 +0000

    fix for [2051]

--- Updates/2051_condition.sql
+++ Updates/2051_condition.sql
@@ -2,4 +2,4 @@
 DELETE FROM `conditions` WHERE `condition_entry` BETWEEN 3169 AND 3170;
 INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`) VALUES 
 (3169, 9, 1886, 0),
-(3170, -1, 345, 3170);
+(3170, -1, 345, 3169);
