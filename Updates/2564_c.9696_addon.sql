1eb00a96ae7ae76f5ae2c9a1f5bf8ad8995bd95b
Grz3s <Grz3s@users.noreply.github.com>
2020-04-25 18:23:47 +0100

    [2564] Bloodaxe Worg - addon
    fixes:
    https://github.com/cmangos/issues/issues/2174

---
+++ Updates/2564_c.9696_addon.sql
@@ -0,0 +1,5 @@
+-- Bloodaxe Worg
+-- addon corrected
+DELETE FROM creature_template_addon WHERE entry = 9696;
+INSERT INTO creature_template_addon (entry, mount, bytes1, b2_0_sheath, b2_1_pvp_state, emote, moveflags, auras) VALUES 
+(9696,0,0,1,0,0,0,NULL);
