21af25ddd04f69e65d351ee3891efe201635374a
Grz3s <Grz3s@users.noreply.github.com>
2020-04-28 17:40:28 +0100

    [2633] D.E.H.T.A. Enforcer 25808
    all moved to wotlk guid range 570+ (570+ also used for Northrend)

---
+++ Updates/2633_c25808.sql
@@ -0,0 +1,16 @@
+-- D.E.H.T.A. Enforcer 25808 - Borean Tundra
+-- all moved to wotlk guid range 571+ (570+ also used for Northrend)
+
+-- Moved to WoTLK Range 570x+
+UPDATE creature SET guid = 5700000 WHERE guid = 132974;
+UPDATE creature_addon SET guid = 5700000 WHERE guid = 132974;
+UPDATE creature SET guid = 5700001 WHERE guid = 132975;
+UPDATE creature_addon SET guid = 5700001 WHERE guid = 132975;
+UPDATE creature SET guid = 5700002 WHERE guid = 132976;
+UPDATE creature_addon SET guid = 5700002 WHERE guid = 132976;
+UPDATE creature SET guid = 5700003 WHERE guid = 132977;
+UPDATE creature_addon SET guid = 5700003 WHERE guid = 132977;
+UPDATE creature SET guid = 5700004 WHERE guid = 132978;
+UPDATE creature_addon SET guid = 5700004 WHERE guid = 132978;
+UPDATE creature SET guid = 5700005 WHERE guid = 132979;
+UPDATE creature_addon SET guid = 5700005 WHERE guid = 132979;
