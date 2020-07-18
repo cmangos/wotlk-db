fdf73e2edf700e05a7049e1e12bcdc236d46c51e
Grz3s <Grz3s@users.noreply.github.com>
2020-07-19 00:09:49 +0100

    [2881] q.12095 'Marked for Death: High Cultist Zangus'

---
+++ Updates/2881_quest_12095.sql
@@ -0,0 +1,12 @@
+-- q.12095 'Marked for Death: High Cultist Zangus'
+-- becomes available after these 3 completed
+-- q.12089 'WANTED: Magister Keldonus'
+-- q.12090 'WANTED: Gigantaur'
+-- q.12091 'WANTED: Dreadtalon'
+DELETE FROM conditions WHERE condition_entry BETWEEN 20047 AND 20050;
+INSERT INTO conditions (condition_entry, type, value1, value2, value3, value4, flags) VALUES 
+(20047, 8, 12089, 0, 0, 0, 0),
+(20048, 8, 12090, 0, 0, 0, 0),
+(20049, 8, 12091, 0, 0, 0, 0),
+(20050, -1, 20049, 20048, 20047, 0, 0);
+UPDATE quest_template SET RequiredCondition = 20050 WHERE entry = 12095;
