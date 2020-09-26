269d55b3e7f8ff70a6ccbe9a3f266b14a82b842f
Xfurry <xfurry.cmangos@outlook.com>
2020-09-26 16:18:27 +0200

    [2986] BG Eye of the Storm Achievement criterias
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>

---
+++ Updates/2986_bg_eots_achievements.sql
@@ -0,0 +1,22 @@
+
+-- ** Achievements **
+
+-- Bloodthirsty Berserker - source aura 24378
+DELETE FROM achievement_criteria_requirement WHERE criteria_id IN (3684);
+INSERT INTO achievement_criteria_requirement VALUES
+(3684, 5, 24378, 0);
+
+-- Eye of the Storm domination - pvp script: hold all 4 bases
+DELETE FROM achievement_criteria_requirement WHERE criteria_id IN (1239);
+INSERT INTO achievement_criteria_requirement VALUES
+(1239, 22, 0, 0);
+
+-- Storm glory - pvp script:
+DELETE FROM achievement_criteria_requirement WHERE criteria_id IN (3693);
+INSERT INTO achievement_criteria_requirement VALUES
+(3693, 22, 0, 0);
+
+-- Take a chill pill - target aura 24378
+DELETE FROM achievement_criteria_requirement WHERE criteria_id IN (1258);
+INSERT INTO achievement_criteria_requirement VALUES
+(1258, 7, 24378, 0);
