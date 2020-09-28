f43ba22ba2872ba0e91adc607f234b95d88cc8ef
Grz3s <Grz3s@users.noreply.github.com>
2020-09-29 00:02:24 +0100

    typo

--- Updates/2994_Nozzlerust_Post_quest_relations.sql
+++ Updates/2994_Nozzlerust_Post_quest_relations.sql
@@ -23,7 +23,7 @@ UPDATE quest_template SET RequiredCondition = 20091 WHERE entry = 12052;
 -- q.12112 'Stiff Negotiations'
 -- becomes available after q.12050 'Lumber Hack' & q.12052 'Harp on This!' completed
 UPDATE quest_template SET PrevQuestId = 0 WHERE entry = 12112;
-DELETE FROM conditions WHERE condition_entry BETWEEN 20113 AND 20113;
+DELETE FROM conditions WHERE condition_entry BETWEEN 20113 AND 20115;
 INSERT INTO conditions (condition_entry, type, value1, value2) VALUES 
 (20113, 8, 12050, 0),
 (20114, 8, 12052, 0),
