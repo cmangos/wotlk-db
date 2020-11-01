1f4c3fc73921261814e63d47da4c236de243301b
Grz3s <Grz3s@users.noreply.github.com>
2020-11-01 23:44:15 +0000

    Update for [2994]
    conditions readded (wrongly reused by later updates)

--- Updates/2994_Nozzlerust_Post_quest_relations.sql
+++ Updates/2994_Nozzlerust_Post_quest_relations.sql
@@ -1,8 +1,8 @@
 -- q.12469 'Return to Sender' not available to players if q.12044 'Stocking Up' taken or completed - breadcrumb quests
-DELETE FROM conditions WHERE condition_entry = 20088;
+DELETE FROM conditions WHERE condition_entry = 20180;
 INSERT INTO conditions (condition_entry, type, value1, value2) VALUES 
-(20088, 22, 12044, 0);
-UPDATE quest_template SET RequiredCondition = 20088 WHERE entry = 12469;
+(20180, 22, 12044, 0);
+UPDATE quest_template SET RequiredCondition = 20180 WHERE entry = 12469;
 UPDATE quest_template SET PrevQuestId = 0 WHERE entry = 12044;
 
 -- q.12049 'Hard to Swallow'
@@ -12,13 +12,13 @@ UPDATE quest_template SET PrevQuestId = 12046 WHERE entry = 12049;
 -- q.12050 'Lumber Hack'
 -- q.12052 'Harp on This!'
 -- becomes available after q.12046 'Soft Packaging' & q.12047 'Something That Doesn't Melt' completed
-DELETE FROM conditions WHERE condition_entry BETWEEN 20089 AND 20091;
+DELETE FROM conditions WHERE condition_entry BETWEEN 20181 AND 20183;
 INSERT INTO conditions (condition_entry, type, value1, value2) VALUES 
-(20089, 8, 12046, 0),
-(20090, 8, 12047, 0),
-(20091, -1, 20090, 20089);
-UPDATE quest_template SET RequiredCondition = 20091 WHERE entry = 12050;
-UPDATE quest_template SET RequiredCondition = 20091 WHERE entry = 12052;
+(20181, 8, 12046, 0),
+(20182, 8, 12047, 0),
+(20183, -1, 20182, 20181);
+UPDATE quest_template SET RequiredCondition = 20183 WHERE entry = 12050;
+UPDATE quest_template SET RequiredCondition = 20183 WHERE entry = 12052;
 
 -- q.12112 'Stiff Negotiations'
 -- becomes available after q.12050 'Lumber Hack' & q.12052 'Harp on This!' completed
