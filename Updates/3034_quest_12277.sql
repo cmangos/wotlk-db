69f505b5a81f1135d7a25b2065212d792e255f55
Grz3s <Grz3s@users.noreply.github.com>
2020-11-11 20:19:19 +0000

    Update for [3034][ACID]
    relay corrected
    Thx to: @NeatElves
    ref:
    https://github.com/cmangos/wotlk-db/commit/3f4562d625b48d0b4e6de11bfdd5de986b03007b#commitcomment-44091830

--- Updates/3034_quest_12277.sql
+++ Updates/3034_quest_12277.sql
@@ -1,16 +1,16 @@
 -- q.12277 'Leave Nothing to Chance'
 -- Wintergarde Mine Bomb 27435
 UPDATE creature_template SET Expansion = 2, MinLevel = 71, MaxLevel = 73, MinLevelHealth = 9291, MaxLevelHealth = 9940, UnitFlags = 0 WHERE entry = 27435;
-DELETE FROM dbscripts_on_relay WHERE id IN (20390);
+DELETE FROM dbscripts_on_relay WHERE id IN (20450);
 INSERT INTO dbscripts_on_relay (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
-(20390,10,0,0,0,0,0,0,0,2000001041,0,0,0,0,0,0,0,'Part of Wintergarde Mine Bomb EAI: say ready'),
-(20390,2000,0,0,0,0,0,0,0,2000001042,0,0,0,0,0,0,0,'Part of Wintergarde Mine Bomb EAI: say 1'),
-(20390,3000,0,0,0,0,0,0,0,2000001043,0,0,0,0,0,0,0,'Part of Wintergarde Mine Bomb EAI: say 2'),
-(20390,4000,0,0,0,0,0,0,0,2000001044,0,0,0,0,0,0,0,'Part of Wintergarde Mine Bomb EAI: say 3'),
-(20390,5000,0,0,0,0,0,0,0,2000001045,0,0,0,0,0,0,0,'Part of Wintergarde Mine Bomb EAI: say 4'),
-(20390,6000,0,0,0,0,0,0,0,2000001046,0,0,0,0,0,0,0,'Part of Wintergarde Mine Bomb EAI: say 5'),
-(20390,7000,15,48742,0,0,0,0,0,0,0,0,0,0,0,0,0,'Part of Wintergarde Mine Bomb EAI: Cast 48742'),
-(20390,8000,18,0,0,0,0,0,0x04,0,0,0,0,0,0,0,0,'Part of Wintergarde Mine Bomb EAI: desp');
+(20450,10,0,0,0,0,0,0,0,2000001041,0,0,0,0,0,0,0,'Part of Wintergarde Mine Bomb EAI: say ready'),
+(20450,2000,0,0,0,0,0,0,0,2000001042,0,0,0,0,0,0,0,'Part of Wintergarde Mine Bomb EAI: say 1'),
+(20450,3000,0,0,0,0,0,0,0,2000001043,0,0,0,0,0,0,0,'Part of Wintergarde Mine Bomb EAI: say 2'),
+(20450,4000,0,0,0,0,0,0,0,2000001044,0,0,0,0,0,0,0,'Part of Wintergarde Mine Bomb EAI: say 3'),
+(20450,5000,0,0,0,0,0,0,0,2000001045,0,0,0,0,0,0,0,'Part of Wintergarde Mine Bomb EAI: say 4'),
+(20450,6000,0,0,0,0,0,0,0,2000001046,0,0,0,0,0,0,0,'Part of Wintergarde Mine Bomb EAI: say 5'),
+(20450,7000,15,48742,0,0,0,0,0,0,0,0,0,0,0,0,0,'Part of Wintergarde Mine Bomb EAI: Cast 48742'),
+(20450,8000,18,0,0,0,0,0,0x04,0,0,0,0,0,0,0,0,'Part of Wintergarde Mine Bomb EAI: desp');
 
 -- Wintergarde Mine Cave In (2) 188712
 UPDATE gameobject SET spawntimesecsmin = 60, spawntimesecsmax = 60, animprogress = 255, state = 0 WHERE id = 188712;
