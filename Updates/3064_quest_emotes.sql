8364ddfb62fe937eddadda35c585863b84c287ed
Grz3s <Grz3s@users.noreply.github.com>
2020-11-13 12:30:23 +0000

    [3064] quest emotes - Dragonblight
    Lots of  Missing quest emotes added in Dragonblight

---
+++ Updates/3064_quest_emotes.sql
@@ -0,0 +1,160 @@
+-- Lots of  Missing quest emotes - Dragonblight
+
+-- q.12102 'In Search of the Ruby Lilac'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 6, CompleteEmote = 6, OfferRewardEmote1 = 6, OfferRewardEmote2 = 1, OfferRewardEmoteDelay2 = 500 WHERE entry = 12102;
+
+-- q.12104 'Return to Soar'
+UPDATE quest_template SET DetailsEmote1 = 25, DetailsEmote2 = 1, DetailsEmoteDelay2 = 500, IncompleteEmote = 6, CompleteEmote = 1, OfferRewardEmote1 = 1 WHERE entry = 12104;
+
+-- q.12111 'Where the Wild Things Roam'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmoteDelay2 = 500, OfferRewardEmote1 = 1 WHERE entry = 12111;
+
+-- q.12048 'Scourge Armaments'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 5, CompleteEmote = 5, OfferRewardEmote1 = 1 WHERE entry = 12048;
+
+-- q.12057 'The Flesh-Bound Tome'
+UPDATE quest_template SET OfferRewardEmote1 = 1, OfferRewardEmote2 = 1, OfferRewardEmoteDelay2 = 500 WHERE entry = 12057;
+
+-- q.12115 'Koltira and the Language of Death'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 25, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, CompleteEmote = 25, OfferRewardEmote1 = 1, OfferRewardEmote2 = 1, OfferRewardEmoteDelay2 = 500 WHERE entry = 12115;
+
+-- q.12125 'In Service of Blood'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 5, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 5, CompleteEmote = 5, OfferRewardEmote1 = 1 WHERE entry = 12125;
+
+-- q.12127 'In Service of Frost'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 5, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 5, CompleteEmote = 5, OfferRewardEmote1 = 1 WHERE entry = 12127;
+
+-- q.12126 'In Service of Unholy'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 5, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 5, CompleteEmote = 5, OfferRewardEmote1 = 1 WHERE entry = 12126;
+
+-- q.12064 'Chains of the Anub\'ar'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmoteDelay2 = 500, IncompleteEmote = 5, CompleteEmote = 5, OfferRewardEmote1 = 1 WHERE entry = 12064;
+
+-- q.12069 'Return of the High Chief'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmoteDelay2 = 500, IncompleteEmote = 6, CompleteEmote = 6, OfferRewardEmote1 = 1 WHERE entry = 12069;
+
+-- q.12132 'The Power to Destroy'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 1, CompleteEmote = 1, OfferRewardEmote1 = 1 WHERE entry = 12132;
+
+-- q.12140 'All Hail Roanauk!'
+UPDATE quest_template SET CompleteEmote = 1, OfferRewardEmote1 = 1 WHERE entry = 12140;
+
+-- q.12136 'The Translated Tome'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 2, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 1, CompleteEmote = 6, OfferRewardEmote1 = 1, OfferRewardEmote2 = 5, OfferRewardEmote3 = 1, OfferRewardEmoteDelay2 = 500, OfferRewardEmoteDelay3 = 500 WHERE entry = 12136;
+
+-- q.12053 'The Might of the Horde'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, CompleteEmote = 1, OfferRewardEmote1 = 1 WHERE entry = 12053;
+
+-- q.12071 'Attack by Air!'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmoteDelay2 = 500, OfferRewardEmote1 = 66 WHERE entry = 12071;
+
+-- q.12072 'Blightbeasts be Damned!'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, OfferRewardEmote1 = 66, OfferRewardEmote2 = 1, OfferRewardEmoteDelay2 = 500 WHERE entry = 12072;
+
+-- q.12767 'Speak with your Ambassador'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, OfferRewardEmote1 = 1, OfferRewardEmote2 = 1, OfferRewardEmoteDelay2 = 500 WHERE entry = 12767;
+
+-- q.12461 'Report to the Ruby Dragonshrine'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, OfferRewardEmote1 = 6, OfferRewardEmote2 = 1, OfferRewardEmoteDelay2 = 500 WHERE entry = 12461;
+
+-- q.12095 'To Dragon's Fall'
+UPDATE quest_template SET OfferRewardEmote1 = 6 WHERE entry = 12095;
+
+-- q.12097 'Sarathstra, Scourge of the North'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmoteDelay2 = 500, OfferRewardEmote1 = 4 WHERE entry = 12097;
+
+-- q.12145 'Canyon Chase'
+UPDATE quest_template SET DetailsEmote1 = 1, OfferRewardEmote1 = 1 WHERE entry = 12145;
+
+-- q.12147 'Disturbing Implications'
+UPDATE quest_template SET CompleteEmote = 6, OfferRewardEmote1 = 1 WHERE entry = 12147;
+
+-- q.13343 'Mystery of the Infinite, Redux'
+UPDATE quest_template SET DetailsEmote1 = 6, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, CompleteEmote = 1, OfferRewardEmote1 = 1 WHERE entry = 13343;
+
+-- q.12303 'Funding the War Effort'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 6, CompleteEmote = 1, OfferRewardEmote1 = 1, OfferRewardEmote2 = 1, OfferRewardEmoteDelay2 = 500 WHERE entry = 12303;
+
+-- q.12209 'Materiel Plunder'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 6, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, CompleteEmote = 1, OfferRewardEmote1 = 1, OfferRewardEmote2 = 1, OfferRewardEmoteDelay2 = 500 WHERE entry = 12209;
+
+-- q.12211 'Let Them Not Rise!'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, DetailsEmote1 = 1, OfferRewardEmote1 = 1 WHERE entry = 12211;
+
+-- q.12205 'WANTED: The Scarlet Onslaught'
+UPDATE quest_template SET OfferRewardEmote1 = 1, OfferRewardEmote2 = 1, OfferRewardEmoteDelay2 = 500 WHERE entry = 12205;
+
+-- q.12230 'Stealing from the Siegesmiths'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 6, CompleteEmote = 1, OfferRewardEmote1 = 1, OfferRewardEmote2 = 11, OfferRewardEmoteDelay2 = 500 WHERE entry = 12230;
+
+-- q.12214 'Fresh Remounts'
+UPDATE quest_template SET DetailsEmote1 = 3, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 1, CompleteEmote = 1, OfferRewardEmote1 = 1, OfferRewardEmote2 = 1, OfferRewardEmoteDelay2 = 500 WHERE entry = 12214;
+
+-- q.12240 'A Means to an End'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 6, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 1, CompleteEmote = 1, OfferRewardEmote1 = 1, OfferRewardEmote2 = 1, OfferRewardEmoteDelay2 = 500 WHERE entry = 12240;
+
+-- q.12232 'Bombard the Ballistae'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 1, OfferRewardEmote1 = 4, OfferRewardEmote2 = 1, OfferRewardEmote3 = 1, OfferRewardEmoteDelay2 = 500, OfferRewardEmoteDelay3 = 500 WHERE entry = 12232;
+
+-- q.12243 'Fire Upon the Waters'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 5, DetailsEmote3 = 11, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 1, CompleteEmote = 1, OfferRewardEmote1 = 11, OfferRewardEmote2 = 1, OfferRewardEmote3 = 4, OfferRewardEmoteDelay2 = 500, OfferRewardEmoteDelay3 = 500 WHERE entry = 12243;
+
+-- q.12245 'No Mercy for the Captured'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 1, CompleteEmote = 1, OfferRewardEmote1 = 1, OfferRewardEmote2 = 1, OfferRewardEmoteDelay2 = 500 WHERE entry = 12245;
+
+-- q.12252 'Torture the Torturer'
+UPDATE quest_template SET DetailsEmote1 = 5, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 1, CompleteEmote = 1, OfferRewardEmote1 = 6, OfferRewardEmote2 = 1,  OfferRewardEmote3 = 1, OfferRewardEmoteDelay2 = 500, OfferRewardEmoteDelay3 = 500 WHERE entry = 12252;
+
+-- q.12271 'The Rod of Compulsion'
+UPDATE quest_template SET CompleteEmote = 6, OfferRewardEmote1 = 1, OfferRewardEmote2 = 1,  OfferRewardEmote3 = 1, OfferRewardEmoteDelay2 = 500, OfferRewardEmoteDelay3 = 500 WHERE entry = 12271;
+
+-- q.12273 'The Denouncement'
+UPDATE quest_template SET DetailsEmote1 = 5, DetailsEmote2 = 25, DetailsEmote3 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 1, CompleteEmote = 1, OfferRewardEmote1 = 4, OfferRewardEmote2 = 1,  OfferRewardEmote3 = 25, OfferRewardEmoteDelay2 = 500, OfferRewardEmoteDelay3 = 500 WHERE entry = 12273;
+
+-- q.12239 'The Spy in New Hearthglen'
+UPDATE quest_template SET CompleteEmote = 1, OfferRewardEmote1 = 6, OfferRewardEmote2 = 1, OfferRewardEmoteDelay2 = 500 WHERE entry = 12239;
+
+-- q.12254 'Without a Prayer'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 1, CompleteEmote = 6, OfferRewardEmote1 = 6, OfferRewardEmote2 = 5,  OfferRewardEmote3 = 1, OfferRewardEmoteDelay2 = 500, OfferRewardEmoteDelay3 = 500 WHERE entry = 12254;
+
+-- q.12260 'The Perfect Dissemblance'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 5, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 1, CompleteEmote = 5, OfferRewardEmote1 = 1 WHERE entry = 12260;
+
+-- q.12274 'A Fall From Grace'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 5, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 6, OfferRewardEmote1 = 5, OfferRewardEmote2 = 6,  OfferRewardEmote3 = 1, OfferRewardEmoteDelay2 = 500, OfferRewardEmoteDelay3 = 500 WHERE entry = 12274;
+
+-- q.12283 'The Truth Will Out'
+UPDATE quest_template SET DetailsEmote1 = 5, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 1, CompleteEmote = 6, OfferRewardEmote1 = 1 WHERE entry = 12283;
+
+-- q.12285 'Do Unto Others'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 25, DetailsEmote3 = 5, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 1, CompleteEmote = 6, OfferRewardEmote1 = 11, OfferRewardEmote2 = 1,  OfferRewardEmote3 = 4, OfferRewardEmoteDelay2 = 500, OfferRewardEmoteDelay3 = 500 WHERE entry = 12285;
+
+-- q.12189 'Imbeciles Abound!'
+UPDATE quest_template SET OfferRewardEmote1 = 1, OfferRewardEmote2 = 1, OfferRewardEmoteDelay2 = 500 WHERE entry = 12189;
+
+-- q.12188 'The Forsaken Blight and You: How Not to Die'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmote4 = 11, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 1, CompleteEmote = 5, OfferRewardEmote1 = 2, OfferRewardEmote2 = 2,  OfferRewardEmote3 = 1, OfferRewardEmoteDelay2 = 500, OfferRewardEmoteDelay3 = 500 WHERE entry = 12188;
+
+-- q.12200 'Emerald Dragon Tears'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 1, CompleteEmote = 5, OfferRewardEmote1 = 1, OfferRewardEmote2 = 1,  OfferRewardEmote3 = 1, OfferRewardEmote4 = 6, OfferRewardEmoteDelay2 = 500, OfferRewardEmoteDelay3 = 500 WHERE entry = 12200;
+
+-- q.12218 'Spread the Good Word'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 1, CompleteEmote = 5, OfferRewardEmote1 = 1, OfferRewardEmote2 = 5 WHERE entry = 12218;
+
+-- q.12221 'The Forsaken Blight'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmoteDelay2 = 500, IncompleteEmote = 1, CompleteEmote = 5, OfferRewardEmote1 = 1 WHERE entry = 12221;
+
+-- q.12224 'The Kor'kron Vanguard!'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 1, DetailsEmote4 = 1, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 1, CompleteEmote = 1, OfferRewardEmote1 = 1, OfferRewardEmote2 = 1, OfferRewardEmote3 = 1, OfferRewardEmoteDelay2 = 500, OfferRewardEmoteDelay3 = 500 WHERE entry = 12224;
+
+-- q.12496 'Audience With The Dragon Queen'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, DetailsEmote3 = 5, DetailsEmoteDelay2 = 500, DetailsEmoteDelay3 = 500, IncompleteEmote = 1, CompleteEmote = 5, OfferRewardEmote1 = 6, OfferRewardEmote2 = 1, OfferRewardEmote3 = 1 WHERE entry = 12496;
+
+-- q.12500 'Return To Angrathar'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, IncompleteEmote = 1, CompleteEmote = 1, OfferRewardEmote1 = 1, OfferRewardEmote2 = 1, OfferRewardEmote3 = 66 WHERE entry = 12500;
+
+-- q.13242 'Darkness Stirs'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, IncompleteEmote = 1, CompleteEmote = 1, OfferRewardEmote1 = 1, OfferRewardEmote2 = 1, OfferRewardEmote3 = 1 WHERE entry = 13242;
+
+-- q.13257 'Herald of War'
+UPDATE quest_template SET DetailsEmote1 = 1, DetailsEmote2 = 1, IncompleteEmote = 1, CompleteEmote = 1, OfferRewardEmote1 = 1, OfferRewardEmote2 = 1, OfferRewardEmote3 = 1 WHERE entry = 13257;
