f1027867693756fbfb58f752278736491ecf7a3c
MantisLord <sabinprosper@gmail.com>
2020-11-21 03:48:57 -0500

    [TDB][Instances] Zul'Aman - update for opening gong event
    Helps the RP script avoid getting stuck
    
    (cherry picked from commit aacbd62cdd5dacc81b1abdc3244af1a64526bf6c)
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/e6360e74070e045188c1b644864a2e1da673389b

---
+++ Updates/3293_c.5592_za_equipment.sql
@@ -0,0 +1,4 @@
+DELETE FROM `creature_equip_template` WHERE `entry` = 5592;
+INSERT INTO `creature_equip_template` (`entry`, `equipentry1`, `equipentry2`, `equipentry3`) VALUES
+(5592, 13631, 0, 0);
+
