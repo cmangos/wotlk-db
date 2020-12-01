0cdd04a2417e6cf9ffb8770fb75230b3b05fc886
Grz3s <Grz3s@users.noreply.github.com>
2020-12-01 20:50:25 +0000

    [3083] Steel Gate Flying Machine 24418 - update
    finally we can set up to diff sets of spell depends of req.
    Part of sd2 script.

---
+++ Updates/3083_c.24418.sql
@@ -0,0 +1,7 @@
+-- Steel Gate Flying Machine 24418
+-- finally we can set up to diff sets of spell depends of req.
+-- Part of sd2 script.
+DELETE FROM creature_template_spells WHERE entry = 24418;
+INSERT INTO creature_template_spells (entry, setId, spell1, spell2, spell3, spell4, spell5) VALUES
+(24418,0,43770,0,0,0,0), -- q.11390
+(24418,1,43799,43769,44009,0,0); -- q.11391
