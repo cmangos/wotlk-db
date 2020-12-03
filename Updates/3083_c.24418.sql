d8aa8f6429a81f079b1ee25fec01693ad7ab2add
Grz3s <Grz3s@users.noreply.github.com>
2020-12-03 19:31:04 +0000

    Update for [3083]
    SpellScripts redone via sd2
    
    Co-Authored-By: killerwife <killerwife@users.noreply.github.com>

--- Updates/3083_c.24418.sql
+++ Updates/3083_c.24418.sql
@@ -5,3 +5,6 @@ DELETE FROM creature_template_spells WHERE entry = 24418;
 INSERT INTO creature_template_spells (entry, setId, spell1, spell2, spell3, spell4, spell5) VALUES
 (24418,0,43770,0,0,0,0), -- q.11390
 (24418,1,43799,43769,44009,0,0); -- q.11391
+
+-- fixed in SD2
+DELETE FROM dbscripts_on_spell WHERE id IN (43770,43789);
