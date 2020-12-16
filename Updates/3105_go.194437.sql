50c002eb4b98c67bd758288fd8abaf137d65b574
killerwife <killerwife@gmail.com>
2020-12-16 15:16:41 +0100

    Fix 3105 coords and spawnmask

--- Updates/3105_go.194437.sql
+++ Updates/3105_go.194437.sql
@@ -3,4 +3,4 @@
 UPDATE gameobject_template SET ExtraFlags = 4096 WHERE entry IN (194675);
 
 -- fix button location for the tram
-UPDATE gameobject SET position_x=2306.999, position_y=274.3252, position_z=424.2883 WHERE guid=6030078;
+UPDATE gameobject SET spawnMask=0,position_x=0,position_y=-10.30644,position_z=0,orientation=1.570796 WHERE id IN(194437);
