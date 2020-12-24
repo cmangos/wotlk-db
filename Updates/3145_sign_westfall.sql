aba70f8133dde144184d67f65af1490fc4f7b4ad
Grz3s <Grz3s@users.noreply.github.com>
2020-12-24 23:32:05 +0000

    [3145] Sign in Westfall
    poss corrected
    
    PArt of:
    https://github.com/cmangos/wotlk-db/issues/332

---
+++ Updates/3145_sign_westfall.sql
@@ -0,0 +1,10 @@
+-- Old Coast Road 80
+UPDATE gameobject SET position_x = -9979.134, position_y = 1007.325, position_z = 32.75878, orientation = 0.09915372, rotation0=0.1006846, rotation1=-0.02514362, rotation2=0.05229092, rotation3=0.9932251 WHERE guid = 11015;
+-- Sentinel Hill 81
+UPDATE gameobject SET position_x = -9978.25, position_y = 1006.509, position_z = 31.75917, orientation = -3.13409, rotation0=-0.009524822, rotation1=-0.05605507, rotation2=-0.998373, rotation3=0.004291601 WHERE guid = 11016;
+-- Elwynn Forest 82
+UPDATE gameobject SET position_x = -9978.593, position_y = 1007.753, position_z = 32.2856 , orientation = -1.419897, rotation0=0.08098269, rotation1=-0.02492523, rotation2=-0.6514511, rotation3=0.7539443 WHERE guid = 11017;
+-- Jangolode Mine 83
+UPDATE gameobject SET position_x = -9977.336, position_y = 1009.862, position_z = 31.46883, orientation = -1.479513, rotation0=0.2197061, rotation1=-0.6859207, rotation2=0.1600161, rotation3=0.6750088 WHERE guid = 11018;
+-- Moonbrook 16396
+UPDATE gameobject SET position_x = -9979.234, position_y = 1007.138, position_z = 31.13727, orientation = 0.0261795, rotation0=0.08714771, rotation1=0.001140594, rotation2=0.01303959, rotation3=0.9961094 WHERE guid = 31962;
