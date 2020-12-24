87faaa38573521e76197c018d197123b0e0a1850
Grz3s <Grz3s@users.noreply.github.com>
2020-12-24 00:37:42 +0000

    [3143] Disciple of Vesperon 30858 - stats

---
+++ Updates/3143_c.30858.sql
@@ -0,0 +1,3 @@
+-- Disciple of Vesperon 30858 - stats
+UPDATE creature_template SET MinLevel = 81, MaxLevel = 81, MinLevelHealth = 130330, MaxLevelHealth = 130330, MinLevelMana = 4081, MaxLevelMana = 4081 WHERE entry = 30858;
+UPDATE creature_template SET MinLevel = 81, MaxLevel = 81, MinLevelHealth = 390990, MaxLevelHealth = 390990, MinLevelMana = 4081, MaxLevelMana = 4081 WHERE entry = 31546;
