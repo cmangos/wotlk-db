5fe0ac5e6fd61c5d88885f9b750fc7c6230ebf24
Grz3s <Grz3s@users.noreply.github.com>
2020-12-27 00:05:20 +0000

    more stuff for [3148]
    small event with attack on Moa'ki Warriors - added
    
    Part of:
    https://github.com/cmangos/wotlk-db/issues/329

--- Updates/3148_Misc_Dragonblight.sql
+++ Updates/3148_Misc_Dragonblight.sql
@@ -10,3 +10,35 @@ INSERT INTO creature_template_spells(entry, spell1, spell2, spell3, spell4, spel
 
 -- Alystros the Verdant Keeper 27249
 UPDATE creature_template SET inhabittype = 4 WHERE entry = 27249;
+
+-- Moa'ki Warriors Event
+-- small event with attack on Moa'ki Warriors
+-- waypoints
+UPDATE creature SET spawndist = 0, MovementType = 2 WHERE guid IN (512302,512303,512310,512330,512362,512383);
+DELETE FROM creature_movement WHERE id IN (512302,512303,512310,512330,512362,512383);
+INSERT INTO creature_movement (id,point,position_x,position_y,position_z,orientation,waittime,script_id) VALUES
+-- 512302
+(512302,1,2900.96,930.407,35.9615,3.7001,40000,5),
+(512302,2,2865.07,908.304,23.4105,100,50000,2619801),
+-- 512303
+(512303,1,2914.92,904.427,33.6372,3.64774,35000,5),
+(512303,2,2902.88,897.905,29.981,100,55000,2619801),
+-- 512310
+(512310,1,2874.28,984.247,34.6679,3.85718,35000,5),
+(512310,2,2831.83,964.636,26.8291,100,45000,2619801),
+-- 512330
+(512330,1,2862.62,1008.03,36.6638,3.42085,60000,5),
+(512330,2,2820.4,998.622,28.6547,100,57000,2619801),
+-- 512362
+(512362,1,2932.11,840.616,36.0044,2.56563,40000,5),
+(512362,2,2930,848.876,33.8126,100,0,0),
+(512362,3,2913.62,853.741,28.5329,100,60000,2619801),
+(512362,4,2929.52,849.112,33.7302,100,0,0),
+-- 512383
+(512383,1,2932.29,773.119,30.7928,2.44346,30000,5),
+(512383,2,2901.21,756.826,19.2377,100,60000,2619801);
+DELETE FROM dbscripts_on_creature_movement WHERE id = 2619801;
+INSERT INTO dbscripts_on_creature_movement (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
+(2619801,0,31,27178,10,0,0,0,0,0,0,0,0,0,0,0,0,'search for 27178'),
+(2619801,1,22,1812,0x01 | 0x02,0,27178,15,7,0,0,0,0,0,0,0,0,'buddy: faction change'),
+(2619801,10,26,0,0,0,27178,15,1,0,0,0,0,0,0,0,0,'attack start');
