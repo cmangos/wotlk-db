ae3af33aa22494993e644009ffeb46d5feb88657
Grz3s <Grz3s@users.noreply.github.com>
2021-01-22 23:48:42 +0000

    [3186][ACID] q.12723 'Behind Scarlet Lines' - Update
    text on LOS added
    
    Part of:
    https://github.com/cmangos/wotlk-db/issues/299

---
+++ Updates/3186_q.12723.sql
@@ -0,0 +1,5 @@
+-- q.12723 'Behind Scarlet Lines'
+-- Part of Orbaz Bloodbane 28914 EAI - quest check for LOS
+DELETE FROM conditions WHERE condition_entry = 20194;
+INSERT INTO conditions (condition_entry, type, value1, value2, value3, value4) VALUES 
+(20194,9,12723,0,0,0);
