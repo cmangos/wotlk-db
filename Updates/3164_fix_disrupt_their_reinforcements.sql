2a2737c390fa9a9a554cc54a7457917febad2aad
Vidar Wahlberg <canidae@exent.net>
2021-01-23 16:17:00 +0100

    Increased carry count of "Demonic Rune Stone" to 8

--- Updates/3164_fix_disrupt_their_reinforcements.sql
+++ Updates/3164_fix_disrupt_their_reinforcements.sql
@@ -1,2 +1,2 @@
--- Players need 4 "Demonic Rune Stone" to disrupt portals in quest "Disrupt Their Reinforcements"
-UPDATE quest_template SET ReqSourceCount3 = 4 WHERE entry IN (10144, 10208);
+-- Players need 4 "Demonic Rune Stone" to disrupt each of the two portals in quest "Disrupt Their Reinforcements"
+UPDATE quest_template SET ReqSourceCount3 = 8 WHERE entry IN (10144, 10208);
