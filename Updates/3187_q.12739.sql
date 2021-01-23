f70c86c274974f70f54e480f75b0f463611fb604
Grz3s <Grz3s@users.noreply.github.com>
2021-01-23 01:24:43 +0000

    [3187] q.12739 'A Special Surprise' - Update
    all NPCs involved shouldnt regenerate and spawn with only half HP
    
    Part of:
    https://github.com/cmangos/wotlk-db/issues/20

---
+++ Updates/3187_q.12739.sql
@@ -0,0 +1,4 @@
+-- q.12739 'A Special Surprise' - Update
+-- shouldnt regenerate and spawn with only half HP
+UPDATE creature_template SET RegenerateStats = 0 WHERE entry IN (29032,29061,29065,29067,29068,29070,29071,29072,29073,29074);
+UPDATE creature SET spawntimesecsmin = 30, spawntimesecsmax = 30, curhealth = 5 WHERE id  IN (29032,29061,29065,29067,29068,29070,29071,29072,29073,29074);
