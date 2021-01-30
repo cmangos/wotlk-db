e0cfd8e8b2c1c129ee0a530c5d1056e3781bfc19
Grz3s <Grz3s@users.noreply.github.com>
2021-01-30 22:04:17 +0000

    [3196] DK Phase 128 Miscs
    Part of:
    https://github.com/cmangos/wotlk-db/issues/325
    
    Part of:
    https://github.com/cmangos/wotlk-db/issues/22

---
+++ Updates/3196_Misc_DK_Phase_128.sql
@@ -0,0 +1,81 @@
+-- DK Phase 128 Miscs
+
+-- Flesh Behemoth 29190
+UPDATE creature_template SET SpeedWalk=2.5/2.5,SpeedRun=16/7 WHERE entry = 29190;
+DELETE FROM creature_template_addon WHERE entry = 29190;
+INSERT INTO creature_template_addon (entry, mount, bytes1, b2_0_sheath, b2_1_pvp_state, emote, moveflags, auras) VALUES 
+(29190,0,0,1,0,0,0,'53624 53627');
+DELETE FROM dbscripts_on_creature_death WHERE id = 29190;
+INSERT INTO dbscripts_on_creature_death (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
+(29190,0,18,0,0,0,0,0,0x04,0,0,0,0,0,0,0,0,'despawn');
+DELETE FROM creature_movement_template WHERE entry = 29190;
+INSERT INTO creature_movement_template (entry, pathid, point, position_x, position_y, position_z, orientation, waittime, script_id) VALUES
+-- path for 561924
+(29190,0,1,2435.987,-5097.607,82.96162,5.061455,100,5), -- spawn point
+(29190,0,2,2419.6,-5122.16,81.1112,100,0,0),
+(29190,0,3,2401.72,-5142.61,83.1335,100,0,0),
+(29190,0,4,2385.38,-5156.83,75.5348,100,0,0),
+(29190,0,5,2373.48,-5172.65,74.6722,100,0,0),
+(29190,0,6,2364.63,-5183.49,77.1403,100,0,0),
+(29190,0,7,2350.4,-5201.39,76.8901,100,0,0),
+(29190,0,8,2330.48,-5205.79,81.8514,100,0,0),
+(29190,0,9,2316.78,-5209.51,81.6041,100,0,0),
+(29190,0,10,2299.41,-5215.87,83.9007,100,0,0),
+(29190,0,11,2280.55,-5211.67,82.0652,100,0,0),
+(29190,0,12,2268.24,-5209.44,82.3729,100,0,0),
+(29190,0,13,2236.47,-5204.69,74.9135,100,0,0),
+(29190,0,14,2228.61,-5223.14,74.3909,100,0,0),
+(29190,0,15,2221.58,-5241.43,78.1746,100,0,0),
+(29190,0,16,2228.65,-5251.06,75.5002,100,0,0),
+(29190,0,17,2236.57,-5262.38,74.7068,100,0,0),
+(29190,0,18,2247.06,-5279.16,81.5796,100,0,0),
+(29190,0,19,2249.22,-5295.68,82.1674,100,5000,20),
+-- path for 561925
+(29190,1,1,2453.178,-5183.313,76.18691,3.228859,100,5), -- spawn point
+(29190,1,2,2412.24,-5183.5,77.0228,100,0,0),
+(29190,1,3,2386.57,-5186.66,74.2527,100,0,0),
+(29190,1,4,2369.94,-5196.97,75.7526,100,0,0),
+(29190,1,5,2355.95,-5206.45,78.8122,100,0,0),
+(29190,1,6,2347.69,-5209.09,79.5427,100,0,0),
+(29190,1,7,2333.67,-5212.68,83.8716,100,0,0),
+(29190,1,8,2317.91,-5216.2,83.733,100,0,0),
+(29190,1,9,2309.61,-5232.05,84.8413,100,0,0),
+(29190,1,10,2300.17,-5246.84,84.4107,100,0,0),
+(29190,1,11,2300.15,-5285.83,81.9137,100,5000,20),
+-- for second wave summons - 1st
+(29190,2,1,2345.58,-5283.828,82.45392,3.033957,100,5), -- spawn point
+(29190,2,2,2282.042,-5277.006,82.18815,100,5000,20),
+-- for second wave summons - 2ndt
+(29190,3,1,2244.037,-5338.109,85.05786,0.296706,100,5), -- spawn point
+(29190,3,2,2254.5,-5298.54,82.1677,100,5000,20);
+
+-- Rampaging Abomination 29186
+DELETE FROM creature_movement_template WHERE entry = 29186;
+INSERT INTO creature_movement_template (entry, pathid, point, position_x, position_y, position_z, orientation, waittime, script_id) VALUES
+-- pathid = 0
+(29186,0,1,2271.599,-5331.55,87.84799,5.951573,100,5), -- spawn point
+(29186,0,2,2271.73,-5288.08,82.4161,100,5000,20),
+-- pathid = 1
+(29186,1,1,2204.03,-5250.58,80.9081,5.52923,100,5), -- spawn point
+(29186,1,2,2227.06,-5262.19,75.4769,100,0,0),
+(29186,1,3,2271.29,-5275.3,81.7531,100,5000,20),
+-- pathid = 2
+(29186,2,1,2347.58,-5259.57,82.2603,3.72025,100,5), -- spawn point
+(29186,2,2,2292.24,-5280,81.9698,100,5000,20);
+
+-- Highlord Darion Mograine 29173
+UPDATE creature SET spawntimesecsmin = 20, spawntimesecsmax = 20 WHERE id = 29173;
+
+-- Defender of the Light 29174
+DELETE FROM creature_template_addon WHERE entry = 29174;
+INSERT INTO creature_template_addon (entry, mount, bytes1, b2_0_sheath, b2_1_pvp_state, emote, moveflags, auras) VALUES 
+(29174,0,0,1,0,375,0,NULL);
+
+-- Leonid Barthalomew the Revered 29179
+UPDATE creature_template SET MovementType = 2 WHERE entry = 29179;
+DELETE FROM creature_movement_template WHERE entry = 29179;
+INSERT INTO creature_movement_template (entry, point, position_x, position_y, position_z, orientation, waittime, script_id) VALUES
+(29179,1,2282.02,-5317.44,88.5497,100,0,0),
+(29179,2,2280.35,-5301.91,85.444,100,0,0),
+(29179,3,2278.215,-5279.796,82.32075,100,1000,0),
+(29179,4,2279.79,-5295.6,84.1678,100,0,0);
