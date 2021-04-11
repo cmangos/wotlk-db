3a67d2fb633ddae0157bb34f050bc9ad7825b23b
Grz3s <Grz3s@users.noreply.github.com>
2021-04-11 20:06:58 +0100

    backport fixes

--- Updates/3435_TDB-0399_icy_veins_spell_affect.sql
+++ Updates/3435_TDB-0399_icy_veins_spell_affect.sql
@@ -1,4 +1,3 @@
+-- not  for WOTLKDB
 -- Allow Frostbolt, Polymorph, and Scorch to be affected by Icy Veins
-UPDATE spell_affect SET SpellFamilyMask=SpellFamilyMask|(1572896+16777216+16) WHERE entry=12472;
-
-
+-- UPDATE spell_affect SET SpellFamilyMask=SpellFamilyMask|(1572896+16777216+16) WHERE entry=12472;
--- Updates/3447_TDB-0412_player_model_sizes.sql
+++ Updates/3447_TDB-0412_player_model_sizes.sql
@@ -1,4 +1,5 @@
 -- vanilla and tbc only - most of it was already in DB - notified by brotalnia it is wrong for combat reaches
+/*
 -- Human Male
 UPDATE `creature_model_info` SET `bounding_radius`=0.306, `combat_reach`=1.5 WHERE `modelid`=49;
 -- Human Female
@@ -20,25 +21,23 @@ UPDATE `creature_model_info` SET `bounding_radius`=0.383, `combat_reach`=1.5 WHE
 -- Undead Female
 UPDATE `creature_model_info` SET `bounding_radius`=0.383, `combat_reach`=1.5 WHERE `modelid`=58;
 -- Tauren Male
-UPDATE `creature_model_info` SET `bounding_radius`=0.9747, `combat_reach`=4.05 WHERE `modelid`=59;
+-- UPDATE `creature_model_info` SET `bounding_radius`=0.9747, `combat_reach`=4.05 WHERE `modelid`=59;
 -- Tauren Female
-UPDATE `creature_model_info` SET `bounding_radius`=0.8725, `combat_reach`=3.75 WHERE `modelid`=60;
+-- UPDATE `creature_model_info` SET `bounding_radius`=0.8725, `combat_reach`=3.75 WHERE `modelid`=60;
 -- Gnome Male
-UPDATE `creature_model_info` SET `bounding_radius`=0.3519, `combat_reach`=1.725 WHERE `modelid`=1563;
+-- UPDATE `creature_model_info` SET `bounding_radius`=0.3519, `combat_reach`=1.725 WHERE `modelid`=1563;
 -- Gnome Female
-UPDATE `creature_model_info` SET `bounding_radius`=0.3519, `combat_reach`=1.725 WHERE `modelid`=1564;
+-- UPDATE `creature_model_info` SET `bounding_radius`=0.3519, `combat_reach`=1.725 WHERE `modelid`=1564;
 -- Troll Male
 UPDATE `creature_model_info` SET `bounding_radius`=0.306, `combat_reach`=1.5 WHERE `modelid`=1478;
 -- Troll Female
 UPDATE `creature_model_info` SET `bounding_radius`=0.306, `combat_reach`=1.5 WHERE `modelid`=1479;
-
 -- Draenei Male
 UPDATE `creature_model_info` SET `bounding_radius`=1, `combat_reach`=1.5 WHERE `modelid`=16125;
 -- Draenei Female
 UPDATE `creature_model_info` SET `bounding_radius`=1, `combat_reach`=1.5 WHERE `modelid`=16126;
-
 -- BELF Male
 UPDATE `creature_model_info` SET `bounding_radius`=0.383, `combat_reach`=1.5 WHERE `modelid`=15476;
 -- BELF Female
 UPDATE `creature_model_info` SET `bounding_radius`=0.383, `combat_reach`=1.5 WHERE `modelid`=15475;
-
+*/
