5ce09710c5c9619ef992861640f5ae88673b94a7
anonxs <anonxs@web.de>
2021-04-16 15:48:09 +0200

    [3825][DBErrors][TDB] Remove LootId for Aquementas, Fix Gossip for Maybess Riverbreeze 9529 & Apothecary Enith 16208
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/20a532abeaf87ef5749ee3cea00f368bd714b09b

---
+++ Updates/3825_TDB-0450_maybess_riverbreeze_gossip_improvements.sql
@@ -0,0 +1,13 @@
+-- Assign correct gossip_menu for Maybess Riverbreeze <Emerald Circle> 9529
+UPDATE `creature_template` SET `GossipMenuId` = 2207 WHERE `entry` = 9529; -- old 20022
+DELETE FROM gossip_menu WHERE entry IN(20022,2207,2361);
+INSERT INTO gossip_menu(entry, text_id, script_id, condition_id) VALUES
+('2207', '2842', '0', '0'),
+('2207', '2843', '0', '4'),
+('2207', '2849', '0', '6'),
+('2361', '3047', '0', '0');
+DELETE FROM gossip_menu_option WHERE menu_id IN(20022,2207);
+INSERT INTO gossip_menu_option(menu_id, id, option_icon, option_text, option_broadcast_text, option_id, npc_option_npcflag, action_menu_id, action_poi_id, action_script_id, box_coded, box_money, box_text, box_broadcast_text, condition_id) VALUES
+('2207', '0', '0', 'I need a Cenarion beacon.', '5243', '1', '1', '-1', '0', '2208', '0', '0', NULL, '0', '132'),
+('2207', '1', '0', 'What plants are in Felwood that might be corrupted?', '5384', '1', '1', '2361', '0', '0', '0', '0', NULL, '0', '0');
+
