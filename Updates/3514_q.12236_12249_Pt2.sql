3a33897983f1a52723f358f5834aa6a4c50f7dc6
Grz3s <Grz3s@users.noreply.github.com>
2021-05-01 02:41:36 +0100

    Update for [3514]
    action corrected

--- Updates/3514_q.12236_12249_Pt2.sql
+++ Updates/3514_q.12236_12249_Pt2.sql
@@ -2,7 +2,7 @@
 -- q.12236 'Ursoc, the Bear God' - Horde
 
 -- quest should be available for both
-UPDATE gossip_menu_option SET action_menu_id = -1, condition_id = 20297 WHERE menu_id = 9496;
+UPDATE gossip_menu_option SET action_menu_id = 9497, condition_id = 20297 WHERE menu_id = 9496;
 DELETE FROM conditions WHERE condition_entry IN (20296,20297);
 INSERT INTO conditions (condition_entry, type, value1, value2, flags) VALUES 
 -- 282 - Quest ID 12249 Taken
