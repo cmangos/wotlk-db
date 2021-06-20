ee7822c8a5925d6a0d38a76e2594193bf8303e68
Grz3s <Grz3s@users.noreply.github.com>
2021-06-21 00:23:54 +0100

    [3642] Battered Chests (2843)
    pools corrected
    
    Co-Authored-By: AnonXS <AnonXS@users.noreply.github.com>

---
+++ Updates/3642_o.2843.sql
@@ -0,0 +1,6 @@
+-- More small Battered Chests (2843) updates
+-- pools corrected
+UPDATE pool_gameobject SET pool_entry = 31003 WHERE guid = 300026;
+UPDATE pool_gameobject SET pool_entry = 31002 WHERE guid = 300027;
+UPDATE pool_gameobject SET pool_entry = 31005 WHERE guid = 300028;
+UPDATE pool_gameobject SET pool_entry = 31006 WHERE guid = 300029;
