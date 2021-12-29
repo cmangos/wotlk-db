72c3749da05789294480adb815c71a29409bee1c
anonxs <anonxs@web.de>
2021-12-29 20:49:34 +0100

    [Custom][TDB] Remove 0 Probability ModelIds for Lykul Stinger 19632
    ServerToClient: SMSG_QUERY_CREATURE_RESPONSE (0x0061) Length: 68 ConnIdx: 0 Time: 01/06/2009 04:59:37.000 Number: 5522
    Entry: 19632
    [0] Name: Lykul Stinger
    
    [0] Model ID: 19367
    [1] Model ID: 6633
    [2] Model ID: 7350
    [3] Model ID: 11091
    
    Heroic Template already has this corrected
    
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/6960b47b346d65d560bb49ebd45f8a534261fe64

--- utilities/cmangos_custom.sql
+++ utilities/cmangos_custom.sql
@@ -162,11 +162,12 @@ UPDATE `gameobject_template` SET `data5` = 1 WHERE `entry` IN (185497,185500);
 -- -------------------------------
 
 -- modelids with probability = 0
-UPDATE creature_template SET `modelid2` = 0 WHERE `entry` IN (
+UPDATE creature_template SET `modelid2` = 0, `modelid3` = 0, `modelid4` = 0 WHERE `entry` IN (
 17734, -- Underbog Lord
 17459, -- Chess Waiting Room (DND)
 18095, -- Doomfire
 18104, -- Doomfire Targeting
+19632, -- Lykul Stinger (6633,7350,11091)
 20155, -- Hillsbrad Internment Lodge Quest Trigger
 20156, -- Thrall Quest Trigger
 25265, -- Demonic Vapor
