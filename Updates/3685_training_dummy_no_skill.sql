bc6b42300daf87ceb00c1451ee589bd11e795a5e
killerwife <killerwife@gmail.com>
2022-02-03 14:40:32 +0100

    [3685] Make training dummies give no skillups
    Closes https://github.com/cmangos/issues/issues/56

---
+++ Updates/3685_training_dummy_no_skill.sql
@@ -0,0 +1,3 @@
+UPDATE creature_template SET ExtraFlags=ExtraFlags|0x01000000 WHERE entry IN(31144,31146,32666,32667,30527,31143);
+UPDATE creature_template SET ExtraFlags=ExtraFlags|0x01000000 WHERE entry IN(32541,32542,32543,32545,32546);
+
