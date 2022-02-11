5c2f69ecb5f31e66cc714cdf9ae9cf0f5a8480be
killerwife <killerwife@gmail.com>
2022-02-12 00:34:53 +0100

    Add unattackable unitflag to army of the dead ghoul

--- Updates/3690_army_of_the_dead_ghoul_stats.sql
+++ Updates/3690_army_of_the_dead_ghoul_stats.sql
@@ -1,3 +1,3 @@
-UPDATE creature_template SET MinLevel=80,MaxLevel=80,HealthMultiplier=0.301265 WHERE entry IN(24207);
+UPDATE creature_template SET MinLevel=80,MaxLevel=80,HealthMultiplier=0.301265,UnitFlags=UnitFlags|2 WHERE entry IN(24207);
 
 
