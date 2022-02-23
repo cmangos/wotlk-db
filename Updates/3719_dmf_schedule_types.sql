fc9d3bf823af340852191942220e8524ac659d64
killerwife <killerwife@gmail.com>
2022-02-23 10:09:20 +0100

    Add rest of dmf schedule types

--- Updates/3719_dmf_schedule_types.sql
+++ Updates/3719_dmf_schedule_types.sql
@@ -1,2 +1,11 @@
+UPDATE game_event SET schedule_type=8 WHERE entry IN(63);
+UPDATE game_event SET schedule_type=5 WHERE entry IN(62);
+UPDATE game_event SET schedule_type=6 WHERE entry IN(64);
+UPDATE game_event SET schedule_type=9 WHERE entry IN(65);
+UPDATE game_event SET schedule_type=7 WHERE entry IN(66);
+UPDATE game_event SET schedule_type=10 WHERE entry IN(23);
+UPDATE game_event SET schedule_type=2 WHERE entry IN(3);
+UPDATE game_event SET schedule_type=3 WHERE entry IN(5);
+UPDATE game_event SET schedule_type=4 WHERE entry IN(4);
 DELETE FROM game_event_time WHERE entry IN(23,62,63,64,65,66);
 
