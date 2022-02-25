eed8e9482fe95f0f25f87ad8edb048595e873f9b
Grz3s <Grz3s@users.noreply.github.com>
2022-02-25 15:00:23 +0100

    Fix broadcast text 30511 Changed sometime after wotlk Closes https://github.com/cmangos/mangos-wotlk/pull/344

---
+++ Updates/3724_broadcast_text_fix.sql
@@ -0,0 +1,3 @@
+UPDATE broadcast_text SET Text='Algar the Chosen is at the Reliquary of Pain to the northeast.\n\nPrince Navarius is on the other side of the Dead Fields to the east.' WHERE Id IN(30511);
+
+
