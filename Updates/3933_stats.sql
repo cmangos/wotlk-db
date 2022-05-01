014240569e8471f76009a5029ef2a9228643392d
Grz3s <Grz3s@users.noreply.github.com>
2022-05-01 23:44:39 +0200

    [3933] Black Morass - stats for few creatures
    Closes:
    https: //github.com/cmangos/issues/issues/2932
    Co-Authored-By: Eriumango <99593557+Eriumango@users.noreply.github.com>

---
+++ Updates/3933_stats.sql
@@ -0,0 +1,14 @@
+-- stats for few creatures in Black Morass
+
+-- Infinite Executioner 18994
+UPDATE creature_template SET minLevel = 70, maxLevel = 70, Faction = 1720, MinLevelHealth = 5589, MaxLevelHealth = 5589, MinLevelMana = 9465, MaxLevelMana = 9465 WHERE entry = 18994;
+-- Infinite Executioner 18995
+UPDATE creature_template SET minLevel = 70, maxLevel = 70, Faction = 1720, MinLevelHealth = 5589, MaxLevelHealth = 5589, MinLevelMana = 9465, MaxLevelMana = 9465 WHERE entry = 18995;
+-- Infinite Executioner 21138
+UPDATE creature_template SET minLevel = 70, maxLevel = 70, Faction = 1720, MinLevelHealth = 5589, MaxLevelHealth = 5589, MinLevelMana = 9465, MaxLevelMana = 9465 WHERE entry = 21138;
+-- Infinite Vanquisher 21139
+UPDATE creature_template SET minLevel = 70, maxLevel = 70, Faction = 1720, MinLevelHealth = 5589, MaxLevelHealth = 5589, MinLevelMana = 9465, MaxLevelMana = 9465 WHERE entry = 21139;
+-- Rift Lord 21140
+UPDATE creature_template SET minLevel = 71, maxLevel = 71, Faction = 1720, MinLevelHealth = 64629, MaxLevelHealth = 64629 WHERE entry = 21140;
+-- Rift Keeper 21148
+UPDATE creature_template SET minLevel = 71, maxLevel = 71, Faction = 1720, MinLevelHealth = 51696, MaxLevelHealth = 51696, MinLevelMana = 32310, MaxLevelMana = 32310 WHERE entry = 21148;
