be39eb989d49f7a7fb4a7d8b97a505fe352b63df
killerwife <killerwife@gmail.com>
2022-05-01 09:57:48 +0200

    Add DK zone tele loc

---
+++ Updates/3929_scarlet_enclave_tele.sql
@@ -0,0 +1,4 @@
+DELETE FROM game_tele WHERE id=1533 AND map=609;
+INSERT INTO game_tele(id, position_x, position_y, position_z, orientation, map, name) VALUES
+(1533,2352.919678,-5671.297852,426.027466,3.671,609,'TheScarletEnclave');
+
