3251ff8eaa94ef838c9c480cc8ad8a051cc88f5e
killerwife <killerwife@gmail.com>
2022-05-02 14:06:11 +0200

    [3935] Add RDF worldstate names

---
+++ Updates/3935_rdf_worldstates.sql
@@ -0,0 +1,7 @@
+DELETE FROM worldstate_name WHERE Id IN(20000,20001,20002,20003);
+INSERT INTO worldstate_name(Id,Name) VALUES
+(20000,'Custom - Dungeon Finder - Headless Horseman'),
+(20001,'Custom - Dungeon Finder - Ahune'),
+(20002,'Custom - Dungeon Finder - Coren Direbrew'),
+(20003,'Custom - Dungeon Finder - Crown Chemical Co');
+
