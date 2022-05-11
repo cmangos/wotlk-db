9b146c2630806a596b9eef7f112fe56df0dd5c4f
Grz3s <Grz3s@users.noreply.github.com>
2022-05-12 01:32:55 +0200

    [4084] Missing text added

---
+++ Updates/4084_missing_text_7178.sql
@@ -0,0 +1,4 @@
+-- missing text added
+-- Source TDB
+REPLACE INTO `npc_text_broadcast_text` (`Id`, `Prob0`, `Prob1`, `Prob2`, `Prob3`, `Prob4`, `Prob5`, `Prob6`, `Prob7`, `BroadcastTextId0`, `BroadcastTextId1`, `BroadcastTextId2`, `BroadcastTextId3`, `BroadcastTextId4`, `BroadcastTextId5`, `BroadcastTextId6`, `BroadcastTextId7`) VALUES
+('7178','1','0','0','0','0','0','0','0','9874','0','0','0','0','0','0','0');
