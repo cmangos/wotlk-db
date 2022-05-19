20ddc6aff908348b2370075ca5e5181b9ac458fd
killerwife <killerwife@gmail.com>
2022-05-19 17:29:38 +0200

    Add second core update to fix build

--- Updates/4151_vehicle_seat_accessory.sql
+++ Updates/4151_vehicle_seat_accessory.sql
@@ -1,3 +1,7 @@
+ALTER TABLE db_version CHANGE COLUMN required_14057_01_mangos_spawn_group_chanced_spawns required_14058_01_mangos_quest_maxlevel bit;
+
+ALTER TABLE `quest_template` ADD COLUMN `MaxLevel` tinyint(3) unsigned NOT NULL DEFAULT '255' AFTER MinLevel;
+
 ALTER TABLE db_version CHANGE COLUMN required_14058_01_mangos_quest_maxlevel required_14059_01_mangos_vehicle_seat_accessory bit;
 
 DROP TABLE IF EXISTS vehicle_seat_addon;
