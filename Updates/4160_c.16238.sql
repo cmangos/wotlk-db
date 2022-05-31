cb27f16b807bfb0664f6eff57ce5c8ef7d8c298d
Grz3s <Grz3s@users.noreply.github.com>
2022-06-01 01:38:16 +0200

    [4160] Night Elf Ambusher 16238 - faction corrected
    Source: TDB
    
    Also:
    Magister Sylastor 16237 - eqip
    Tranquillien Scout 16242 - eqip
    
    Closes:
    https://github.com/cmangos/wotlk-db/issues/420

---
+++ Updates/4160_c.16238.sql
@@ -0,0 +1,8 @@
+-- Night Elf Ambusher 16238
+UPDATE creature_template SET Faction = 80, EquipmentTemplateId = 158 WHERE entry IN (16238);
+
+-- Magister Sylastor 16237
+UPDATE creature_template SET EquipmentTemplateId = 127 WHERE entry IN (16237);
+
+-- Tranquillien Scout 16242
+UPDATE creature_template SET EquipmentTemplateId = 45 WHERE entry IN (16242);
