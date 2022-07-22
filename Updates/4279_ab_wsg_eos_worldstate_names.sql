385b4f3fc574a15a3b0abcd70c17116912628b05
killerwife <killerwife@gmail.com>
2022-07-22 16:09:19 +0200

    [4279] AB, WSG, EOS worldstate names

---
+++ Updates/4279_ab_wsg_eos_worldstate_names.sql
@@ -0,0 +1,71 @@
+INSERT INTO worldstate_name(Id, Name) VALUES
+(1778,'Arathi Basin - Occupied Bases Horde'),
+(1779,'Arathi Basin - Occupied Bases Alliance'),
+(1776,'Arathi Basin - Resources Alliance'),
+(1777,'Arathi Basin - Resources Horde'),
+(1780,'Arathi Basin - Resources Max'),
+(1955,'Arathi Basin - Close to Victory warning threshold'),
+(1842,'Arathi Basin - Stable Neutral'),
+(1843,'Arathi Basin - Gold Mine Neutral'),
+(1844,'Arathi Basin - Lumber Mill Neutral'),
+(1845,'Arathi Basin - Farm Neutral'),
+(1846,'Arathi Basin - Blacksmith Neutral'),
+(1767,'Arathi Basin - Stable Alliance'),
+(1768,'Arathi Basin - Stable Horde'),
+(1769,'Arathi Basin - Stable contested Alliance'),
+(1770,'Arathi Basin - Stable contested Horde'),
+(1772,'Arathi Basin - Farm Alliance'),
+(1773,'Arathi Basin - Farm Horde'),
+(1774,'Arathi Basin - Farm contested Alliance'),
+(1775,'Arathi Basin - Farm contested Horde'),
+(1782,'Arathi Basin - Blacksmith Alliance'),
+(1783,'Arathi Basin - Blacksmith Horde'),
+(1784,'Arathi Basin - Blacksmith contested Alliance'),
+(1785,'Arathi Basin - Blacksmith contested Horde'),
+(1792,'Arathi Basin - Lumber Mill Alliance'),
+(1793,'Arathi Basin - Lumber Mill Horde'),
+(1794,'Arathi Basin - Lumber Mill contested Alliance'),
+(1795,'Arathi Basin - Lumber Mill contested Horde'),
+(1787,'Arathi Basin - Gold Mine Alliance'),
+(1788,'Arathi Basin - Gold Mine Horde'),
+(1789,'Arathi Basin - Gold Mine contested Alliance'),
+(1790,'Arathi Basin - Gold Mine contested Horde');
+
+INSERT INTO worldstate_name(Id, Name) VALUES
+(1545,'Warsong Gulch - Picked up flag state Alliance'),
+(1546,'Warsong Gulch - Picked up flag state Horde'),
+(1581,'Warsong Gulch - Captures Alliance'),
+(1582,'Warsong Gulch - Captures Horde'),
+(1601,'Warsong Gulch - Captures Max'),
+(2338,'Warsong Gulch - Horde HUD enabled'),
+(2339,'Warsong Gulch - Alliance HUD enabled'),
+(4247,'Warsong Gulch - Time enabled'),
+(4248,'Warsong Gulch - Time remaining');
+
+INSERT INTO worldstate_name(Id, Name) VALUES
+(2749,'Eye of the Storm - Resources Alliance'),
+(2750,'Eye of the Storm - Resources Horde'),
+(2752,'Eye of the Storm - Towers Alliance'),
+(2753,'Eye of the Storm - Towers Horde'),
+(2718,'Eye of the Storm - Point Slider Display'),
+(2757,'Eye of the Storm - Flag Ready'),
+(2769,'Eye of the Storm - Flag state Alliance'),
+(2770,'Eye of the Storm - Flag state Horde'),
+
+(2723,'Eye of the Storm - Blood Elf Tower Alliance'),
+(2724,'Eye of the Storm - Blood Elf Tower Horde'),
+(2722,'Eye of the Storm - Blood Elf Tower Neutral'),
+
+(2730,'Eye of the Storm - Mage Tower Alliance'),
+(2729,'Eye of the Storm - Mage Tower Horde'),
+(2728,'Eye of the Storm - Mage Tower Neutral'),
+
+(2726,'Eye of the Storm - Fel Reaver Ruins Alliance'),
+(2727,'Eye of the Storm - Fel Reaver Ruins Horde'),
+(2725,'Eye of the Storm - Fel Reaver Ruins Neutral'),
+
+(2732,'Eye of the Storm - Draenei Ruins Alliance'),
+(2733,'Eye of the Storm - Draenei Ruins Horde'),
+(2731,'Eye of the Storm - Draenei Ruins Neutral');
+
+
