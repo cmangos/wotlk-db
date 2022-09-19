cb195b73bf9bfac00cee79e2e51a63d130b12aad
Grz3s <Grz3s@users.noreply.github.com>
2022-09-20 00:52:21 +0200

    Update for [4338]
    fixed
    
    Closes:
    https://github.com/cmangos/wotlk-db/issues/462

--- Updates/4338_q.8728_mail_fix.sql
+++ Updates/4338_q.8728_mail_fix.sql
@@ -1,3 +1,7 @@
 -- q.8728 'The Good News and The Bad News'
--- delay added to mail (Player should receice it 36hrs after complete)
-UPDATE quest_template SET RewMailDelaySecs=86400 WHERE entry=8728;
+-- gold & delay added to mail (Player should receive it 36hrs after complete) 
+UPDATE quest_template SET RewMailTemplateId=0, RewMailDelaySecs=0 WHERE entry=8728;
+DELETE FROM dbscripts_on_quest_end WHERE id IN (8728);
+INSERT INTO dbscripts_on_quest_end (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
+(8728,1,38,123,11811,1000000,0,0,0,129600,0,0,0,0,0,0,0,'send mail with 36hrs delay to Player');
+UPDATE quest_template SET CompleteScript = 8728 WHERE entry = 8728;
