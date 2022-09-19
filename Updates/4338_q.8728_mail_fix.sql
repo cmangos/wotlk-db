bae01edbbbb9f0cbd9e52bcee66ce6631d42b34d
Grz3s <Grz3s@users.noreply.github.com>
2022-09-19 23:20:53 +0200

    [4338] Mail Delay fixed for quest q.8728 'The Good News and The Bad News'
    Part of:
    https://github.com/cmangos/wotlk-db/issues/462

---
+++ Updates/4338_q.8728_mail_fix.sql
@@ -0,0 +1,3 @@
+-- q.8728 'The Good News and The Bad News'
+-- delay added to mail (Player should receice it 36hrs after complete)
+UPDATE quest_template SET RewMailDelaySecs=86400 WHERE entry=8728;
