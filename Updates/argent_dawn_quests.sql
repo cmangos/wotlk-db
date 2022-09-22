e6411f1af48a2aa0d23228be936212274635c8fb
RazzorX <RazzorX@users.noreply.github.com>
2022-09-23 00:08:36 +0200

    Update argent_dawn_quests.sql

--- Updates/argent_dawn_quests.sql
+++ Updates/argent_dawn_quests.sql
@@ -1,8 +1,13 @@
 
-SET @COND1 = XXX;
+-- not needed anymore, replaced with @COND5 (Quest ID 5401 Rewarded OR Quest ID 5405 Rewarded OR Quest ID 5503 Rewarded AND Player Has Less Than 1 of Item ID 12846 in Inventory)
+-- Condition 522 - (Quest ID 5503 Rewarded AND Player Has Less Than 1 of Item ID 12846 in Inventory)
+-- Condition 560 - (Quest ID 5405 Rewarded AND Player Has Less Than 1 of Item ID 12846 in Inventory)
+DELETE FROM conditions WHERE condition_entry IN (522, 560);
+
+SET @COND1 = 522; -- condID reused
 SET @COND2 = 320;
 SET @COND3 = 160;
-SET @COND4 = XXX;
+SET @COND4 = 560; -- condID reused
 SET @COND5 = XXX;
 
 -- only 1 of these quests can be taken/completed (Argent Dawn Commission - Quests)
@@ -33,7 +38,3 @@ UPDATE gossip_menu_option SET condition_id = @COND5 WHERE menu_id = 3421 AND id 
 UPDATE gossip_menu_option SET condition_id = @COND5 WHERE menu_id = 3441 AND id = 0; -- 560 (Argent Quartermaster Lightspark)
 UPDATE gossip_menu_option SET condition_id = @COND5 WHERE menu_id = 3461 AND id = 0; -- 167 (Quartermaster Miranda Breechlock)
 
--- not needed anymore, replaced with @COND5 (Quest ID 5401 Rewarded OR Quest ID 5405 Rewarded OR Quest ID 5503 Rewarded AND Player Has Less Than 1 of Item ID 12846 in Inventory)
--- Condition 522 - (Quest ID 5503 Rewarded AND Player Has Less Than 1 of Item ID 12846 in Inventory)
--- Condition 560 - (Quest ID 5405 Rewarded AND Player Has Less Than 1 of Item ID 12846 in Inventory)
-DELETE FROM conditions WHERE condition_entry IN (522, 560);
