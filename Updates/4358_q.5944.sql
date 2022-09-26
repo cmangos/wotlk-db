cd4da9110ef09ee1eac092509d698ea237049ba1
Grz3s <Grz3s@users.noreply.github.com>
2022-09-26 15:47:27 +0200

    [4358] q.5944 'In Dreams' - Update
    Part of:
    https://github.com/cmangos/wotlk-db/issues/467

---
+++ Updates/4358_q.5944.sql
@@ -0,0 +1,5 @@
+-- q.5944 'In Dreams' - Update
+-- Lord Tirion Fordring 12126
+UPDATE creature_template SET Faction = 250, EquipmentTemplateId = 305 WHERE entry = 12126;
+-- Crimson Elite 12128
+UPDATE creature_template SET Faction = 67, EquipmentTemplateId = 259 WHERE entry = 12128;
