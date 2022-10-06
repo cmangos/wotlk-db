673c2ff9cd58363b91892837963b580fbbaacbac
Grz3s <Grz3s@users.noreply.github.com>
2022-10-07 00:41:22 +0200

    [4373][ACID] Traveler's Tundra Mammoth - Update
    Hakmud of Argus 32638 & Gnmio 32639 & Drix Blackwrench 32641 & Mojodishu 32642
    - random texts added
    
    Closes:
    https: //github.com/cmangos/issues/issues/3071
    Co-Authored-By: Kel <22120553+kelbren@users.noreply.github.com>

---
+++ Updates/4373_Travelers_Tundra_Mammoth.sql
@@ -0,0 +1,20 @@
+-- Traveler's Tundra Mammoth
+-- Hakmud of Argus 32638 & Gnmio 32639 & Drix Blackwrench 32641 & Mojodishu 32642
+-- random texts added
+DELETE FROM `dbscript_random_templates` WHERE `id` BETWEEN 20295 AND ;
+INSERT INTO `dbscript_random_templates` (`id`, `type`, `target_id`, `chance`, `comments`) VALUES
+(20295,0,33014,0,'Hakmud of Argus 32638 - Random Texts'),
+(20295,0,33015,0,'Hakmud of Argus 32638 - Random Texts'),
+(20295,0,33016,0,'Hakmud of Argus 32638 - Random Texts'),
+(20295,0,33017,0,'Hakmud of Argus 32638 - Random Texts'),
+(20295,0,33018,0,'Hakmud of Argus 32638 - Random Texts'),
+(20296,0,33019,0,'Gnmio 32639 - Random Texts'),
+(20296,0,33020,0,'Gnmio 32639 - Random Texts'),
+(20296,0,33021,0,'Gnmio 32639 - Random Texts'),
+(20297,0,33023,0,'Drix Blackwrench 32641 - Random Texts'),
+(20297,0,33024,0,'Drix Blackwrench 32641 - Random Texts'),
+(20297,0,33025,0,'Drix Blackwrench 32641 - Random Texts'),
+(20297,0,33026,0,'Drix Blackwrench 32641 - Random Texts'),
+(20298,0,33027,0,'Mojodishu 32642 - Random Texts'),
+(20298,0,33028,0,'Mojodishu 32642 - Random Texts'),
+(20298,0,33029,0,'Mojodishu 32642 - Random Texts');
