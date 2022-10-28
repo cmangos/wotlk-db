29615f50ec4ee74bd87bec25a5e210d21b224f18
Grz3s <Grz3s@users.noreply.github.com>
2022-10-28 03:10:55 +0200

    [4389][ACID] Released Offspring of Har'koa 28526
    will run back to his master

---
+++ Updates/4389_c.28526.sql
@@ -0,0 +1,6 @@
+-- Part of Released Offspring of Har'koa 28526 EAI
+DELETE FROM dbscripts_on_relay WHERE id IN (20665);
+INSERT INTO dbscripts_on_relay (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
+(20665,1,25,1,0,0,0,0,0x04,0,0,0,0,0,0,0,0,'RUN ON'),
+(20665,10,3,0,0,0,0,0,0x04,0,0,0,0,5326.53,-3779.88,371.34,3.93214,'move'),
+(20665,6000,18,0,0,0,0,0,0x04,0,0,0,0,0,0,0,0,'Despawn Self');
