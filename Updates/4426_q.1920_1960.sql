d9d7a87a4cb35343910fc4d3effc96a0da50bec5
Grz3s <Grz3s@users.noreply.github.com>
2022-11-22 21:50:40 +0100

    Update for [4426]
    corrected
    
    Co-Authored-By: NeatElves <neatelves@gmail.com>

--- Updates/4426_q.1920_1960.sql
+++ Updates/4426_q.1920_1960.sql
@@ -1,6 +1,6 @@
 -- q.1920 'Investigate the Blue Recluse' - Alliance
 -- q.1960 'Investigate the Alchemist Shop' - Horde
-DELETE FROM dbscripts_on_quest_start WHERE id = 1920;
+DELETE FROM dbscripts_on_quest_start WHERE id IN (1920,1960);
 INSERT INTO dbscripts_on_quest_start (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
 (1920,0,0,0,0,0,0,0,0,2681,0,0,0,0,0,0,0,''),
 (1920,10,3,0,0,0,0,0,0x04,0,0,0,0,0,0,0,6.056,''),
