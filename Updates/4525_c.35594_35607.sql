9dd12556bbe50d5a26d5f93cf634116fbb8772f0
Grz3s <Grz3s@users.noreply.github.com>
2022-12-24 00:12:06 +0100

    [4525] Brassbolt Mechawrench 35594 & Reginald Arcfire 35607
    Fixed
    
    Part of:
    https://github.com/cmangos/wotlk-db/issues/258

---
+++ Updates/4525_c.35594_35607.sql
@@ -0,0 +1,9 @@
+-- Dalaran
+-- Only Alliance Players should see Brassbolt Mechawrench 35594
+-- Only Horde Players should see Reginald Arcfire 35607
+DELETE FROM spell_area WHERE spell IN (60194,60197);
+INSERT INTO spell_area (spell, area, quest_start, quest_start_active, quest_end, condition_id, aura_spell, racemask, gender, autocast) VALUES
+(60194,4395,0,0,0,0,0,1101,2,1), -- alliance Only
+(60197,4395,0,0,0,0,0,690,2,1); -- horde Only
+-- Reginald Arcfire 35607
+UPDATE creature_template SET NpcFlags = 2097153, UnitFlags = 33536 WHERE ENTRY = 35607;
