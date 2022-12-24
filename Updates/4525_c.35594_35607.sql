989467ff3712a21f0d9718f1578014afc03c5275
Grz3s <Grz3s@users.noreply.github.com>
2022-12-24 12:14:52 +0100

    Update for [4525] Portal to Orgrimmar 193948
    Readded
    
    Co-Authored-By: NeatElves <neatelves@gmail.com>

--- Updates/4525_c.35594_35607.sql
+++ Updates/4525_c.35594_35607.sql
@@ -3,6 +3,7 @@
 -- Only Horde Players should see Reginald Arcfire 35607
 DELETE FROM spell_area WHERE spell IN (60194,60197);
 INSERT INTO spell_area (spell, area, quest_start, quest_start_active, quest_end, condition_id, aura_spell, racemask, gender, autocast) VALUES
+(60194,1519,0,0,0,20104,0,0,2,0),
 (60194,4395,0,0,0,0,0,1101,2,1), -- alliance Only
 (60197,4395,0,0,0,0,0,690,2,1); -- horde Only
 -- Reginald Arcfire 35607
