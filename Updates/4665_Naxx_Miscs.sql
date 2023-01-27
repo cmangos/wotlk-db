2b5173b52b7a97925b62f4c26e40dd3f17e76f48
Grz3s <Grz3s@users.noreply.github.com>
2023-01-27 23:28:46 +0100

    Update to [4665] added spellclick for 29912

--- Updates/4665_Naxx_Miscs.sql
+++ Updates/4665_Naxx_Miscs.sql
@@ -25,6 +25,9 @@ UPDATE creature_template SET UnitFlags = 64 WHERE entry IN (16028,29576);
 
 -- Obedience Crystal 29912
 UPDATE creature_template SET NpcFlags = 16777216 WHERE entry IN (29912);
+DELETE FROM npc_spellclick_spells WHERE npc_entry IN(29912);
+INSERT INTO npc_spellclick_spells(npc_entry, spell_id, cast_flags, quest_start, quest_start_active, quest_end, condition_id) VALUES
+(29912, 55479, 0, 0, 0, 0, 0);
 
 -- Sapphiron 15989 / 29991
 UPDATE creature_template SET HealthMultiplier = 390, MinLevelHealth = 5438550, MaxLevelHealth = 5438550 WHERE entry IN (15989);
