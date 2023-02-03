72969071359b5fa799394453471f937dda960063
killerwife <killerwife@gmail.com>
2023-02-03 16:31:21 +0100

    [4671] Naxx: Add spell targeting for kt spell spawning

---
+++ Updates/4671_naxx_kt_targeting.sql
@@ -0,0 +1,7 @@
+DELETE FROM spell_script_target WHERE entry IN(28415,28416,28417,28455);
+INSERT INTO spell_script_target VALUES
+('28415', '1', '15384', '0'),
+('28416', '1', '15384', '0'),
+('28417', '1', '15384', '0'),
+('28455', '1', '17286', '0');
+
