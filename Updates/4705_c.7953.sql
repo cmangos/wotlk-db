d8f5dc5998c7f643a9e3e446e5e1e52a43b37c4d
Grz3s <Grz3s@users.noreply.github.com>
2023-02-24 21:53:31 +0100

    Update for [4705]
    Co-Authored-By: AnonXS <19734826+AnonXS@users.noreply.github.com>

--- Updates/4705_c.7953.sql
+++ Updates/4705_c.7953.sql
@@ -1,6 +1,7 @@
 -- Xar'Ti 7953
 -- gossip corrected
-UPDATE gossip_menu SET condition_id = 810 WHERE entry = 4022 AND text_id = 7953; -- player must be exalted with `Darkspear Trolls`
+UPDATE gossip_menu SET condition_id = 810 WHERE entry = 4022 AND text_id = 4875; -- player must be exalted with `Darkspear Trolls`
+UPDATE gossip_menu_option SET condition_id = 810 WHERE menu_id = 4022;
 UPDATE creature_template_addon SET emote = 0 WHERE entry = 7953; -- emote shouldnt be state
 DELETE FROM dbscripts_on_relay WHERE id = 9995;
 INSERT INTO dbscripts_on_relay (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
