e67df0d75a695542a04c6141373ad88ba7f7660f
Grz3s <Grz3s@users.noreply.github.com>
2023-03-29 13:52:58 +0200

    [4745][ACID] Vanquished Tentacle 15901
    Fixed
    
    Closes:
    https://github.com/cmangos/wotlk-db/issues/547

---
+++ Updates/4745_c.15901.sql
@@ -0,0 +1,2 @@
+-- Vanquished Tentacle 15901 
+UPDATE creature_template SET MinLevel = 60, MaxLevel = 60, MinLevelHealth = 6104, MaxLevelHealth = 6104, MinLevelMana = 0, MaxLevelMana = 0, UnitFlags = 0 WHERE entry IN (15901);
