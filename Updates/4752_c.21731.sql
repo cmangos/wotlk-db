1a25aac55d2814e8ca60fb8a9d4023c1a817c4fc
Grz3s <Grz3s@users.noreply.github.com>
2023-04-06 14:58:12 +0200

    [4752] Encased Electromental 21731
    Stats
    
    Closes:
    https://github.com/cmangos/issues/issues/3332

---
+++ Updates/4752_c.21731.sql
@@ -0,0 +1,2 @@
+-- Encased Electromental 21731
+UPDATE creature_template SET MinLevel = 67, MaxLevel = 67, Faction = 14, MinLevelHealth = 6326, MaxLevelHealth = 6326, UnitFlags = 0 WHERE entry IN (21731);
