6526af1247f901349bcbaa49ed636a70e20775fe
Grz3s <Grz3s@users.noreply.github.com>
2023-04-07 17:35:26 +0200

    Update [4750] Wrong Update removed
    q.11790 'Cultists Among Us' - Quest replaced with new version q.11920 'Cultists Among Us'
    
    q.11789 'A Soldier In Need' is required to start this quest.
    
    Ref:
    https: //github.com/cmangos/issues/issues/3342
    Co-Authored-By: NeatElves <neatelves@gmail.com>

--- Updates/4750_q.11790.sql
+++ Updates/4750_q.11790.sql
@@ -1,10 +1,6 @@
--- q.11790 'Cultists Among Us'
--- Req q.11789 'A Soldier in Need' - completed
-UPDATE quest_template SET RequiredCondition = 20545 WHERE entry = 11790;
+-- Wrong update removed
+-- q.11790 'Cultists Among Us' - Quest replaced with new version q.11920 'Cultists Among Us'
+UPDATE quest_template SET RequiredCondition = 0 WHERE entry = 11790;
 DELETE FROM conditions WHERE condition_entry = 20545;
-INSERT INTO conditions (condition_entry, type, value1, value2, value3, value4) VALUES 
-(20545,8,11789,0,0,0);
 -- quest added to object
 DELETE FROM gameobject_questrelation WHERE quest = 11790;
-INSERT INTO gameobject_questrelation (id, quest) VALUES
-(187851, 11790);
