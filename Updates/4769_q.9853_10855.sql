c0f0207eecf2fb3edd39bf9a2467bccf0f4b3fca
Grz3s <Grz3s@users.noreply.github.com>
2023-04-22 02:29:42 +0200

    [4769] Quests 9853 & 10855
    fixed
    
    Part of:
    https://github.com/cmangos/wotlk-db/issues/486

---
+++ Updates/4769_q.9853_10855.sql
@@ -0,0 +1,5 @@
+-- q.9853 'Gurok the Usurper'
+UPDATE quest_template SET ReqSourceCount2 = 7 WHERE entry = 9853;
+
+-- q.10855 'Fel Reavers, No Thanks!'
+UPDATE quest_template SET ReqSourceCount1 = 5 WHERE entry = 10855;
