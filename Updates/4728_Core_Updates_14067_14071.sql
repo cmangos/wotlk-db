310282e0cc4c73c52b566bb13df26353ca40c5cc
AnonXS <anonxsdev@gmail.com>
2023-04-23 16:56:30 +0200

    [4728] Dont drop reference_loot_template_names table

--- Updates/4728_Core_Updates_14067_14071.sql
+++ Updates/4728_Core_Updates_14067_14071.sql
@@ -1,11 +1,11 @@
 -- 1466
 ALTER TABLE db_version CHANGE COLUMN required_14065_01_mangos_aggro_range required_14066_01_mangos_reference_loot_template_names bit;
-DROP TABLE IF EXISTS `reference_loot_template_names`;
-CREATE TABLE `reference_loot_template_names` (
-  `entry` int unsigned NOT NULL DEFAULT '0',
-  `name` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`entry`)
-)COMMENT='Reference Loot Template Names';
+-- DROP TABLE IF EXISTS `reference_loot_template_names`;
+-- CREATE TABLE `reference_loot_template_names` (
+  -- `entry` int unsigned NOT NULL DEFAULT '0',
+  -- `name` varchar(255) NOT NULL DEFAULT '',
+  -- PRIMARY KEY (`entry`)
+-- )COMMENT='Reference Loot Template Names';
 
 -- 1467
 ALTER TABLE db_version CHANGE COLUMN required_14066_01_mangos_reference_loot_template_names required_14067_01_mangos_addon_stand_state bit;
