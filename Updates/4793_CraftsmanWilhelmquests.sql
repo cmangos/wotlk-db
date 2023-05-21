287155c69d90cf154eef16df7da82acb20b36690
Grz3s <Grz3s@users.noreply.github.com>
2023-05-21 23:03:47 +0200

    [4793] Craftsman Wilhelm quests
    The following quests can be picked up from Craftsman Wilhelm at the Light's Hope Chapel in the Eastern Plaguelands by completing q.9233 'Omarion's Handbook'
    
    Closes:
    https: //github.com/cmangos/wotlk-db/issues/541
    Co-Authored-By: NeatElves <neatelves@gmail.com>

---
+++ Updates/4793_CraftsmanWilhelmquests.sql
@@ -0,0 +1,3 @@
+-- Craftsman Wilhelm quests
+-- The following quests can be picked up from Craftsman Wilhelm at the Light's Hope Chapel in the Eastern Plaguelands by completing q.9233 'Omarion's Handbook'
+UPDATE `quest_template` SET `PrevQuestId`=9233 WHERE `entry` IN (9234,9235,9236,9237,9238,9239,9240,9241,9242,9243,9244,9245,9246);
