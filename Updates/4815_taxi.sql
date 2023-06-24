0a1761ce3d169022b72d4105e3d79672d86a036a
Grz3s <Grz3s@users.noreply.github.com>
2023-06-24 16:42:05 +0200

    [4815] DB errors fixed
    Closes:
    https: //github.com/cmangos/wotlk-db/issues/585
    Co-Authored-By: NeatElves <neatelves@gmail.com>

---
+++ Updates/4815_taxi.sql
@@ -0,0 +1,4 @@
+DELETE FROM `dbscripts_on_gossip` WHERE id IN (978301,978501);
+INSERT INTO `dbscripts_on_gossip` (`id`, `delay`, `priority`, `command`, `datalong`, `datalong2`, `datalong3`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `datafloat`, `x`, `y`, `z`, `o`, `speed`, `condition_id`, `comments`) VALUES
+(978501, 0, 0, 15, 53289, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Flight - Onequah to Light''s Breach'),
+(978301, 0, 0, 15, 53404, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Flight - Zim''Torga to Light''s Breach');
