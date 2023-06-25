16224ba39fb89a14f54aaa984e0b7ffd91139b6c
Fonix <av2zeal@gmail.com>
2023-06-25 11:21:01 -0700

    [4820] Fix loot table for Sealed Scroll Case (i.33926)

---
+++ Updates/4820_sealed_scroll_case_i.33926.sql
@@ -0,0 +1,22 @@
+-- fix loot table for i.33926 (Sealed Scroll Case)
+-- https://web.archive.org/web/20090208155218/http://wow.allakhazam.com/db/item.html?witem=33926#Contains_Items
+
+-- remove refloot since we cannot group refloots
+DELETE FROM `item_loot_template` WHERE entry = 33926 AND item = 50604;
+
+-- add all scrolls V/VI individually to Sealed Scroll Case loot table
+DELETE FROM `item_loot_template` WHERE entry = 33926 AND item IN
+(27498, 27499, 27500, 27501, 27502, 27503, 33457, 33458, 33459, 33460, 33461, 33462);
+INSERT INTO `item_loot_template` (entry, item, ChanceOrQuestChance, groupid, mincountOrRef, maxcount, condition_id, comments) VALUES
+(33926, 27498, 0, 1, 5, 5, 0, 'Scroll of Agility V'),
+(33926, 27499, 0, 1, 5, 5, 0, 'Scroll of Intellect V'),
+(33926, 27500, 0, 1, 5, 5, 0, 'Scroll of Protection V'),
+(33926, 27501, 0, 1, 5, 5, 0, 'Scroll of Spirit V'),
+(33926, 27502, 0, 1, 5, 5, 0, 'Scroll of Stamina V'),
+(33926, 27503, 0, 1, 5, 5, 0, 'Scroll of Strength V'),
+(33926, 33457, 0, 1, 6, 8, 0, 'Scroll of Agility VI'),
+(33926, 33458, 0, 1, 6, 8, 0, 'Scroll of Intellect VI'),
+(33926, 33459, 0, 1, 6, 8, 0, 'Scroll of Protection VI'),
+(33926, 33460, 0, 1, 6, 8, 0, 'Scroll of Spirit VI'),
+(33926, 33461, 0, 1, 6, 8, 0, 'Scroll of Stamina VI'),
+(33926, 33462, 0, 1, 6, 8, 0, 'Scroll of Strength VI');
