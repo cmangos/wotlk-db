11c777edf3ac6ecde0d1e1197d39678387d23d43
Grz3s <Grz3s@users.noreply.github.com>
2023-07-05 16:45:26 +0200

    [4826][ACID] The Avalanche - Sholazar Basin
    Reconstructed
    
    Also:
    4824_c.11073.sql -> 4825_c.11073.sql

--- Updates/4824_c.11073.sql
+++
@@ -1,2 +0,0 @@
--- Annora 11073
-UPDATE creature_template SET EquipmentTemplateId = 845 WHERE entry IN(11073);
---
+++ Updates/4825_c.11073.sql
@@ -0,0 +1,2 @@
+-- Annora 11073
+UPDATE creature_template SET EquipmentTemplateId = 845 WHERE entry IN(11073);
