10b7995ea9ed351a591494d853039409561ce7f9
AnonXS <anonxsdev@gmail.com>
2023-07-16 12:31:23 +0200

    [4814] Revisit RFC CallForHelp / Linking

--- Updates/4814_TDB-0049_rfc_callforhelp.sql
+++ Updates/4814_TDB-0049_rfc_callforhelp.sql
@@ -1,3 +1,4 @@
 -- Reduce default CallForHelp for RFC npcs
-UPDATE `creature_template` SET `CallForHelp` = 5 WHERE `entry` IN (11318,11319,11320,11321,11322,11323,11324);
+UPDATE `creature_template` SET `CallForHelp` = 7 WHERE `entry` IN (11318,11319,11320,11321);
+UPDATE `creature_template` SET `CallForHelp` = 5 WHERE `entry` IN (11322,11323,11324);
 
