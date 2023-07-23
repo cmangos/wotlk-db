b248de119e63ed08f0390e947617ea4a9bc0831b
Grz3s <Grz3s@users.noreply.github.com>
2023-07-24 00:38:39 +0200

    [4855] Fizzcrank Mechagnome 25814 - Random Texts on Aggro - update
    more added
    
    Closes:
    https://github.com/cmangos/wotlk-db/issues/635

---
+++ Updates/4855_c.25814_aggro_texts.sql
@@ -0,0 +1,12 @@
+-- Fizzcrank Mechagnome 25814 - Random Texts on Aggro - update
+DELETE FROM dbscript_random_templates WHERE id = 20086;
+INSERT INTO dbscript_random_templates (id, type, target_id, chance, comments) VALUES
+('20086','0','25000','0','Fizzcrank Mechagnome 25814 - Random Texts on Aggro'),
+('20086','0','25001','0','Fizzcrank Mechagnome 25814 - Random Texts on Aggro'),
+('20086','0','25002','0','Fizzcrank Mechagnome 25814 - Random Texts on Aggro'),
+('20086','0','25003','0','Fizzcrank Mechagnome 25814 - Random Texts on Aggro'),
+('20086','0','25004','0','Fizzcrank Mechagnome 25814 - Random Texts on Aggro'),
+('20086','0','25005','0','Fizzcrank Mechagnome 25814 - Random Texts on Aggro'),
+('20086','0','25006','0','Fizzcrank Mechagnome 25814 - Random Texts on Aggro'),
+('20086','0','25007','0','Fizzcrank Mechagnome 25814 - Random Texts on Aggro'),
+('20086','0','25008','0','Fizzcrank Mechagnome 25814 - Random Texts on Aggro');
