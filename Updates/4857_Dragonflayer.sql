51aa0ec43aac9ef92a33409537320c1cde029e41
Grz3s <Grz3s@users.noreply.github.com>
2023-07-24 01:07:32 +0200

    [4857] Common Dragonflayer Text
    1 more added
    
    Closes:
    https://github.com/cmangos/wotlk-db/issues/627

---
+++ Updates/4857_Dragonflayer.sql
@@ -0,0 +1,4 @@
+-- Common Dragonflayer Text - Random Texts on Aggro - 1 more added
+DELETE FROM `dbscript_random_templates` WHERE `id` = 20087 AND target_id = 22702;
+INSERT INTO `dbscript_random_templates` (`id`, `type`, `target_id`, `chance`, `comments`) VALUES
+('20087','0','22702','0','Common Dragonflayer Text - Random Texts on Aggro');
