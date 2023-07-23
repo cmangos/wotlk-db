a91fbd105271034aec41f130a4a769b91cfc1466
Grz3s <Grz3s@users.noreply.github.com>
2023-07-24 01:29:54 +0200

    typo

--- Updates/4856_c.25806_25979_aggro_texts.sql
+++ Updates/4856_c.25806_25979_aggro_texts.sql
@@ -1,5 +1,5 @@
 -- 25806/25979 - Random Aggro Texts - update
-DELETE FROM `dbscript_random_templates` WHERE `id` = 20086;
+DELETE FROM `dbscript_random_templates` WHERE `id` = 20031;
 INSERT INTO `dbscript_random_templates` (`id`, `type`, `target_id`, `chance`, `comments`) VALUES
 ('20031','0','25016','0','25806/25979 - Random Aggro Texts'),
 ('20031','0','25017','0','25806/25979 - Random Aggro Texts'),
