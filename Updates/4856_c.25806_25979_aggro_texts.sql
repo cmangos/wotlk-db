c0aba3cd0e386f05c7cadf3afd4558b5b0f82643
Grz3s <Grz3s@users.noreply.github.com>
2023-07-24 00:58:32 +0200

    [4856] 25806/25979 - Random Aggro Texts - update
    more added
    
    Closes:
    https://github.com/cmangos/wotlk-db/issues/633

---
+++ Updates/4856_c.25806_25979_aggro_texts.sql
@@ -0,0 +1,17 @@
+-- 25806/25979 - Random Aggro Texts - update
+DELETE FROM `dbscript_random_templates` WHERE `id` = 20086;
+INSERT INTO `dbscript_random_templates` (`id`, `type`, `target_id`, `chance`, `comments`) VALUES
+('20031','0','25016','0','25806/25979 - Random Aggro Texts'),
+('20031','0','25017','0','25806/25979 - Random Aggro Texts'),
+('20031','0','25018','0','25806/25979 - Random Aggro Texts'),
+('20031','0','25019','0','25806/25979 - Random Aggro Texts'),
+('20031','0','25020','0','25806/25979 - Random Aggro Texts'),
+('20031','0','25021','0','25806/25979 - Random Aggro Texts'),
+('20031','0','25022','0','25806/25979 - Random Aggro Texts'),
+('20031','0','25023','0','25806/25979 - Random Aggro Texts'),
+('20031','0','25113','0','25806/25979 - Random Aggro Texts'),
+('20031','0','25114','0','25806/25979 - Random Aggro Texts'),
+('20031','0','25115','0','25806/25979 - Random Aggro Texts'),
+('20031','0','25116','0','25806/25979 - Random Aggro Texts'),
+('20031','0','25117','0','25806/25979 - Random Aggro Texts'),
+('20031','0','25118','0','25806/25979 - Random Aggro Texts');
