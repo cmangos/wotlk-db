28a28d0577412996af23493d5a3c3a3a5be210c8
Grz3s <Grz3s@users.noreply.github.com>
2023-07-24 00:31:54 +0200

    [4854] Magmoth Kobold - Common Text - update
    1 corrected & 1 more added
    
    Closes:
    https://github.com/cmangos/wotlk-db/issues/638

---
+++ Updates/4854_MagmothKobold_texts.sql
@@ -0,0 +1,7 @@
+-- Magmoth Kobold - Common Text - update
+DELETE FROM dbscript_random_templates WHERE id = 20076;
+INSERT INTO dbscript_random_templates (id, type, target_id, chance, comments) VALUES
+('20076','0','24649','0','Magmoth Kobold - Common Text - Random Texts on Aggro'),
+('20076','0','24650','0','Magmoth Kobold - Common Text - Random Texts on Aggro'),
+('20076','0','24651','0','Magmoth Kobold - Common Text - Random Texts on Aggro'),
+('20076','0','24652','0','Magmoth Kobold - Common Text - Random Texts on Aggro');
