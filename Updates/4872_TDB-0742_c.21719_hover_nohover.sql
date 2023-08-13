7f640613e7eb5b12ee9902600995fd55a8ba27c6
AnonXS <anonxsdev@gmail.com>
2023-08-13 12:15:53 +0200

    [4872][DBError] Remove equipment_id

--- Updates/4872_TDB-0742_c.21719_hover_nohover.sql
+++ Updates/4872_TDB-0742_c.21719_hover_nohover.sql
@@ -4,16 +4,16 @@
 -- some are seen to sometimes have hover at home location, could do this with OOC disable hover, not sure what is correct.
 UPDATE `creature_template` SET `ExtraFlags` = `ExtraFlags`|1048576, `InhabitType` = 1, `MovementType` = 0 WHERE `entry` = 21719; -- dynguid, no fly
 DELETE FROM `creature` WHERE `id` = 21719;
-INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `spawndist`, `MovementType`) VALUES
-(5304073, 21719, 530, 1, 0, -4072.34, 361.866, 64.5531, 2.04204, 300, 300, 0, 0),
-(5304074, 21719, 530, 1, 0, -4155.55, 386.615, 141.415, 1.20428, 300, 300, 0, 0),
-(5304075, 21719, 530, 1, 0, -4218.88, 381.402, 135.627, 1.44862, 300, 300, 0, 0),
-(5304076, 21719, 530, 1, 0, -4117.26, 452.178, 57.7584, 4.85202, 300, 300, 0, 0),
-(5304077, 21719, 530, 1, 0, -4141.86, 416.067, 53.6638, 0.488692, 300, 300, 0, 0),
-(5304078, 21719, 530, 1, 0, -4230.58, 489.843, 43.7573, 0.261799, 300, 300, 0, 0),
-(5304079, 21719, 530, 1, 0, -4271.06, 509.687, 75.0461, 1.50098, 300, 300, 0, 0),
-(5304080, 21719, 530, 1, 0, -4209.37, 535.747, 77.3108, 0.349066, 300, 300, 0, 0),
-(5304081, 21719, 530, 1, 0, -4154.23, 492.462, 59.1349, 2.84489, 300, 300, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `spawndist`, `MovementType`) VALUES
+(5304073, 21719, 530, 1, -4072.34, 361.866, 64.5531, 2.04204, 300, 300, 0, 0),
+(5304074, 21719, 530, 1, -4155.55, 386.615, 141.415, 1.20428, 300, 300, 0, 0),
+(5304075, 21719, 530, 1, -4218.88, 381.402, 135.627, 1.44862, 300, 300, 0, 0),
+(5304076, 21719, 530, 1, -4117.26, 452.178, 57.7584, 4.85202, 300, 300, 0, 0),
+(5304077, 21719, 530, 1, -4141.86, 416.067, 53.6638, 0.488692, 300, 300, 0, 0),
+(5304078, 21719, 530, 1, -4230.58, 489.843, 43.7573, 0.261799, 300, 300, 0, 0),
+(5304079, 21719, 530, 1, -4271.06, 509.687, 75.0461, 1.50098, 300, 300, 0, 0),
+(5304080, 21719, 530, 1, -4209.37, 535.747, 77.3108, 0.349066, 300, 300, 0, 0),
+(5304081, 21719, 530, 1, -4154.23, 492.462, 59.1349, 2.84489, 300, 300, 0, 0);
 
 DELETE FROM dbscripts_on_relay WHERE id IN (30611);
 INSERT INTO dbscripts_on_relay (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
