fa519c051cac6bc2b6d08e08d9f6857ee715fb3b
Grz3s <Grz3s@users.noreply.github.com>
2023-08-24 22:25:09 +0200

    [4881] Lieutenant Karter 12783
    corrected
    
    Closes:
    https://github.com/cmangos/wotlk-db/issues/730

---
+++ Updates/4881_c.12783.sql
@@ -0,0 +1,3 @@
+-- Lieutenant Karter 12783
+UPDATE creature SET position_x = -8753.4375, position_y = 400.25348, position_z = 101.13949, orientation = 5.0265 WHERE id = 12783;
+UPDATE creature_template_addon SET mount = 14337 WHERE entry = 12783;
