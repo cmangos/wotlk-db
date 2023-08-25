014ff7847b158021816168157ccbb06efee68443
Grz3s <Grz3s@users.noreply.github.com>
2023-08-26 00:05:48 +0200

    [4884][ACID] Guard Lasiter 4973 - Update
    script added
    must have always same model - fixed
    
    Closes:
    https://github.com/cmangos/wotlk-db/issues/775

---
+++ Updates/4884_c.4973.sql
@@ -0,0 +1,10 @@
+-- Guard Lasiter 4973 - Update
+UPDATE creature_movement_template SET ScriptId = 497301 WHERE Entry = 4973 AND point = 1;
+DELETE FROM dbscripts_on_creature_movement WHERE id = 497301;
+INSERT INTO dbscripts_on_creature_movement (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
+(497301,1,31,5202,25,0,0,0,0,0,0,0,0,0,0,0,0,'4973 - search for 5202'),
+(497301,10,36,0,0,0,5202,30,1,0,0,0,0,0,0,0,0,'4973 - face buddy'),
+(497301,11,54,2,0,0,0,0,0x04,0,0,0,0,0,0,0,0,'4973 - set SHEATH_STATE_Ranged(2)'),
+(497301,100,15,42131,0,0,5202,30,1,0,0,0,0,0,0,0,0,'4973 - cast 42131 on buddy'),
+(497301,10000,0,0,0,0,0,0,0x04,1789,1790,1791,1792,0,0,0,0,'4973 - random Say'),
+(497301,10100,54,0,0,0,0,0,0x04,0,0,0,0,0,0,0,0,'4973 - set SHEATH_STATE_UNARMED(0)');
