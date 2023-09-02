e2426039a71690941d18cae941ef1287938ce5dd
Grz3s <Grz3s@users.noreply.github.com>
2023-09-02 23:26:18 +0200

    [4900] q.10169 'Losing Gracefully'
    Removed in 2.4
    
    + typo fixed in [4899]
    
    Closes:
    https://github.com/cmangos/wotlk-db/issues/827

--- Updates/4899_q.12569.sql
+++ Updates/4899_q.12569.sql
@@ -4,7 +4,7 @@ INSERT INTO dbscripts_on_event (id, delay, command, datalong, datalong2, datalon
 (18503,1,10,28317,300000,0,0,0,0x08,0,0,0,0,5421.839,4867.183,-199.7485,1.518436,'summon: 28317');
 DELETE FROM dbscripts_on_relay WHERE id = 20772;
 INSERT INTO dbscripts_on_relay (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
-(20772,1,25,1,0,0,0,0,2,0,0,0,0,0,0,0,0,'Part of Bushwhacker 28317 EAI: move towards summoner'),
+(20772,1,25,1,0,0,0,0,2,0,0,0,0,0,0,0,0,'Part of Bushwhacker 28317 EAI: Run ON'),
 (20772,100,37,0,0,0,0,0,2,0,0,0,0,0,0,0,0,'Part of Bushwhacker 28317 EAI: move towards summoner'),
 (20772,10000,0,0,0,0,0,0,0,27731,0,0,0,0,0,0,0,'Part of Bushwhacker 28317 EAI: emote'),
 (20772,10001,1,50,0,0,0,0,6,0,0,0,0,0,0,0,0,'Part of Bushwhacker 28317 EAI: emote'),
