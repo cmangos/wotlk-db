304eaaeca90412664022e3a2f7242d14a6590a40
Grz3s <Grz3s@users.noreply.github.com>
2023-10-09 00:22:22 +0200

    Update [5120] ...
    wrong data removed
    missing spell will be added soon

--- Updates/5120_Backport_FiX.sql
+++ Updates/5120_Backport_FiX.sql
@@ -2,7 +2,3 @@
 -- moved to spawn groups
 DELETE FROM `pool_template` WHERE `entry` IN (25724);
 DELETE FROM `pool_creature_template` WHERE `pool_entry` IN (25724);
-
--- Creature (Entry: 24557) has wrong spell 44288 defined in `auras` field in `creature_template_addon`.
--- ss autra that triggers another spell in combat - will add it when correct spell found.
-UPDATE creature_template_addon SET auras = '42459' WHERE entry = 24557;
