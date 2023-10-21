9df07c140367ef0d067ed99fcf400d13b4f78232
Grz3s <Grz3s@users.noreply.github.com>
2023-10-21 20:15:54 +0200

    Update [5125][5126]
    Mild Spices 2678 - returned :)
    
    Thx to Karth for pointing:
    Ref:
    https://github.com/cmangos/wotlk-db/commit/2da733d38c586227a89125aa22b96269ac4918aa

--- Updates/5125_spices.sql
+++ Updates/5125_spices.sql
@@ -2,6 +2,5 @@
 -- As of 3.1, looks like these spices have actually been removed from the game
 -- Soothing Spices 3713
 -- Hot Spices 2692
--- Mild Spices 2678
-DELETE FROM npc_vendor WHERE item IN (2678,2692,3713);
-DELETE FROM npc_vendor_template WHERE item IN (2678,2692,3713);
+DELETE FROM npc_vendor WHERE item IN (2692,3713);
+DELETE FROM npc_vendor_template WHERE item IN (2692,3713);
--- Updates/5126_c.3400.sql
+++ Updates/5126_c.3400.sql
@@ -3,5 +3,6 @@
 -- 3.3.5 only (he has more items before 3.1)
 UPDATE npc_vendor SET Slot = 0 WHERE entry = 3400 AND item = 159; -- Refreshing Spring Water
 UPDATE npc_vendor SET Slot = 1 WHERE entry = 3400 AND item = 30817; -- Simple Flour
-UPDATE npc_vendor SET Slot = 2 WHERE entry = 3400 AND item = 21099; -- Recipe: Smoked Sagefish
-UPDATE npc_vendor SET Slot = 3 WHERE entry = 3400 AND item = 21219; -- Recipe: Sagefish Delight
+UPDATE npc_vendor SET Slot = 2 WHERE entry = 3400 AND item = 2678; -- Mild Spices
+UPDATE npc_vendor SET Slot = 3 WHERE entry = 3400 AND item = 21099; -- Recipe: Smoked Sagefish
+UPDATE npc_vendor SET Slot = 4 WHERE entry = 3400 AND item = 21219; -- Recipe: Sagefish Delight
