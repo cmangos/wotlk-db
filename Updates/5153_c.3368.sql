74345e176e44f747f078e183e406c86ca1edb1cf
Grz3s <Grz3s@users.noreply.github.com>
2023-10-26 00:54:36 +0200

    [5153] Borstan 3368
    Vendor list corrected

---
+++ Updates/5153_c.3368.sql
@@ -0,0 +1,11 @@
+-- Borstan 3368
+-- Vendor list corrected 
+UPDATE npc_vendor SET Slot = 0 WHERE entry = 3368 AND item = 117; -- Tough Jerky
+UPDATE npc_vendor SET Slot = 1 WHERE entry = 3368 AND item = 2287; -- Haunch of Meat
+UPDATE npc_vendor SET Slot = 2 WHERE entry = 3368 AND item = 3770; -- Mutton Chop
+UPDATE npc_vendor SET Slot = 3 WHERE entry = 3368 AND item = 3771; -- Wild Hog Shank
+UPDATE npc_vendor SET Slot = 4 WHERE entry = 3368 AND item = 4599; -- Cured Ham Steak
+UPDATE npc_vendor SET Slot = 5 WHERE entry = 3368 AND item = 8952; -- Roasted Quail
+UPDATE npc_vendor SET Slot = 6 WHERE entry = 3368 AND item = 27854; -- Smoked Talbuk Venison
+UPDATE npc_vendor SET Slot = 7 WHERE entry = 3368 AND item = 33454; -- Salted Venison
+UPDATE npc_vendor SET Slot = 8 WHERE entry = 3368 AND item = 35953; -- Mead Basted Caribou
