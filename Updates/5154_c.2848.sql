a98f756cba0212b8e537a77db41c2cba2ea76616
Grz3s <Grz3s@users.noreply.github.com>
2023-10-26 02:10:54 +0200

    [5154] Glyx Brewright 2848
    Vendor list corrected

---
+++ Updates/5154_c.2848.sql
@@ -0,0 +1,11 @@
+-- Glyx Brewright 2848
+-- Vendor list corrected 
+UPDATE npc_vendor SET Slot = 5 WHERE entry = 2848 AND item = 1710; -- Greater Healing Potion
+UPDATE npc_vendor SET Slot = 0 WHERE entry = 2848 AND item = 3371; -- Empty Vial
+UPDATE npc_vendor SET Slot = 1 WHERE entry = 2848 AND item = 3372; -- Leaded Vial
+UPDATE npc_vendor SET Slot = 6 WHERE entry = 2848 AND item = 3827; -- Mana Potion
+UPDATE npc_vendor SET Slot = 8 WHERE entry = 2848 AND item = 6056; -- Recipe: Frost Protection Potion
+UPDATE npc_vendor SET Slot = 7 WHERE entry = 2848 AND item = 6057; -- Recipe: Nature Protection Potion
+UPDATE npc_vendor SET Slot = 2 WHERE entry = 2848 AND item = 8925; -- Crystal Vial
+UPDATE npc_vendor SET Slot = 3 WHERE entry = 2848 AND item = 18256; -- Imbued Vial
+UPDATE npc_vendor SET Slot = 4 WHERE entry = 2848 AND item = 40411; -- Enchanted Vial
