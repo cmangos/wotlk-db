a5697bad928239af0a7884dfcda7c701030237ce
Grz3s <Grz3s@users.noreply.github.com>
2023-10-28 23:32:42 +0200

    [5157] Flint and Tinder 4471
    Removed from vendor list in 3.1.x

---
+++ Updates/5157_i.4471.sql
@@ -0,0 +1,4 @@
+-- Flint and Tinder 4471
+-- Removed from vendor list in 3.1.x
+-- WoWhead: (...) I can confirm from over 50 checked Alliance Trade goods' vendors which used to sell it that they are not selling it anymore(...)
+DELETE FROM npc_vendor WHERE item IN (4471);
