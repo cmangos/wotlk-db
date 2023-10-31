76c9c400d812a830c0562e9b9078bd29a27ff9a7
Grz3s <Grz3s@users.noreply.github.com>
2023-10-31 20:02:45 +0100

    typo
    Closes:
    https://github.com/cmangos/issues/issues/3591

--- Updates/5163_c.4855.sql
+++ Updates/5163_c.4855.sql
@@ -4,4 +4,4 @@
 [5] Run Speed: 8
 */
 -- corrected
-UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7), WHERE Entry = 4855;
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 4855;
