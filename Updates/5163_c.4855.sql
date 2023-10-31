c3b8940b7231d2e1784121b02f405b60ff07dab4
Grz3s <Grz3s@users.noreply.github.com>
2023-10-31 01:00:57 +0100

    [5163] Stonevault Brawler 4855
    Speed corrected
    
    Part of:
    https://github.com/cmangos/wotlk-db/issues/1001

---
+++ Updates/5163_c.4855.sql
@@ -0,0 +1,7 @@
+-- Stonevault Brawler 4855
+/*
+[5] Walk Speed: 2.5
+[5] Run Speed: 8
+*/
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7), WHERE Entry = 4855;
