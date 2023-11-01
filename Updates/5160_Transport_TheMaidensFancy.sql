4aae49f2fc5fc1a43e53e01775f03bd9c647aafe
Grz3s <Grz3s@users.noreply.github.com>
2023-11-02 00:04:37 +0100

    Update [5160] transport: The Maiden's Fancy
    non existing in original 3.x.x objects removed

--- Updates/5160_Transport_TheMaidensFancy.sql
+++ Updates/5160_Transport_TheMaidensFancy.sql
@@ -192,3 +192,7 @@ INSERT INTO dbscripts_on_event (id, delay, command, datalong, datalong2, datalon
 (16397,2,31,25078,100,0,0,0,0,0,0,0,0,0,0,0,0,'Transports (Ships) - search for 25078'),
 (16397,500,20,2,1,0,25078,100,0x04,0,0,0,0,0,0,0,0,'Transports (Ships) - Captain Krick Wrenchnozzle 25078 - Path 1'),
 (16397,1000,16,5154,16,0,0,0,0,0,0,0,0,0,0,0,0,'Transports (Ships) - Play - ShipDocked');
+
+-- objects
+-- they did not exist during 3.x.x
+DELETE FROM gameobject WHERE map = 593;
