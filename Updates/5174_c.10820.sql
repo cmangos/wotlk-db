dbda3b79686db1cec8e9c01d11689b1a3d2dd897
Grz3s <Grz3s@users.noreply.github.com>
2023-11-12 20:03:07 +0100

    [5174] Duke Ragereaver 10820
    stats & equip

---
+++ Updates/5174_c.10820.sql
@@ -0,0 +1,4 @@
+-- Duke Ragereaver 10820
+-- Data taken from TDB
+-- equip data taken from TC
+UPDATE creature_template SET MinLevel = 45, MaxLevel = 45, Faction = 16, SpeedWalk = 1.2, Detection = 18, HealthMultiplier = 1.5, DamageMultiplier = 1.25, EquipmentTemplateId = 1932 WHERE entry = 10820;
