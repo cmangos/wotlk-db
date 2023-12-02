63bb9e8bc0ca1912059f4ddecd814ffb2b7ee0b0
Grz3s <Grz3s@users.noreply.github.com>
2023-12-03 00:45:38 +0100

    [5207] q.11672 'Enlistment Day' - update
    source of text corrected

---
+++ Updates/5207_q.11672.sql
@@ -0,0 +1,3 @@
+-- q.11672 'Enlistment Day' - update
+-- source of text corrected
+UPDATE dbscripts_on_quest_start SET buddy_entry = 25317, search_radius = 508540, data_flags = 0x10 WHERE id = 11672;
