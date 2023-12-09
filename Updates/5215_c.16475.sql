a9b15f582bae3f9de045ae6a673d90aba8656f96
Grz3s <Grz3s@users.noreply.github.com>
2023-12-10 00:42:51 +0100

    Update [5215] ...
    They moved him with 3.x.x launch and kept it like that till 3.3.5 ...
    3.3.5 sniff used for correct possition
    Reff:
    https://github.com/cmangos/wotlk-db/issues/1296

--- Updates/5215_c.16475.sql
+++ Updates/5215_c.16475.sql
@@ -1,3 +1,4 @@
 -- Megelon 16475
--- poss corrected
-UPDATE creature SET position_x = -3985.072, position_y = -13877.06, position_z = 92.04234, orientation = 5.131268, movementtype = 0, spawndist = 0 WHERE id = 16475;
+-- poss corrected 
+-- 3.3.5 sniff
+UPDATE creature SET position_x = -3961.6963, position_y = -13926.944, position_z = 100.97172, orientation = 4.537856, movementtype = 0, spawndist = 0 WHERE id = 16475;
