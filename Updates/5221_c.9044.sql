055cf618066b2f5f84ef1ffc6f9e916ef06cccce
Grz3s <Grz3s@users.noreply.github.com>
2023-12-13 02:37:11 +0100

    [5221][ACID] Scarshield Sentry 9044
    missing data added

---
+++ Updates/5221_c.9044.sql
@@ -0,0 +1,4 @@
+-- Scarshield Sentry 9044
+DELETE FROM creature_template_addon WHERE entry IN (9044);
+INSERT INTO creature_template_addon (entry, mount, stand_state, sheath_state, pvp_flags, emote, moveflags, auras) VALUES
+(9044,0,0,1,16,0,0,8278);
