a6368381a67d741e695dc5d40d66e3725a03a5a8
LaX <rmzV@users.noreply.github.com>
2024-01-14 14:19:27 +0000

    Add files via upload

---
+++ locales/Russian/updates.sql
@@ -0,0 +1,6 @@
+-- Update file must be applied after Localization DB installed
+-- creature (NPC) part
+-- https://www.wowhead.com/wotlk/ru/npc=entry
+
+UPDATE locales_creature SET subname_loc8 = '\"Железный орел\"' WHERE entry = 24929;
+
