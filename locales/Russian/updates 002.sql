41e1f05f94d71de627763b2e4a5d4addc42ad4b6
LaX <rmzV@users.noreply.github.com>
2024-01-26 11:22:02 +0000

    Add files via upload

---
+++ locales/Russian/updates 002.sql
@@ -0,0 +1,69 @@
+-- Update file must be applied after Localization DB installed
+-- creature (NPC) part
+-- https://www.wowhead.com/wotlk/ru/npc=entry
+
+UPDATE locales_creature SET subname_loc8 = '\"Железный орел\"' WHERE entry = 24929;
+
+-- item (item) part
+-- https://www.wowhead.com/wotlk/ru/item=entry
+
+UPDATE locales_item SET description_loc8 = 'Вся кромка лезвия испещрена зазубринами.' WHERE entry = 45947;
+UPDATE locales_item SET description_loc8 = 'Путь защитника' WHERE entry = 21196;
+UPDATE locales_item SET description_loc8 = 'Путь защитника' WHERE entry = 21197;
+UPDATE locales_item SET description_loc8 = 'Путь защитника' WHERE entry = 21198;
+UPDATE locales_item SET description_loc8 = 'Путь защитника' WHERE entry = 21199;
+UPDATE locales_item SET description_loc8 = 'Путь защитника' WHERE entry = 21200;
+UPDATE locales_item SET description_loc8 = 'Путь победителя' WHERE entry = 21201;
+UPDATE locales_item SET description_loc8 = 'Путь победителя' WHERE entry = 21202;
+UPDATE locales_item SET description_loc8 = 'Путь победителя' WHERE entry = 21203;
+UPDATE locales_item SET description_loc8 = 'Путь победителя' WHERE entry = 21204;
+UPDATE locales_item SET description_loc8 = 'Путь победителя' WHERE entry = 21205;
+UPDATE locales_item SET description_loc8 = 'Путь заклинателя' WHERE entry = 21206;
+UPDATE locales_item SET description_loc8 = 'Путь заклинателя' WHERE entry = 21207;
+UPDATE locales_item SET description_loc8 = 'Путь заклинателя' WHERE entry = 21208;
+UPDATE locales_item SET description_loc8 = 'Путь заклинателя' WHERE entry = 21209;
+UPDATE locales_item SET description_loc8 = 'Путь заклинателя' WHERE entry = 21210;
+UPDATE locales_item SET description_loc8 = 'Переливается темными рунами.' WHERE entry = 11732;
+UPDATE locales_item SET description_loc8 = 'Переливается темными рунами.' WHERE entry = 11733;
+UPDATE locales_item SET description_loc8 = 'Переливается темными рунами.' WHERE entry = 11734;
+UPDATE locales_item SET description_loc8 = 'Переливается темными рунами.' WHERE entry = 11736;
+UPDATE locales_item SET description_loc8 = 'Переливается темными рунами.' WHERE entry = 11737;
+UPDATE locales_item SET description_loc8 = 'Переливается темными рунами.' WHERE entry = 18332;
+UPDATE locales_item SET description_loc8 = 'Переливается темными рунами.' WHERE entry = 18333;
+UPDATE locales_item SET description_loc8 = 'Переливается темными рунами.' WHERE entry = 18334;
+UPDATE locales_item SET description_loc8 = 'Голова матери черных драконов' WHERE entry = 49643;
+UPDATE locales_item SET description_loc8 = 'Голова матери черных драконов' WHERE entry = 49644;
+UPDATE locales_item SET description_loc8 = 'Почетный член ордена Мифрила' WHERE entry = 8703;
+UPDATE locales_item SET description_loc8 = 'Утягивающий эффект и укрепление!' WHERE entry = 18258;
+UPDATE locales_item SET description_loc8 = 'Переливается темными рунами.' WHERE entry = 19022;
+UPDATE locales_item SET description_loc8 = 'Ограниченный выпуск' WHERE entry = 31404;
+UPDATE locales_item SET description_loc8 = 'Нервущиеся путы' WHERE entry = 11286;
+UPDATE locales_item SET description_loc8 = 'ОСТОРОЖНО! Призраки не любят, когда их замечают.' WHERE entry = 30721;
+UPDATE locales_item SET description_loc8 = 'Этот предмет, когда-то принадлежавший караборскому священнику, позволяет войти в Черный храм.' WHERE entry = 32649;
+UPDATE locales_item SET description_loc8 = 'Собственность лучшего орка' WHERE entry = 32863;
+UPDATE locales_item SET description_loc8 = 'Собственность лучшего орка' WHERE entry = 37311;
+UPDATE locales_item SET description_loc8 = 'Чтобы уничтожить стержень Бездны нужно будет несколько раз использовать этот прибор.' WHERE entry = 29778;
+UPDATE locales_item SET description_loc8 = 'В оке клубится тьма.' WHERE entry = 18665;
+UPDATE locales_item SET description_loc8 = 'На внутренней стороне воротника написано: "Проинспектировано О. Б. Легченным".' WHERE entry = 18231;
+UPDATE locales_item SET description_loc8 = 'Владелец явно очень любил эту игрушку, долго она не протянет.' WHERE entry = 31666;
+UPDATE locales_item SET description_loc8 = 'Оружие закалено и усилено элементием.' WHERE entry = 21273;
+UPDATE locales_item SET description_loc8 = 'Оружие закалено и усилено элементием.' WHERE entry = 21275;
+
+
+UPDATE locales_item SET description_loc8 = 'Для прилежных начинающих охотников на ос.' WHERE entry = 38621;
+UPDATE locales_item SET description_loc8 = 'Око Командования' WHERE entry = 46839;
+UPDATE locales_item SET description_loc8 = 'Око Командования' WHERE entry = 46840;
+UPDATE locales_item SET description_loc8 = 'Око Командования' WHERE entry = 17904;
+UPDATE locales_item SET description_loc8 = 'Око Командования' WHERE entry = 17909;
+UPDATE locales_item SET description_loc8 = 'Довольно старый.' WHERE entry = 38624;
+UPDATE locales_item SET description_loc8 = 'Там что-то шевелится!' WHERE entry = 38622;
+UPDATE locales_item SET description_loc8 = 'Служит материалом для создания свитка наложения чар на оружие для последующего использования. Подходит только для чар, не имеющих огранич' WHERE entry = 39349;
+UPDATE locales_item SET description_loc8 = 'Служит материалом для создания свитка наложения чар на оружие для последующего использования. Подходит только для чар, требующих предмет' WHERE entry = 39350;
+UPDATE locales_item SET description_loc8 = 'Служит материалом для создания свитка наложения чар на оружие для последующего использования. Подходит только для чар, требующих предмет ' WHERE entry = 43146;
+UPDATE locales_item SET description_loc8 = 'Включает в себя искусно отточенные перья и высококачественные хрустальные пузырьки, которые сделают честь любому начертателю.' WHERE entry = 39505;
+
+
+UPDATE locales_item SET description_loc8 = 'Для желтого или красного гнезда.' WHERE entry = 31874;
+UPDATE locales_item SET description_loc8 = 'Ослабляет Аража Призывателя.' WHERE entry = 12650;
+UPDATE locales_item SET description_loc8 = 'Регалии Киражской Знати. Используется заклинателями' WHERE entry = 21237;
+UPDATE locales_item SET description_loc8 = 'Церемониальное вооружение Владык Киражей. Используется киражской пехотой.' WHERE entry = 21232;
