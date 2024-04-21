1496cd38c44b48ac171638dadeed1f09710800ec
killerwife <killerwife@gmail.com>
2024-04-21 23:26:53 +0200

    [5328] Add mind control resistances

---
+++ Updates/5328_resistances.sql
@@ -0,0 +1,7 @@
+UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNature=0, ResistanceFrost=0, ResistanceShadow=91, ResistanceArcane=0 WHERE Entry=25378;
+UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNature=0, ResistanceFrost=200, ResistanceShadow=0, ResistanceArcane=0 WHERE Entry=23677;
+UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNature=0, ResistanceFrost=0, ResistanceShadow=15, ResistanceArcane=0 WHERE Entry=19732;
+UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNature=0, ResistanceFrost=0, ResistanceShadow=16, ResistanceArcane=0 WHERE Entry=19994;
+UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNature=0, ResistanceFrost=0, ResistanceShadow=-80, ResistanceArcane=0 WHERE Entry=16803;
+UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNature=0, ResistanceFrost=200, ResistanceShadow=0, ResistanceArcane=0 WHERE Entry=26456;
+UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNature=0, ResistanceFrost=0, ResistanceShadow=20, ResistanceArcane=0 WHERE Entry=29614;
