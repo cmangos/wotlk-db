ab46b27c5de8a10fa8be014f2e1d658978e427a1
killerwife <killerwife@gmail.com>
2024-04-22 14:22:55 +0200

    [5329] More CLS, spell lists and resists

--- Updates/5328_resistances.sql
+++ Updates/5328_resistances.sql
@@ -5,3 +5,17 @@ UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNatur
 UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNature=0, ResistanceFrost=0, ResistanceShadow=-80, ResistanceArcane=0 WHERE Entry=16803;
 UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNature=0, ResistanceFrost=200, ResistanceShadow=0, ResistanceArcane=0 WHERE Entry=26456;
 UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNature=0, ResistanceFrost=0, ResistanceShadow=20, ResistanceArcane=0 WHERE Entry=29614;
+
+
+UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNature=0, ResistanceFrost=0, ResistanceShadow=3, ResistanceArcane=0 WHERE Entry=436;
+UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNature=0, ResistanceFrost=0, ResistanceShadow=50, ResistanceArcane=0 WHERE Entry=432;
+UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNature=0, ResistanceFrost=0, ResistanceShadow=50, ResistanceArcane=0 WHERE Entry=568;
+UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNature=0, ResistanceFrost=0, ResistanceShadow=50, ResistanceArcane=0 WHERE Entry=429;
+UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNature=0, ResistanceFrost=0, ResistanceShadow=50, ResistanceArcane=0 WHERE Entry=431;
+UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNature=0, ResistanceFrost=0, ResistanceShadow=56, ResistanceArcane=0 WHERE Entry=533;
+UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNature=0, ResistanceFrost=0, ResistanceShadow=60, ResistanceArcane=0 WHERE Entry=920;
+UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNature=0, ResistanceFrost=0, ResistanceShadow=118, ResistanceArcane=0 WHERE Entry=2642;
+UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNature=0, ResistanceFrost=0, ResistanceShadow=250, ResistanceArcane=0 WHERE Entry=2647;
+UPDATE creature_template SET ResistanceHoly=0, ResistanceFire=0, ResistanceNature=0, ResistanceFrost=0, ResistanceShadow=0, ResistanceArcane=78 WHERE Entry=7034;
+
+
