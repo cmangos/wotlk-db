9f43bf71182ec56fafe74dd8c4d7ba00ed1cef63
killerwife <killerwife@gmail.com>
2024-04-23 08:56:27 +0200

    [5326] Add kelgash the wicked charm immunity

--- Updates/5326_charm_immune.sql
+++ Updates/5326_charm_immune.sql
@@ -27,3 +27,6 @@ Injured Warsong Warrior
 
 UPDATE creature_template SET MechanicImmuneMask=MechanicImmuneMask|1 WHERE entry IN(23940,26844,24218,24152,24238,27579,28373,29885,29375,29503,27334,27762,27210,26926,26828,26180,25392,26158,25804,25605,27108,27106,27107,27110);
 
+-- Kel'gash the Wicked
+UPDATE creature_template SET MechanicImmuneMask=MechanicImmuneMask|1 WHERE entry IN(16358);
+
