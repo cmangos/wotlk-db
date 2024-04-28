3d2166eb7246b0831e1e6b7ef6412699afa0d64e
killerwife <killerwife@gmail.com>
2024-04-28 09:25:09 +0200

    [5328] Fill empty spots in CLS using manual guessing

---
+++ Updates/5328_cls_stage_2_guesswork.sql
@@ -0,0 +1,14 @@
+-- manual guesswork
+UPDATE creature_template_classlevelstats SET Strength=22, Agility=8, Stamina=22, Intellect=20, Spirit=21 WHERE Level=1 AND Class=2;
+UPDATE creature_template_classlevelstats SET Strength=21, Agility=8, Stamina=21, Intellect=19, Spirit=20 WHERE Level=2 AND Class=2;
+UPDATE creature_template_classlevelstats SET Strength=21, Agility=20, Stamina=21, Intellect=26, Spirit=24 WHERE Level=2 AND Class=8;
+
+UPDATE creature_template_classlevelstats SET Strength=443, Agility=140, Stamina=378, Intellect=66, Spirit=112 WHERE Level=83 AND Class=1;
+
+UPDATE creature_template_classlevelstats SET Strength=345, Agility=96, Stamina=333, Intellect=152, Spirit=212 WHERE Level=82 AND Class=2;
+UPDATE creature_template_classlevelstats SET Strength=373, Agility=101, Stamina=339, Intellect=154, Spirit=215 WHERE Level=83 AND Class=2;
+
+UPDATE creature_template_classlevelstats SET Strength=360, Agility=89, Stamina=121, Intellect=377, Spirit=379 WHERE Level=82 AND Class=8;
+UPDATE creature_template_classlevelstats SET Strength=396, Agility=94, Stamina=123, Intellect=381, Spirit=384 WHERE Level=83 AND Class=8;
+
+
