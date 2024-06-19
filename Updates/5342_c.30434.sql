99d272698ecbc8d84f647a16800b3c3f42729cce
Grz3s <Grz3s@users.noreply.github.com>
2024-06-20 00:19:20 +0200

    [5342] Durik Bronzebomb 30434
    vendor list corrected

---
+++ Updates/5342_c.30434.sql
@@ -0,0 +1,20 @@
+-- Durik Bronzebomb 30434
+-- vendor list corrected
+UPDATE npc_vendor SET Slot = 1  WHERE entry IN (30434) AND item = 5956; -- Blacksmith Hammer
+UPDATE npc_vendor SET Slot = 2  WHERE entry IN (30434) AND item = 2901; -- Mining Pick
+UPDATE npc_vendor SET Slot = 3  WHERE entry IN (30434) AND item = 4399; -- Wooden Stock
+UPDATE npc_vendor SET Slot = 4  WHERE entry IN (30434) AND item = 4400; -- Heavy Stock
+UPDATE npc_vendor SET Slot = 5  WHERE entry IN (30434) AND item = 40533; -- Walnut Stock
+UPDATE npc_vendor SET Slot = 6  WHERE entry IN (30434) AND item = 39684; -- Hair Trigger
+UPDATE npc_vendor SET Slot = 7  WHERE entry IN (30434) AND item = 2880; -- Weak Flux
+UPDATE npc_vendor SET Slot = 8  WHERE entry IN (30434) AND item = 3466; -- Strong Flux
+UPDATE npc_vendor SET Slot = 9  WHERE entry IN (30434) AND item = 10648; -- Common Parchment
+UPDATE npc_vendor SET Slot = 10 WHERE entry IN (30434) AND item = 10647; -- Engineer's Ink
+UPDATE npc_vendor SET Slot = 11 WHERE entry IN (30434) AND item = 4382; -- Bronze Framework
+UPDATE npc_vendor SET Slot = 12 WHERE entry IN (30434) AND item = 4361; -- Copper Tube
+UPDATE npc_vendor SET Slot = 13 WHERE entry IN (30434) AND item = 4371; -- Bronze Tube
+UPDATE npc_vendor SET Slot = 14 WHERE entry IN (30434) AND item = 4363; -- Copper Modulator
+UPDATE npc_vendor SET Slot = 15 WHERE entry IN (30434) AND item = 4357; -- Rough Blasting Powder
+UPDATE npc_vendor SET Slot = 16 WHERE entry IN (30434) AND item = 4364; -- Coarse Blasting Powder
+UPDATE npc_vendor SET Slot = 17 WHERE entry IN (30434) AND item = 4389; -- Gyrochronatom
+UPDATE npc_vendor SET Slot = 18 WHERE entry IN (30434) AND item = 4404; -- Silver Contact
