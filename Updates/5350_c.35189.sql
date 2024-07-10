c87702219e9388b7bf094dacc53522e4236f4dc4
Grz3s <Grz3s@users.noreply.github.com>
2024-07-10 02:00:09 +0200

    Update [3550]
    moved to Storm Peaks 33xxx Range

--- Updates/5350_c.35189.sql
+++ Updates/5350_c.35189.sql
@@ -2,13 +2,13 @@
 DELETE FROM pool_creature_template WHERE id = 35189;
 DELETE FROM pool_template WHERE entry = 1084;
 -- moved to spawn groups
-DELETE FROM spawn_group WHERE id = 34095;
+DELETE FROM spawn_group WHERE id = 33006;
 INSERT INTO spawn_group (Id, Name, Type, MaxCount, WorldState, Flags) VALUES
-(34095, 'Storm Peaks - Skoll 35189 - RARE', 0, 1, 0, 0);
-DELETE FROM spawn_group_spawn WHERE id = 34095;
+(33006, 'Storm Peaks - Skoll 35189 - RARE', 0, 1, 0, 0);
+DELETE FROM spawn_group_spawn WHERE id = 33006;
 INSERT INTO spawn_group_spawn (Id, Guid, SlotId) VALUES
-(34095, 534218, -1),
-(34095, 534219, -1),
-(34095, 534220, -1),
-(34095, 534221, -1),
-(34095, 534222, -1);
+(33006, 534218, -1),
+(33006, 534219, -1),
+(33006, 534220, -1),
+(33006, 534221, -1),
+(33006, 534222, -1);
