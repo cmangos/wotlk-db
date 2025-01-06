96b9ac3a02d783cbcc532295a93ebafe525d2f1f
Grz3s <Grz3s@users.noreply.github.com>
2025-01-06 17:51:56 +0100

    [5475] Q: 8338 & 9798
    Both do not work with AUTO_ACCEPT flag. Removed
    Pls show me proof if im wrong

---
+++ Updates/5475_q.8338_9798.sql
@@ -0,0 +1,5 @@
+-- q.8338 'Tainted Arcane Sliver'
+UPDATE Quest_template SET QuestFlags = 128 WHERE entry = 8338;
+
+-- q.9798 'Blood Elf Plans'
+UPDATE Quest_template SET QuestFlags = 128 WHERE entry = 9798;
