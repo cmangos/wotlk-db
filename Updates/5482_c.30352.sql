909780e35069076900f8faaa0a3bce689b288fa2
Grz3s <Grz3s@users.noreply.github.com>
2025-01-22 23:07:55 +0100

    [5482] Skybreaker Marine 30352
    orientation corrected for few in Skybreaker

---
+++ Updates/5482_c.30352.sql
@@ -0,0 +1,4 @@
+-- Skybreaker Marine 30352
+-- orientation corrected for few in Skybreaker
+UPDATE creature SET orientation = 3.141 WHERE guid IN (6230042,6230048,6230050);
+UPDATE creature SET orientation = 0 WHERE guid = 6230045;
