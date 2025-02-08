5dbdf126215e0207e6f2f7160b62a46fb9c1f7f7
Grz3s <Grz3s@users.noreply.github.com>
2025-02-08 21:58:07 +0100

    Update [5631] more backport errors corrected

--- Updates/5631_backport_fixes.sql
+++ Updates/5631_backport_fixes.sql
@@ -23,3 +23,11 @@ DELETE FROM creature_movement WHERE Id = 55584;
 -- Table `dbscripts_on_creature_movement` contain unused script, id 1855611.
 -- Table `dbscripts_on_creature_movement` contain unused script, id 1855612.
 DELETE FROM dbscripts_on_creature_movement WHERE Id IN (1378,1849301,1855602,1855603,1855604,1855605,1855606,1855607,1855608,1855609,1855610,1855611,1855612);
+-- Event 1879402 Action 3 references invalid dbscript_on_relay id 1879401
+-- Event 1879403 Action 3 references invalid dbscript_on_relay id 1879402
+-- Event 1879404 Action 2 references invalid dbscript_on_relay id 1879403
+DELETE FROM dbscripts_on_relay WHERE id IN (1879401,1879402,1879403);
+INSERT INTO dbscripts_on_relay (id, delay, priority, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, datafloat, x, y, z, o, speed, condition_id, comments) VALUES
+(1879401,0,0,42,0,0,0,0,0,0,12421,0,0,0,0,0,0,0,0,0,0,'Cabal Ritualist - Equip Staff 1'),
+(1879402,0,0,42,0,0,0,0,0,0,14618,0,0,0,0,0,0,0,0,0,0,'Cabal Ritualist - Equip Staff 2'),
+(1879403,0,0,42,0,0,0,0,0,0,13718,0,0,0,0,0,0,0,0,0,0,'Cabal Ritualist - Equip Sword');
