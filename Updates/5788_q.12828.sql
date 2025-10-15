098cdc342eebcab6eef14bb69a81e4fa414985a5
Grz3s <Grz3s@users.noreply.github.com>
2025-10-16 00:43:51 +0200

    [5788][ACID] q.12828 'Ample Inspiration'
    Fixed

---
+++ Updates/5788_q.12828.sql
@@ -0,0 +1,15 @@
+-- q.12828 'Ample Inspiration'
+-- Ironwool Mammoth 29402
+DELETE FROM dbscripts_on_relay WHERE id IN (20489);
+INSERT INTO dbscripts_on_relay (id, delay, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
+(20489,1,15,54627,0,0,0,0,0,0,0,0,0,0,0,0,0,'Part of Ironwool Mammoth 29402 EAI: cast 54627'),
+(20489,2,15,54627,0,0,0,0,0,0,0,0,0,0,0,0,0,'Part of Ironwool Mammoth 29402 EAI: cast 54627'),
+(20489,3,15,54627,0,0,0,0,0,0,0,0,0,0,0,0,0,'Part of Ironwool Mammoth 29402 EAI: cast 54627'),
+(20489,4,15,54628,0,0,0,0,0,0,0,0,0,0,0,0,0,'Part of Ironwool Mammoth 29402 EAI: cast 54628'),
+(20489,5,15,54628,0,0,0,0,0,0,0,0,0,0,0,0,0,'Part of Ironwool Mammoth 29402 EAI: cast 54628'),
+(20489,6,15,54628,0,0,0,0,0,0,0,0,0,0,0,0,0,'Part of Ironwool Mammoth 29402 EAI: cast 54628'),
+(20489,7,15,54628,0,0,0,0,0,0,0,0,0,0,0,0,0,'Part of Ironwool Mammoth 29402 EAI: cast 54628'),
+(20489,8,15,54623,0,0,0,0,0,0,0,0,0,0,0,0,0,'Part of Ironwool Mammoth 29402 EAI: cast 54623'),
+(20489,9,15,54623,0,0,0,0,0,0,0,0,0,0,0,0,0,'Part of Ironwool Mammoth 29402 EAI: cast 54623'),
+(20489,10,15,54581,0,0,0,0,0,0,0,0,0,0,0,0,0,'Part of Ironwool Mammoth 29402 EAI: cast 54581'),
+(20489,11,15,57444,0,0,0,0,0,0,0,0,0,0,0,0,0,'Part of Ironwool Mammoth 29402 EAI: cast 54581');
